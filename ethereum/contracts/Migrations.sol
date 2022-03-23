b766c0085e616e543c11faa26e84394d81544dd5
Tom Pointon <tom@teepeestudios.net>
2022-03-23 14:27:41 +0000

    Make Migrations contract Ownable

--- ethereum/contracts/Migrations.sol
+++ ethereum/contracts/Migrations.sol
@@ -1,19 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Migrations {
-    address public owner = msg.sender;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Needed for truffle migrate to work correctly.
+// Simply stores the last completed migration.
+contract Migrations is Ownable {
     uint public last_completed_migration;
 
-    modifier restricted() {
-        require(
-            msg.sender == owner,
-            "This function is restricted to the contract's owner"
-        );
-        _;
-    }
-
-    function setCompleted(uint completed) public restricted {
+    function setCompleted(uint completed) public onlyOwner  {
         last_completed_migration = completed;
     }
 }
