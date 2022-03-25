e8e813378ac1fbd95cb609707a37c3956f8ff0e9
Ali Behjati <bahjatia@gmail.com>
2022-03-25 17:13:00 +0000

    Add unit test to the terra contract
    - Refactors the code into multiple functions to make unit testing easier
    - Adds build and test of terra contract to CI according to #73

--- terra/contracts/pyth-bridge/src/contract.rs
+++ terra/contracts/pyth-bridge/src/contract.rs
@@ -9,8 +9,8 @@ use cosmwasm_std::{
     QueryRequest,
     Response,
     StdResult,
-    WasmQuery,
     Timestamp,
+    WasmQuery,
 };
 
 use pyth_sdk::{
@@ -23,8 +23,8 @@ use crate::{
         ExecuteMsg,
         InstantiateMsg,
         MigrateMsg,
-        QueryMsg,
         PriceFeedResponse,
+        QueryMsg,
     },
     state::{
         config,
@@ -37,18 +37,13 @@ use crate::{
     },
 };
 
-use p2w_sdk::{
-    BatchPriceAttestation,
-};
+use p2w_sdk::BatchPriceAttestation;
 
 use wormhole::{
     error::ContractError,
     msg::QueryMsg as WormholeQueryMsg,
-    state::{
-        ParsedVAA,
-    },
+    state::ParsedVAA,
 };
-
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(_deps: DepsMut, _env: Env, _msg: MigrateMsg) -> StdResult<Response> {
@@ -101,29 +96,41 @@ fn submit_vaa(
     let state = config_read(deps.storage).load()?;
 
     let vaa = parse_vaa(deps.branch(), env.block.time.seconds(), data)?;
- 
-    // This checks the emitter to be the pyth emitter in wormhole and it comes from emitter chain (Solana)
+
+    verify_vaa_sender(&state, &vaa)?;
+
+    let data = &vaa.payload;
+    let batch_attestation = BatchPriceAttestation::deserialize(&data[..])
+        .map_err(|_| ContractError::InvalidVAA.std())?;
+
+    process_batch_attestation(deps, env, &batch_attestation)
+}
+
+// This checks the emitter to be the pyth emitter in wormhole and it comes from emitter chain (Solana)
+fn verify_vaa_sender(state: &ConfigInfo, vaa: &ParsedVAA) -> StdResult<()> {
     if vaa.emitter_address != state.pyth_emitter || vaa.emitter_chain != state.pyth_emitter_chain {
         return ContractError::InvalidVAA.std_err();
     }
+    Ok(())
+}
 
-    let data = vaa.payload;
-
-    let message = BatchPriceAttestation::deserialize(&data[..])
-        .map_err(|_| ContractError::InvalidVAA.std())?;
-    
+fn process_batch_attestation(
+    mut deps: DepsMut,
+    env: Env,
+    batch_attestation: &BatchPriceAttestation,
+) -> StdResult<Response> {
     let mut new_attestations_cnt: u8 = 0;
 
     // Update prices
-    for price_attestation in message.price_attestations.iter() {
+    for price_attestation in batch_attestation.price_attestations.iter() {
         let price_feed = PriceFeed::new(
-                price_attestation.price_id.to_bytes(),
-                price_attestation.status,
-                price_attestation.expo,
-                0, // max_num_publishers data is currently unavailable
-                0, // num_publishers data is currently unavailable
-                price_attestation.product_id.to_bytes(),
-                price_attestation.price,
+            price_attestation.price_id.to_bytes(),
+            price_attestation.status,
+            price_attestation.expo,
+            0, // max_num_publishers data is currently unavailable
+            0, // num_publishers data is currently unavailable
+            price_attestation.product_id.to_bytes(),
+            price_attestation.price,
             price_attestation.confidence_interval,
             price_attestation.ema_price.val,
             price_attestation.ema_conf.val as u64,
@@ -131,58 +138,63 @@ fn submit_vaa(
 
         let attestation_time = Timestamp::from_seconds(price_attestation.timestamp as u64);
 
-        price_info(deps.storage).update(
-            price_feed.id.as_ref(),
-        |maybe_price_info| -> StdResult<PriceInfo> {
-            match maybe_price_info {
-                Some(price_info) => {
-                    // This check ensures that a price won't be updated with the same or older message.
-                    // Attestation_time is guaranteed increasing in solana
-                    if price_info.attestation_time < attestation_time {
-                        new_attestations_cnt += 1;
-                        Ok(PriceInfo {
-                            arrival_time: env.block.time,
-                            arrival_block: env.block.height,
-                            price_feed,
-                            attestation_time
-                        })
-                    } else {
-                        Ok(price_info)
-                    }
-                },
-                None => {
-                    new_attestations_cnt += 1;
-                    Ok(PriceInfo {
-                        arrival_time: env.block.time,
-                        arrival_block: env.block.height,
-                        price_feed,
-                        attestation_time
-                    })
-                }
-            }
-        })?;
+        if update_price_feed_if_new(&mut deps, &env, price_feed, attestation_time)? {
+            new_attestations_cnt += 1;
+        }
     }
 
     Ok(Response::new()
         .add_attribute("action", "price_update")
         .add_attribute(
             "batch_size",
-            format!("{}", message.price_attestations.len()),
+            format!("{}", batch_attestation.price_attestations.len()),
         )
-        .add_attribute(
-            "num_updates",
-            format!("{}", new_attestations_cnt),
-        )
-    )
+        .add_attribute("num_updates", format!("{}", new_attestations_cnt)))
 }
 
+/// Returns true if the price_feed is newer than the stored one
+fn update_price_feed_if_new(
+    deps: &mut DepsMut,
+    env: &Env,
+    price_feed: PriceFeed,
+    attestation_time: Timestamp,
+) -> StdResult<bool> {
+    let mut is_new_price = true;
+    price_info(deps.storage).update(
+        price_feed.id.as_ref(),
+        |maybe_price_info| -> StdResult<PriceInfo> {
+            match maybe_price_info {
+                Some(price_info) => {
+                    // This check ensures that a price won't be updated with the same or older message.
+                    // Attestation_time is guaranteed increasing in solana
+                    if price_info.attestation_time < attestation_time {
+                        Ok(PriceInfo {
+                            arrival_time: env.block.time,
+                            arrival_block: env.block.height,
+                            price_feed,
+                            attestation_time,
+                        })
+                    } else {
+                        is_new_price = false;
+                        Ok(price_info)
+                    }
+                }
+                None => Ok(PriceInfo {
+                    arrival_time: env.block.time,
+                    arrival_block: env.block.height,
+                    price_feed,
+                    attestation_time,
+                }),
+            }
+        },
+    )?;
+    Ok(is_new_price)
+}
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::PriceFeed { id } => {
-            to_binary(&query_price_info(deps, env, id.as_ref())?)
-        }
+        QueryMsg::PriceFeed { id } => to_binary(&query_price_info(deps, env, id.as_ref())?),
     }
 }
 
@@ -194,25 +206,325 @@ pub fn query_price_info(deps: Deps, env: Env, address: &[u8]) -> StdResult<Price
             // - This will ensure to set status unknown if the price has become very old and hasn't updated yet.
             // - If a price has arrived very late to terra it will set the status to unknown.
             // - If a price is coming from future it's tolerated up to VALID_TIME_PERIOD seconds (using abs diff)
-            //   but more than that is set to unknown, the reason is huge clock difference means there exists a 
+            //   but more than that is set to unknown, the reason is huge clock difference means there exists a
             //   problem in a either Terra or Solana blockchain and if it is Solana we don't want to propagate
             //   Solana internal problems to Terra
-            let time_abs_diff = if env.block.time.seconds() > terra_price_info.attestation_time.seconds() {
-                env.block.time.seconds() - terra_price_info.attestation_time.seconds()
-            } else {
-                terra_price_info.attestation_time.seconds() - env.block.time.seconds()
-            };
+            let time_abs_diff =
+                if env.block.time.seconds() > terra_price_info.attestation_time.seconds() {
+                    env.block.time.seconds() - terra_price_info.attestation_time.seconds()
+                } else {
+                    terra_price_info.attestation_time.seconds() - env.block.time.seconds()
+                };
 
             if time_abs_diff > VALID_TIME_PERIOD.as_secs() {
                 terra_price_info.price_feed.status = PriceStatus::Unknown;
             }
 
-            Ok(
-                PriceFeedResponse {
-                    price_feed: terra_price_info.price_feed,
-                }
-            )
-        },
+            Ok(PriceFeedResponse {
+                price_feed: terra_price_info.price_feed,
+            })
+        }
         Err(_) => ContractError::AssetNotFound.std_err(),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use cosmwasm_std::testing::{
+        mock_dependencies,
+        mock_env,
+    };
+
+    use super::*;
+
+    fn zero_vaa() -> ParsedVAA {
+        ParsedVAA {
+            version: 0,
+            guardian_set_index: 0,
+            timestamp: 0,
+            nonce: 0,
+            len_signers: 0,
+            emitter_chain: 0,
+            emitter_address: vec![],
+            sequence: 0,
+            consistency_level: 0,
+            payload: vec![],
+            hash: vec![],
+        }
+    }
+
+    #[test]
+    fn test_verify_vaa_sender_ok() {
+        let mut config_info = ConfigInfo::default();
+        config_info.pyth_emitter = vec![1u8];
+        config_info.pyth_emitter_chain = 3;
+
+        let mut vaa = zero_vaa();
+        vaa.emitter_address = vec![1u8];
+        vaa.emitter_chain = 3;
+
+        assert_eq!(verify_vaa_sender(&config_info, &vaa), Ok(()));
+    }
+
+    #[test]
+    fn test_verify_vaa_sender_fail_wrong_emitter_address() {
+        let mut config_info = ConfigInfo::default();
+        config_info.pyth_emitter = vec![1u8];
+        config_info.pyth_emitter_chain = 3;
+
+        let mut vaa = zero_vaa();
+        vaa.emitter_address = vec![3u8, 4u8];
+        vaa.emitter_chain = 3;
+        assert_eq!(
+            verify_vaa_sender(&config_info, &vaa),
+            ContractError::InvalidVAA.std_err()
+        );
+
+        let mut vaa = zero_vaa();
+        vaa.emitter_address = vec![1u8];
+        vaa.emitter_chain = 2;
+        assert_eq!(
+            verify_vaa_sender(&config_info, &vaa),
+            ContractError::InvalidVAA.std_err()
+        );
+    }
+
+    #[test]
+    fn test_verify_vaa_sender_fail_wrong_emitter_chain() {
+        let mut config_info = ConfigInfo::default();
+        config_info.pyth_emitter = vec![1u8];
+        config_info.pyth_emitter_chain = 3;
+
+        let mut vaa = zero_vaa();
+        vaa.emitter_address = vec![1u8];
+        vaa.emitter_chain = 2;
+        assert_eq!(
+            verify_vaa_sender(&config_info, &vaa),
+            ContractError::InvalidVAA.std_err()
+        );
+    }
+
+    #[test]
+    fn test_update_price_feed_if_new_first_price_ok() {
+        let mut deps = mock_dependencies(&[]);
+
+        let env = mock_env();
+
+        let mut price_feed = PriceFeed::default();
+        price_feed.expo = 3;
+
+        assert_eq!(
+            update_price_feed_if_new(
+                &mut deps.as_mut(),
+                &env,
+                price_feed,
+                Timestamp::from_seconds(100)
+            ),
+            Ok(true)
+        );
+
+        assert_eq!(
+            price_info(&mut deps.storage)
+                .load(price_feed.id.as_ref())
+                .unwrap()
+                .price_feed,
+            price_feed
+        );
+    }
+
+    #[test]
+    fn test_update_price_feed_if_new_ignore_duplicate_time() {
+        let mut deps = mock_dependencies(&[]);
+
+        let env = mock_env();
+
+        let mut price_feed = PriceFeed::default();
+        price_feed.expo = 3;
+
+        update_price_feed_if_new(
+            &mut deps.as_mut(),
+            &env,
+            price_feed,
+            Timestamp::from_seconds(100),
+        )
+        .unwrap();
+
+        let mut duplicate_time_price_feed = PriceFeed::default();
+        duplicate_time_price_feed.expo = 4;
+        assert_eq!(
+            update_price_feed_if_new(
+                &mut deps.as_mut(),
+                &env,
+                duplicate_time_price_feed,
+                Timestamp::from_seconds(100)
+            ),
+            Ok(false)
+        );
+
+        assert_eq!(
+            price_info(&mut deps.storage)
+                .load(price_feed.id.as_ref())
+                .unwrap()
+                .price_feed,
+            price_feed
+        );
+    }
+
+    #[test]
+    fn test_update_price_feed_if_new_ignore_older() {
+        let mut deps = mock_dependencies(&[]);
+
+        let env = mock_env();
+
+        let mut price_feed = PriceFeed::default();
+        price_feed.expo = 3;
+
+        update_price_feed_if_new(
+            &mut deps.as_mut(),
+            &env,
+            price_feed,
+            Timestamp::from_seconds(100),
+        )
+        .unwrap();
+
+        let mut old_price_feed = PriceFeed::default();
+        old_price_feed.expo = 4;
+
+        assert_eq!(
+            update_price_feed_if_new(
+                &mut deps.as_mut(),
+                &env,
+                old_price_feed,
+                Timestamp::from_seconds(100 - 10)
+            ),
+            Ok(false)
+        );
+
+        assert_eq!(
+            price_info(&mut deps.storage)
+                .load(price_feed.id.as_ref())
+                .unwrap()
+                .price_feed,
+            price_feed
+        );
+    }
+
+    #[test]
+    fn test_update_price_feed_if_new_accept_newer() {
+        let mut deps = mock_dependencies(&[]);
+
+        let env = mock_env();
+
+        let mut price_feed = PriceFeed::default();
+        price_feed.expo = 3;
+
+        update_price_feed_if_new(
+            &mut deps.as_mut(),
+            &env,
+            price_feed,
+            Timestamp::from_seconds(100),
+        )
+        .unwrap();
+
+        let mut new_price_feed = PriceFeed::default();
+        new_price_feed.expo = 4;
+
+        assert_eq!(
+            update_price_feed_if_new(
+                &mut deps.as_mut(),
+                &env,
+                new_price_feed,
+                Timestamp::from_seconds(100 + 10)
+            ),
+            Ok(true)
+        );
+
+        assert_eq!(
+            price_info(&mut deps.storage)
+                .load(price_feed.id.as_ref())
+                .unwrap()
+                .price_feed,
+            new_price_feed
+        );
+    }
+
+    #[test]
+    fn test_query_price_info_ok_trading() {
+        let mut deps = mock_dependencies(&[]);
+        let address = b"123".as_ref();
+
+        let mut dummy_price_info = PriceInfo::default();
+        dummy_price_info.attestation_time = Timestamp::from_seconds(80);
+        dummy_price_info.price_feed.status = PriceStatus::Trading;
+
+        price_info(&mut deps.storage)
+            .save(address, &dummy_price_info)
+            .unwrap();
+
+        let mut env = mock_env();
+        env.block.time = Timestamp::from_seconds(100);
+
+        let price_feed = query_price_info(deps.as_ref(), env, address)
+            .unwrap()
+            .price_feed;
+
+        assert_eq!(price_feed.status, PriceStatus::Trading);
+    }
+
+    #[test]
+    fn test_query_price_info_ok_stale_past() {
+        let mut deps = mock_dependencies(&[]);
+        let address = b"123".as_ref();
+
+        let mut dummy_price_info = PriceInfo::default();
+        dummy_price_info.attestation_time = Timestamp::from_seconds(500);
+        dummy_price_info.price_feed.status = PriceStatus::Trading;
+
+        price_info(&mut deps.storage)
+            .save(address, &dummy_price_info)
+            .unwrap();
+
+        let mut env = mock_env();
+        env.block.time = Timestamp::from_seconds(500 + VALID_TIME_PERIOD.as_secs() + 1);
+
+        let price_feed = query_price_info(deps.as_ref(), env, address)
+            .unwrap()
+            .price_feed;
+
+        assert_eq!(price_feed.status, PriceStatus::Unknown);
+    }
+
+    #[test]
+    fn test_query_price_info_ok_stale_future() {
+        let mut deps = mock_dependencies(&[]);
+        let address = b"123".as_ref();
+
+        let mut dummy_price_info = PriceInfo::default();
+        dummy_price_info.attestation_time = Timestamp::from_seconds(500);
+        dummy_price_info.price_feed.status = PriceStatus::Trading;
+
+        price_info(&mut deps.storage)
+            .save(address, &dummy_price_info)
+            .unwrap();
+
+        let mut env = mock_env();
+        env.block.time = Timestamp::from_seconds(500 - VALID_TIME_PERIOD.as_secs() - 1);
+
+        let price_feed = query_price_info(deps.as_ref(), env, address)
+            .unwrap()
+            .price_feed;
+
+        assert_eq!(price_feed.status, PriceStatus::Unknown);
+    }
+
+    #[test]
+    fn test_query_price_info_err_not_found() {
+        let env = mock_env();
+        let deps = mock_dependencies(&[]);
+
+        assert_eq!(
+            query_price_info(deps.as_ref(), env, b"123".as_ref()),
+            ContractError::AssetNotFound.std_err()
+        );
     }
 }
