125b44eb0ed2de3e883bba7ca291d11e6f575714
Ali Behjati <bahjatia@gmail.com>
2022-04-22 10:48:23 +0000

    Add parsing multiple ids for getting latest feed

--- third_party/pyth/price-service/src/rest.ts
+++ third_party/pyth/price-service/src/rest.ts
@@ -56,25 +56,60 @@ export class RestAPI {
     });
     endpoints.push("latest_vaa_bytes/<price_feed_id>");
 
-    app.get("/latest_price_feed/:price_feed_id", (req: Request, res: Response) => {
+    // It will be called with query param `id` such as: `/latest_price_feed?id=xyz&id=abc
+    app.get("/latest_price_feed", (req: Request, res: Response) => {
       this.promClient?.incApiLatestPriceFeedRequests();
-      logger.info(`Received latest_price_feed request for ${req.params.price_feed_id}`)
+      logger.info(`Received latest_price_feed request for query: ${req.query}`);
 
-      let latestPriceInfo = this.priceFeedVaaInfo.getLatestPriceInfo(req.params.price_feed_id);
-
-      if (latestPriceInfo === undefined) {
-        this.promClient?.incApiLatestPriceFeedNotFoundResponse();
-        res.sendStatus(404);
+      if (req.query.id === undefined) {
+        res.status(400);
+        res.end("No id is provided");
         return;
+      }
+
+      console.log(req.query);
+
+      let priceIds: string[] = [];
+      if (typeof(req.query.id) === "string") {
+        priceIds.push(req.query.id);
+      } else if (Array.isArray(req.query.id)) {
+        for (let entry of req.query.id) {
+          if (typeof(entry) === "string") {
+            priceIds.push(entry);
+          } else {
+            res.status(400);
+            res.end("id is expected to be string or an array of strings");
+            return;    
+          }
+        }
+      } else {
+        res.status(400);
+        res.end("id is expected to be string or an array of strings");
+        return;
+      }
+
+      let responseJson = []
+
+      for (let id of priceIds) {
+        let latestPriceInfo = this.priceFeedVaaInfo.getLatestPriceInfo(id);
+
+        if (latestPriceInfo === undefined) {
+          this.promClient?.incApiLatestPriceFeedNotFoundResponse();
+          res.status(404);
+          res.end(`Price Feed with id ${id} not found`);
+          return;
+        }
+    
+        const freshness: DurationInSec = (new Date).getTime()/1000 - latestPriceInfo.receiveTime;
+        this.promClient?.addApiLatestPriceFeedFreshness(freshness); 
+        
+        responseJson.push(latestPriceInfo.priceFeed.toJson());
       }
 
       this.promClient?.incApiLatestPriceFeedSuccessResponse();
 
-      const freshness: DurationInSec = (new Date).getTime()/1000 - latestPriceInfo.receiveTime;
-      this.promClient?.addApiLatestPriceFeedFreshness(freshness);
-
       res.status(200);
-      res.write(JSON.stringify(latestPriceInfo.priceFeed.toJson()));
+      res.write(JSON.stringify(responseJson));
       res.end();
     });
     endpoints.push("latest_price_feed/<price_feed_id>");
