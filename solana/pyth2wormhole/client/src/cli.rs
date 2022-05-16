dfb7e69eec9932c42d2983ece243a90f182f2181
Stan Drozd <stan@nexantic.com>
2022-05-16 13:33:16 +0200

    Bump Solana to v1.10.13, bump borsh to v0.9.3, bump clap to v3.18
    commit-id:519b2a6d

--- solana/pyth2wormhole/client/src/cli.rs
+++ solana/pyth2wormhole/client/src/cli.rs
@@ -3,8 +3,9 @@
 use solana_program::pubkey::Pubkey;
 use std::path::PathBuf;
 
-use clap::Clap;
-#[derive(Clap)]
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
 #[clap(
     about = "A client for the pyth2wormhole Solana program",
     author = "Pyth Network Contributors"
@@ -14,12 +15,12 @@ pub struct Cli {
         short,
         long,
         default_value = "3",
-        about = "Logging level, where 0..=1 RUST_LOG=error and 5.. is RUST_LOG=trace"
+        help = "Logging level, where 0..=1 RUST_LOG=error and 5.. is RUST_LOG=trace"
     )]
     pub log_level: u32,
     #[clap(
         long,
-        about = "Identity JSON file for the entity meant to cover transaction costs",
+        help = "Identity JSON file for the entity meant to cover transaction costs",
         default_value = "~/.config/solana/id.json"
     )]
     pub payer: String,
@@ -31,7 +32,7 @@ pub struct Cli {
     pub action: Action,
 }
 
-#[derive(Clap)]
+#[derive(Subcommand)]
 pub enum Action {
     #[clap(about = "Initialize a pyth2wormhole program freshly deployed under <p2w_addr>")]
     Init {
@@ -48,32 +49,32 @@ pub enum Action {
     )]
     // Note: defaults target SOL mainnet-beta conditions at implementation time
     Attest {
-        #[clap(short = 'f', long = "--config", about = "Attestation YAML config")]
+        #[clap(short = 'f', long = "--config", help = "Attestation YAML config")]
         attestation_cfg: PathBuf,
         #[clap(
             short = 'n',
             long = "--n-retries",
-            about = "How many times to retry send_transaction() on each batch before flagging a failure.",
+            help = "How many times to retry send_transaction() on each batch before flagging a failure.",
             default_value = "5"
         )]
         n_retries: usize,
         #[clap(
             short = 'd',
             long = "--daemon",
-            about = "Do not stop attesting. In this mode, this program will behave more like a daemon and continuously attest the specified symbols.",
+            help = "Do not stop attesting. In this mode, this program will behave more like a daemon and continuously attest the specified symbols.",
         )]
         daemon: bool,
         #[clap(
             short = 't',
             long = "--timeout",
-            about = "How many seconds to wait before giving up on get_transaction() for tx confirmation.",
+            help = "How many seconds to wait before giving up on get_transaction() for tx confirmation.",
             default_value = "40"
         )]
         conf_timeout_secs: u64,
         #[clap(
             short = 'i',
             long = "--rpc-interval",
-            about = "How many milliseconds to wait between SOL RPC requests",
+            help = "How many milliseconds to wait between SOL RPC requests",
             default_value = "200"
         )]
         rpc_interval_ms: u64,
