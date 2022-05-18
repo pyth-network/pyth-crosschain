8b221da772db485dab0eb8d9a18e6fb0d6227732
Ali Behjati <bahjatia@gmail.com>
2022-05-18 16:11:27 +0000

    removes guardian + use fixed version release of it

--- node/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/certusone/wormhole/node/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- node/pkg/processor/quorum.go
+++
@@ -1,9 +0,0 @@
-package processor
-
-// CalculateQuorum returns the minimum number of guardians that need to sign a VAA for a given guardian set.
-//
-// The canonical source is the calculation in the contracts (solana/bridge/src/processor.rs and
-// ethereum/contracts/Wormhole.sol), and this needs to match the implementation in the contracts.
-func CalculateQuorum(numGuardians int) int {
-	return ((numGuardians*10/3)*2)/10 + 1
-}
