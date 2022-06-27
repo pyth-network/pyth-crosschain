b4a8c8c54f705024e118e0c48edff9a0a268e284
Stan Drozd <stan@nexantic.com>
2022-06-27 13:22:30 +0200

    ethereum: Add the multi-source migration to prod-receiver

---
+++ ethereum/migrations/prod-receiver/4_pyth_multiple_emitters.js
@@ -0,0 +1,13 @@
+require('dotenv').config({ path: "../.env" });
+
+const PythUpgradable = artifacts.require("PythUpgradable");
+
+const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
+
+/**
+ * Adds multiple emitter/chain ID support
+ */
+module.exports = async function (deployer) {
+    const instance = await PythUpgradable.deployed();
+  await upgradeProxy(instance.address, PythUpgradable, { deployer, call: "migrateMultiSources" });
+}
