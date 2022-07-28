f7c6d2f7760db089b3c6ec4cf3e651dbec9015f7
Ali Behjati <bahjatia@gmail.com>
2022-07-28 09:23:55 +0000

    Add events to migration names and docs

--- ethereum/migrations/prod-receiver/5_pyth_add_events_and_update_fee.js
+++ ethereum/migrations/prod-receiver/5_pyth_add_events_and_update_fee.js
@@ -5,8 +5,10 @@ const PythUpgradable = artifacts.require("PythUpgradable");
 const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 
 /**
- * This change adds fee for updating prices. As it is intended to keep the fee 0 and default value in Ethereum is 0,
- * this migration only upgrades the contract and does not set the value. 
+ * This change:
+ * - Adds fee for updating prices. Default value in Ethereum is 0, so the value after upgrade will be 0.
+ * - Emits multiple events when a price gets updated. This can be used by off-chain applications to monitor
+ *   the contract activity.
  */
 module.exports = async function (deployer) {
     const instance = await PythUpgradable.deployed();
