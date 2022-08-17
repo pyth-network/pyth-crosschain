f4e8ca36d9579edc72accda3d90e6e01551dee94
Ali Behjati <bahjatia@gmail.com>
2022-08-17 12:19:26 +0000

    Add migration files for this update

---
+++ ethereum/migrations/prod-receiver/6_pyth_update_interface_add_latest_methods.js
@@ -0,0 +1,16 @@
+require('dotenv').config({ path: "../.env" });
+
+const PythUpgradable = artifacts.require("PythUpgradable");
+
+const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
+
+/**
+ * This change:
+ * - Updates the interface, removes `getPrevPriceUnsafe` and adds two functions
+ *   `getLatestAvailablePriceUnsafe` and `getLatestAvailablePriceWithinDuration`
+ *   to replace its behaviour in a more elegant way.
+ */
+module.exports = async function (deployer) {
+    const proxy = await PythUpgradable.deployed();
+    await upgradeProxy(proxy.address, PythUpgradable, { deployer });
+}
