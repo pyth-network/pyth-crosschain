06ecb21cba9f2de0391a3b36c65fff73b3ab4d0f
Ali Behjati <bahjatia@gmail.com>
2022-08-24 11:32:57 +0000

    Update latest migration descriptions

--- ethereum/migrations/test/8_pyth_update_interface_add_update_if_necessary.js
+++ ethereum/migrations/test/8_pyth_update_interface_add_update_if_necessary.js
@@ -8,6 +8,9 @@ const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
  * This change:
  * - Updates the interface, adds `updatePriceFeedsIfNecessary` that wraps
  *   `updatePriceFeeds` and rejects if the price update is not necessary. 
+ * - Change some memory modifiers to improve gas efficiency.
+ * - Change staleness logic to be included in the sdk and bring
+ *   more clarity to the existing code.
  */
 module.exports = async function (deployer) {
     const proxy = await PythUpgradable.deployed();
