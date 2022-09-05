77bb0d3e4c74d041439235462632426d2e021c0c
Daniel Chew <cctdaniel@outlook.com>
2022-09-05 08:30:08 +0000

    add back logging statement for VAA

--- third_party/pyth/price-service/src/listen.ts
+++ third_party/pyth/price-service/src/listen.ts
@@ -150,6 +150,7 @@ export class Listener implements PriceStore {
   }
 
   async processVaa(vaaBytes: string) {
+    logger.info("Received a new VAA");
     const { parse_vaa } = await importCoreWasm();
     const parsedVAA = parse_vaa(hexToUint8Array(vaaBytes));
 
