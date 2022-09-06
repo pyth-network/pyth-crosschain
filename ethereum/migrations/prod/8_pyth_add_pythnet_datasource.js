65c273fa0afd5872f10972f65ead2cc6fa076da8
Ali Behjati <bahjatia@gmail.com>
2022-09-06 13:03:21 +0200

    Add pythnet migration files (#246)
    * Add pythnet migration files
    
    Also:
    - Add related env vars for testnet networks. Mainnet ones to be added
    when it's ready.
    - Rename Pyth2Wormhole env vars to solana
    
    * Add mainnet emitter
    
    * Fix migration file numbers
    
    * Address Jayant comments

---
+++ ethereum/migrations/prod/8_pyth_add_pythnet_datasource.js
@@ -0,0 +1,21 @@
+require('dotenv').config({ path: "../.env" });
+
+const PythUpgradable = artifacts.require("PythUpgradable");
+
+const pythnetChainId = process.env.PYTHNET_CHAIN_ID;
+const pythnetEmitter = process.env.PYTHNET_EMITTER;
+
+console.log("pythnetEmitter: " + pythnetEmitter);
+console.log("pythnetChainId: " + pythnetChainId);
+
+/**
+ * This change:
+ * - Adds PythNet data source.
+ */
+module.exports = async function () {
+    const proxy = await PythUpgradable.deployed();
+    await proxy.addDataSource(
+        pythnetChainId,
+        pythnetEmitter,
+    );
+}
