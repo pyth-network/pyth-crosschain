9a285cf799ea09eac71704afafc382fc889b0849
Ali Behjati <bahjatia@gmail.com>
2022-09-23 19:19:02 +0200

    Abehjati/eth-update-interface (#301)
    * Update contract interface to the latest sdk
    
    * Add migration step
    
    * Fix eth2 chainid change
    
    * Address reviews

---
+++ ethereum/migrations/prod-receiver/11_pyth_make_interface_simpler.js
@@ -0,0 +1,21 @@
+require('dotenv').config({ path: "../.env" });
+
+const PythUpgradable = artifacts.require("PythUpgradable");
+const governanceChainId = process.env.GOVERNANCE_CHAIN_ID;
+const governanceEmitter = process.env.GOVERNANCE_EMITTER;
+
+console.log("governanceEmitter: " + governanceEmitter);
+console.log("governanceChainId: " + governanceChainId);
+
+const { upgradeProxy } = require("@openzeppelin/truffle-upgrades");
+
+/**
+ * Version 1.1.0
+ * 
+ * This change:
+ * - Use pyth-sdk-solidity 1.0.0 which simplifies the PriceFeed interface
+ */
+module.exports = async function (deployer) {
+    const proxy = await PythUpgradable.deployed();
+    await upgradeProxy(proxy.address, PythUpgradable, { deployer, unsafeSkipStorageCheck: true });
+}
