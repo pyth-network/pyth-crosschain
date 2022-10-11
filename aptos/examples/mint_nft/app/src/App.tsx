949ea465f87be11e0f921f1858b07c677b81b0f7
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-10-11 13:37:47 -0500

    Add recency check

--- aptos/examples/mint_nft/app/src/App.tsx
+++ aptos/examples/mint_nft/app/src/App.tsx
@@ -28,6 +28,14 @@ const PYTH_TABLE_HANDLE =
   "0x21b2122f77d3f9f944456c0ca8ffa6a13c541476433e64ab6ae81d48277a1181"; // The prices are stored in this table. WARNING : Consumers should not access this table in frontend code and should instead get the prices from the price service.
 const MINT_NFT_MODULE = "0x122f1cd6062f72a12a5755d6bbf590ee9ae31ba5a12ce60f7f6aea1967e1c02b";
 
+function RecentText(props : {isRecent : boolean}) {
+  const isRecent = props.isRecent;
+  if (isRecent) {
+    return <span style={{color: 'green'}}> Recent </span>;
+  }
+  return <span style={{color: 'red'}}> Outdated </span>;
+}
+
 function App() {
   const [isConnected, setIsConnected] = React.useState<boolean>(false);
 
@@ -37,6 +45,12 @@ function App() {
 
   const [pythOffChainPrice, setPythOffChainPrice] = React.useState<number>(0);
   const [pythOnChainPrice, setPythOnChainPrice] = React.useState<number>(0);
+  const [pythOffChainPublishTimestamp, setPythOffChainPublishTimestamp] = React.useState<number>(0);
+  const [pythOnChainPublishTimestamp, setPythOnChainPublishTimestamp] = React.useState<number>(0);
+  const [pythRecencyThreshold, setRecencyThreshold] = React.useState<number>(0);
+  const [onChainPriceIsRecent, setOnchainPriceRecent] = React.useState<boolean>(false);
+  const [offChainPriceIsRecent, setOffchainPriceRecent] = React.useState<boolean>(false);
+
 
   // Reading directly from the table should never be done by a consumer, as this bypasses our recency checks
   // We're doing it here to highlight the inner workings of Pyth
@@ -52,11 +66,21 @@ function App() {
       data.price_feed.price.price.magnitude *
         10 ** -data.price_feed.price.expo.magnitude
     );
+    setPythOnChainPublishTimestamp( data.price_feed.price.timestamp);
   };
 
   // Fetch onchain price when you land on the website
   React.useEffect(() => {
     fetchOnChainPrice();
+  }, []);
+
+  const getRecencyThreshold = async () => {
+    let data = await aptosClient.getAccountResource(PYTH_MODULE, `${PYTH_MODULE}::state::StalePriceThreshold`);
+    setRecencyThreshold((data.data as any).threshold_secs);
+  };
+  // Fetch onchain recency threshold when you land on the website
+  React.useEffect(() => {
+    getRecencyThreshold();
   }, []);
 
   // Subscribe to offchain prices
@@ -66,17 +90,28 @@ function App() {
       setPythOffChainPrice(
         priceFeed.getPriceUnchecked()?.getPriceAsNumberUnchecked() || 0
       );
+      setPythOffChainPublishTimestamp(priceFeed.getPriceUnchecked()?.publishTime || 0);
     }
   );
+
+  // Check recency of the prices 
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setOnchainPriceRecent(Date.now()/1000 - pythOnChainPublishTimestamp < pythRecencyThreshold);
+      setOffchainPriceRecent(Date.now()/1000 -  pythOffChainPublishTimestamp < pythRecencyThreshold);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [pythOnChainPrice, pythOffChainPrice, pythOffChainPrice]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Mint your Pythian NFT</p>
-        <p>Current offchain ETH/USD : {pythOffChainPrice.toFixed(3)}</p>
+        <p>Current offchain ETH/USD : {pythOffChainPrice.toFixed(3)} <RecentText isRecent={offChainPriceIsRecent} /></p>
         <p>Current NFT price : {(100 / pythOffChainPrice).toFixed(5)} APT</p>
-        <p>Onchain ETH/USD : {pythOnChainPrice.toFixed(3)}</p>
+        <p>Onchain ETH/USD : {pythOnChainPrice.toFixed(3)} <RecentText isRecent={onChainPriceIsRecent} /> </p>
         <div>
           <button
             onClick={async () => {
