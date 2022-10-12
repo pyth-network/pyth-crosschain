befe57379bddaeb6700ebc05dfe90f08bf651333
Ali Behjati <bahjatia@gmail.com>
2022-10-12 20:55:34 +0000

    Move common variables to cluster env Also rename mainnet to ethereum

--- ethereum/truffle-config.js
+++ ethereum/truffle-config.js
@@ -9,7 +9,7 @@ module.exports = {
       port: 8545,
       network_id: "*",
     },
-    mainnet: {
+    ethereum: {
       provider: () =>
         new HDWalletProvider(
           process.env.MNEMONIC,
