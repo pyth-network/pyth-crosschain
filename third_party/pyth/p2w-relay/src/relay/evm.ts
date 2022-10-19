191f3fc6252ee5d51fe5f6693c3cfc83e812c902
Ali Behjati <bahjatia@gmail.com>
2022-10-19 08:42:40 +0000

    Fix evm relayer compile issue

--- third_party/pyth/p2w-relay/src/relay/evm.ts
+++ third_party/pyth/p2w-relay/src/relay/evm.ts
@@ -46,7 +46,7 @@ export class EvmRelay implements Relay {
         : null;
 
       const updateData = ["0x" + signedVAAs[i]];
-      const updateFee = await this.p2wContract.getUpdateFee(updateData);
+      const updateFee = await this.p2wContract["getUpdateFee(bytes[])"](updateData);
 
       let tx = this.p2wContract
         .updatePriceFeeds(updateData, { gasLimit: 2000000, value: updateFee })
