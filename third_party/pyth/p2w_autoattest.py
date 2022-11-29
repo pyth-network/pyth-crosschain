efade987653fa0719e8a6b6599acbea0ba9be93f
Jayant Krishnamurthy <jkrishnamurthy@jumptrading.com>
2022-11-29 13:49:32 -0800

    main

--- third_party/pyth/p2w_autoattest.py
+++ third_party/pyth/p2w_autoattest.py
@@ -190,32 +190,31 @@ mapping_addr: {mapping_addr}
 mapping_reload_interval_mins: 1 # Very fast for testing purposes
 min_rpc_interval_ms: 0 # RIP RPC
 max_batch_jobs: 1000 # Where we're going there's no oomkiller
-default_attestation_conditions:
-  min_interval_secs: 30
-  price_changed_bps: 500
-name_groups:
+symbol_groups:
   - group_name: fast_interval_only
     conditions:
       min_interval_secs: 1
-    symbol_names:
+    symbols:
 """
 
-    # integer-divide the symbols in ~thirds for three test groups:
-    # a fast group that is specified by symbol name only,
-    # a slower group that is specified with full account details,
-    # and a default group that is read from the mapping account without being explicitly configured.
-    third_len = len(pyth_accounts) // 3
+    # integer-divide the symbols in ~half for two test
+    # groups. Assumes arr[:idx] is exclusive, and arr[idx:] is
+    # inclusive
+    third_len = len(pyth_accounts) // 3;
 
     for thing in pyth_accounts[:third_len]:
         name = thing["name"]
+        price = thing["price"]
+        product = thing["product"]
 
         cfg_yaml += f"""
-      - {name}"""
+      - name: {name}
+        price_addr: {price}
+        product_addr: {product}"""
 
     # End of fast_interval_only
 
     cfg_yaml += f"""
-symbol_groups:
   - group_name: longer_interval_sensitive_changes
     conditions:
       min_interval_secs: 10
@@ -232,6 +231,14 @@ symbol_groups:
       - name: {name}
         price_addr: {price}
         product_addr: {product}"""
+
+    cfg_yaml += f"""
+  - group_name: mapping
+    conditions:
+      min_interval_secs: 30
+      price_changed_bps: 500
+    symbols: []
+"""
 
     with open(P2W_ATTESTATION_CFG, "w") as f:
         f.write(cfg_yaml)
