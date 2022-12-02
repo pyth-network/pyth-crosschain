b7a1651bbfde3ab992df54c54793c6cf019b86f9
Ali Behjati <bahjatia@gmail.com>
2022-12-02 17:55:52 +0100

    Some refactoring to add types with JSDoc

--- ethereum/scripts/assertVaaPayloadEquals.js
+++ ethereum/scripts/assertVaaPayloadEquals.js
@@ -5,22 +5,25 @@ const {
 setDefaultWasm("node");
 const { assert } = require("chai");
 
+/**
+ * Assert the VAA has payload equal to `expectedPayload`
+ * @param {string} vaaHex
+ * @param {Buffer} expectedPayload
+ */
 module.exports = async function assertVaaPayloadEquals(
-  vaaHexString,
-  expectedPayloadBuffer
+  vaaHex,
+  expectedPayload
 ) {
   const { parse_vaa } = await importCoreWasm();
 
-  if (vaaHexString.startsWith("0x")) {
-    vaaHexString = vaaHexString.substring(2);
+  if (vaaHex.startsWith("0x")) {
+    vaaHex = vaaHex.substring(2);
   }
 
-  const vaaPayload = Buffer.from(
-    parse_vaa(Buffer.from(vaaHexString, "hex")).payload
-  );
+  const vaaPayload = Buffer.from(parse_vaa(Buffer.from(vaaHex, "hex")).payload);
 
   assert(
-    expectedPayloadBuffer.equals(vaaPayload),
+    expectedPayload.equals(vaaPayload),
     "The VAA payload is not equal to the expected payload"
   );
 };
