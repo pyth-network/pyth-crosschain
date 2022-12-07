3642b6f53cf768827909788d7fd815e797ab0fa2
Ali Behjati <bahjatia@gmail.com>
2022-12-07 17:32:11 +0100

    Add a comment about merging wh and receiver chains

--- third_party/pyth/xc-governance-sdk-js/src/chains.ts
+++ third_party/pyth/xc-governance-sdk-js/src/chains.ts
@@ -6,6 +6,8 @@ const RECEIVER_CHAINS = {
   zksync: 60003,
 };
 
+// If there is any overlapping value the receiver chain will replace the wormhole
+// value and that is an expected behavior.
 export const CHAINS = { ...WORMHOLE_CHAINS, ...RECEIVER_CHAINS };
 export declare type ChainName = keyof typeof CHAINS;
 export declare type ChainId = typeof CHAINS[ChainName];
