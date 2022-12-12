5217d5e8746651c7f2a52384768520c055bb3f49
Jayant Krishnamurthy <jkrishnamurthy@jumptrading.com>
2022-12-12 13:40:44 -0800

    mabye

--- cosmwasm/tools/deploy.js
+++ cosmwasm/tools/deploy.js
@@ -183,7 +183,7 @@ addresses["pyth_cosmwasm.wasm"] = await instantiate(
     governance_sequence_number: 0,
     chain_id: 3,
     valid_time_period_secs: 60,
-    fee: 1,
+    fee: "1",
     fee_denom: "uluna",
   },
   "pyth"
