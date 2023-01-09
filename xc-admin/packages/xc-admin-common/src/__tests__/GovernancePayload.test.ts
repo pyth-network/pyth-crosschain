e36dcc26951bf773dc6cda5a7ce90095655f4a29
Guillermo Bescos Alapont <gbescos@stanford.edu>
2023-01-09 12:51:16 -0600

    Update test

--- xc-admin/packages/xc-admin-common/src/__tests__/GovernancePayload.test.ts
+++ xc-admin/packages/xc-admin-common/src/__tests__/GovernancePayload.test.ts
@@ -1,45 +1,62 @@
-import { PublicKey, SystemProgram } from "@solana/web3.js";
-import { decodeExecutePostedVaa, decodeHeader, encodeHeader } from "..";
+import { ChainName } from "@certusone/wormhole-sdk";
+import {
+  PublicKey,
+  SystemProgram,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import {
+  ActionName,
+  decodeExecutePostedVaa,
+  decodeHeader,
+  encodeHeader,
+} from "..";
 import { encodeExecutePostedVaa } from "../governance_payload/ExecutePostedVaa";
 
 test("GovernancePayload ser/de", (done) => {
   jest.setTimeout(60000);
 
-  let governanceHeader = decodeHeader(
-    Buffer.from([80, 84, 71, 77, 0, 0, 0, 26, 0, 0, 0, 0])
-  );
-  expect(governanceHeader?.targetChainId).toBe("pythnet");
-  expect(governanceHeader?.action).toBe("ExecutePostedVaa");
-
+  // Valid header 1
+  let expectedGovernanceHeader = {
+    targetChainId: "pythnet" as ChainName,
+    action: "ExecutePostedVaa" as ActionName,
+  };
   let buffer = Buffer.alloc(1000);
-  let span = encodeHeader(governanceHeader!, buffer);
+  let span = encodeHeader(expectedGovernanceHeader, buffer);
   expect(
     buffer.subarray(0, span).equals(Buffer.from([80, 84, 71, 77, 0, 0, 0, 26]))
   ).toBeTruthy();
 
-  governanceHeader = decodeHeader(
-    Buffer.from([80, 84, 71, 77, 0, 0, 0, 0, 0, 0, 0, 0])
-  );
-  expect(governanceHeader?.targetChainId).toBe("unset");
+  let governanceHeader = decodeHeader(buffer.subarray(0, span));
+  expect(governanceHeader?.targetChainId).toBe("pythnet");
   expect(governanceHeader?.action).toBe("ExecutePostedVaa");
 
+  // Valid header 2
+  expectedGovernanceHeader = {
+    targetChainId: "unset" as ChainName,
+    action: "ExecutePostedVaa" as ActionName,
+  };
   buffer = Buffer.alloc(1000);
-  span = encodeHeader(governanceHeader!, buffer);
+  span = encodeHeader(expectedGovernanceHeader, buffer);
   expect(
     buffer.subarray(0, span).equals(Buffer.from([80, 84, 71, 77, 0, 0, 0, 0]))
   ).toBeTruthy();
+  governanceHeader = decodeHeader(buffer.subarray(0, span));
+  expect(governanceHeader?.targetChainId).toBe("unset");
+  expect(governanceHeader?.action).toBe("ExecutePostedVaa");
 
-  governanceHeader = decodeHeader(
-    Buffer.from([80, 84, 71, 77, 1, 3, 0, 1, 0, 0, 0, 0])
-  );
-  expect(governanceHeader?.targetChainId).toBe("solana");
-  expect(governanceHeader?.action).toBe("SetFee");
-
+  // Valid header 3
+  expectedGovernanceHeader = {
+    targetChainId: "solana" as ChainName,
+    action: "SetFee" as ActionName,
+  };
   buffer = Buffer.alloc(1000);
-  span = encodeHeader(governanceHeader!, buffer);
+  span = encodeHeader(expectedGovernanceHeader, buffer);
   expect(
     buffer.subarray(0, span).equals(Buffer.from([80, 84, 71, 77, 1, 3, 0, 1]))
   ).toBeTruthy();
+  governanceHeader = decodeHeader(buffer.subarray(0, span));
+  expect(governanceHeader?.targetChainId).toBe("solana");
+  expect(governanceHeader?.action).toBe("SetFee");
 
   // Wrong magic number
   governanceHeader = decodeHeader(
@@ -59,34 +76,61 @@ test("GovernancePayload ser/de", (done) => {
   );
   expect(governanceHeader).toBeUndefined();
 
-  // Decode executePostVaa
-  let executePostedVaaArgs = decodeExecutePostedVaa(
-    Buffer.from([80, 84, 71, 77, 0, 0, 0, 26, 0, 0, 0, 0])
-  );
-  expect(executePostedVaaArgs?.header.targetChainId).toBe("pythnet");
-  expect(executePostedVaaArgs?.header.action).toBe("ExecutePostedVaa");
-  expect(executePostedVaaArgs?.instructions.length).toBe(0);
-
+  // Decode executePostVaa with empty instructions
+  let expectedExecuteVaaArgs = {
+    header: {
+      targetChainId: "pythnet" as ChainName,
+      action: "ExecutePostedVaa" as ActionName,
+    },
+    instructions: [] as TransactionInstruction[],
+  };
   buffer = Buffer.alloc(1000);
-  span = encodeExecutePostedVaa(executePostedVaaArgs!, buffer);
-
+  span = encodeExecutePostedVaa(expectedExecuteVaaArgs, buffer);
   expect(
     buffer
       .subarray(0, span)
       .equals(Buffer.from([80, 84, 71, 77, 0, 0, 0, 26, 0, 0, 0, 0]))
   ).toBeTruthy();
+  let executePostedVaaArgs = decodeExecutePostedVaa(buffer.subarray(0, span));
+  expect(executePostedVaaArgs?.header.targetChainId).toBe("pythnet");
+  expect(executePostedVaaArgs?.header.action).toBe("ExecutePostedVaa");
+  expect(executePostedVaaArgs?.instructions.length).toBe(0);
 
-  executePostedVaaArgs = decodeExecutePostedVaa(
-    Buffer.from([
-      80, 84, 71, 77, 0, 0, 0, 26, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0,
-      141, 65, 8, 219, 216, 57, 229, 94, 74, 17, 138, 50, 121, 176, 38, 178, 50,
-      229, 210, 103, 232, 253, 133, 66, 14, 47, 228, 224, 162, 147, 232, 251, 1,
-      1, 252, 221, 21, 33, 156, 1, 72, 252, 246, 229, 150, 218, 109, 165, 127,
-      11, 165, 252, 140, 6, 121, 57, 204, 91, 119, 165, 106, 241, 234, 131, 75,
-      180, 0, 1, 12, 0, 0, 0, 2, 0, 0, 0, 0, 152, 13, 0, 0, 0, 0, 0,
-    ])
-  );
+  // Decode executePostVaa with one system instruction
+  expectedExecuteVaaArgs = {
+    header: {
+      targetChainId: "pythnet" as ChainName,
+      action: "ExecutePostedVaa" as ActionName,
+    },
+    instructions: [
+      SystemProgram.transfer({
+        fromPubkey: new PublicKey(
+          "AWQ18oKzd187aM2oMB4YirBcdgX1FgWfukmqEX91BRES"
+        ),
+        toPubkey: new PublicKey("J25GT2knN8V2Wvg9jNrYBuj9SZdsLnU6bK7WCGrL7daj"),
+        lamports: 890880,
+      }),
+    ] as TransactionInstruction[],
+  };
+  buffer = Buffer.alloc(1000);
+  span = encodeExecutePostedVaa(expectedExecuteVaaArgs, buffer);
+  expect(
+    buffer
+      .subarray(0, span)
+      .equals(
+        Buffer.from([
+          80, 84, 71, 77, 0, 0, 0, 26, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+          0, 0, 0, 141, 65, 8, 219, 216, 57, 229, 94, 74, 17, 138, 50, 121, 176,
+          38, 178, 50, 229, 210, 103, 232, 253, 133, 66, 14, 47, 228, 224, 162,
+          147, 232, 251, 1, 1, 252, 221, 21, 33, 156, 1, 72, 252, 246, 229, 150,
+          218, 109, 165, 127, 11, 165, 252, 140, 6, 121, 57, 204, 91, 119, 165,
+          106, 241, 234, 131, 75, 180, 0, 1, 12, 0, 0, 0, 2, 0, 0, 0, 0, 152,
+          13, 0, 0, 0, 0, 0,
+        ])
+      )
+  ).toBeTruthy();
+  executePostedVaaArgs = decodeExecutePostedVaa(buffer.subarray(0, span));
   expect(executePostedVaaArgs?.header.targetChainId).toBe("pythnet");
   expect(executePostedVaaArgs?.header.action).toBe("ExecutePostedVaa");
   expect(executePostedVaaArgs?.instructions.length).toBe(1);
@@ -114,25 +158,6 @@ test("GovernancePayload ser/de", (done) => {
       Buffer.from([2, 0, 0, 0, 0, 152, 13, 0, 0, 0, 0, 0])
     )
   );
-
-  buffer = Buffer.alloc(1000);
-  span = encodeExecutePostedVaa(executePostedVaaArgs!, buffer);
-  expect(
-    buffer
-      .subarray(0, span)
-      .equals(
-        Buffer.from([
-          80, 84, 71, 77, 0, 0, 0, 26, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
-          0, 0, 0, 141, 65, 8, 219, 216, 57, 229, 94, 74, 17, 138, 50, 121, 176,
-          38, 178, 50, 229, 210, 103, 232, 253, 133, 66, 14, 47, 228, 224, 162,
-          147, 232, 251, 1, 1, 252, 221, 21, 33, 156, 1, 72, 252, 246, 229, 150,
-          218, 109, 165, 127, 11, 165, 252, 140, 6, 121, 57, 204, 91, 119, 165,
-          106, 241, 234, 131, 75, 180, 0, 1, 12, 0, 0, 0, 2, 0, 0, 0, 0, 152,
-          13, 0, 0, 0, 0, 0,
-        ])
-      )
-  ).toBeTruthy();
 
   done();
 });
