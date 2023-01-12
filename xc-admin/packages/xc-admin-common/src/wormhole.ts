2ddaa4561e5039f9ee5c38f152d3b1e0d61779f2
Guillermo Bescos Alapont <gbescos@stanford.edu>
2023-01-12 11:43:52 -0600

    Fix CI

--- xc-admin/packages/xc-admin-common/src/wormhole.ts
+++ xc-admin/packages/xc-admin-common/src/wormhole.ts
@@ -7,4 +7,5 @@ export const WORMHOLE_ADDRESS: Record<PythCluster, PublicKey | undefined> = {
   devnet: new PublicKey("3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5"),
   pythnet: new PublicKey("H3fxXJ86ADW2PNuDDmZJg6mzTtPxkYCpNuQUTgmJ7AjU"),
   testnet: undefined,
+  localnet: new PublicKey("gMYYig2utAxVoXnM9UhtTWrt8e7x2SVBZqsWZJeT5Gw"),
 };
