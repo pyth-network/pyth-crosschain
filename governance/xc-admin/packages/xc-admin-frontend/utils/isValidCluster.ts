d4371294d2a88ead58b29b03adf672da420e570e
Guillermo Bescos Alapont <gbescos@stanford.edu>
2023-01-13 16:47:58 -0600

    Delete unused files

--- governance/xc-admin/packages/xc-admin-frontend/utils/isValidCluster.ts
+++
@@ -1,5 +0,0 @@
-import { PythCluster } from '../contexts/ClusterContext'
-
-export const isValidCluster = (str: string): str is PythCluster =>
-  ['devnet', 'testnet', 'mainnet-beta', 'pythtest', 'pythnet'].indexOf(str) !==
-  -1
