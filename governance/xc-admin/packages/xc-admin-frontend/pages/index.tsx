3cfc68eed78047e588799954cfa555dcbb78e416
Daniel Chew <cctdaniel@outlook.com>
2023-01-16 16:18:08 +0900

    xc admin/add min pub page (#492)
    * add min pubs page
    
    * add wallet
    
    * update connect wallet button
    
    * fix header
    
    * add ClusterSwitch
    
    * fix header css

--- governance/xc-admin/packages/xc-admin-frontend/pages/index.tsx
+++ governance/xc-admin/packages/xc-admin-frontend/pages/index.tsx
@@ -1,21 +1,13 @@
 import type { NextPage } from 'next'
-import { useContext, useEffect } from 'react'
 import Layout from '../components/layout/Layout'
-import Main from '../components/Main'
+import MinPublishers from '../components/MinPublishers'
 import { PythContextProvider } from '../contexts/PythContext'
-import { ClusterContext, DEFAULT_CLUSTER } from './../contexts/ClusterContext'
 
 const Home: NextPage = () => {
-  const { setCluster } = useContext(ClusterContext)
-
-  useEffect(() => {
-    setCluster(DEFAULT_CLUSTER)
-  }, [setCluster])
-
   return (
     <Layout>
       <PythContextProvider>
-        <Main />
+        <MinPublishers />
       </PythContextProvider>
     </Layout>
   )
