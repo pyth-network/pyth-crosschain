bdecaf17c5635c0442e9394bb2b107aebbb6b60d
0xfirefist <dkalra@pyth.network>
2023-01-20 03:23:33 +0530

    update cosmwasm compiler

--- target-chains/cosmwasm/build.sh
+++ target-chains/cosmwasm/build.sh
@@ -4,4 +4,4 @@ docker run --rm -v "$(pwd)":/code \
   -v $(cd ../../third_party; pwd):/third_party \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.12.6
+  cosmwasm/workspace-optimizer:0.12.11
