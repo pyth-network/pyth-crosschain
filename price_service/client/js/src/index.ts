5628f721d564c277196edfd365b2f918d42e62df
Ali Behjati <bahjatia@gmail.com>
2023-01-24 15:26:42 +0000

    Add pyth-common-js as the service client

---
+++ price_service/client/js/src/index.ts
@@ -0,0 +1,12 @@
+export {
+  DurationInMs,
+  PriceServiceConnection,
+  PriceServiceConnectionConfig,
+} from "./PriceServiceConnection";
+
+export {
+  HexString,
+  PriceFeed,
+  Price,
+  UnixTimestamp,
+} from "@pythnetwork/pyth-sdk-js";
