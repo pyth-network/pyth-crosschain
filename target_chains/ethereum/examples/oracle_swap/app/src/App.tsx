fb12860b647790f20db5ed26c8599e301b18e9f7
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2023-01-27 10:58:16 -0800

    make this look better

--- target_chains/ethereum/examples/oracle_swap/app/src/App.tsx
+++ target_chains/ethereum/examples/oracle_swap/app/src/App.tsx
@@ -34,6 +34,17 @@ const CONFIG = {
   priceServiceUrl: "https://xc-testnet.pyth.network",
 };
 
+export interface TokenConfig {
+  name: string;
+  erc20Address: string;
+  pythPriceFeedId: string;
+}
+
+export interface ExchangeRateMeta {
+  rate: number;
+  lastUpdatedTime: Date;
+}
+
 // The Pyth price service client is used to retrieve the current Pyth prices and the price update data that
 // needs to be posted on-chain with each transaction.
 const pythPriceService = new EvmPriceServiceConnection(CONFIG.priceServiceUrl);
@@ -48,7 +59,11 @@ function timeAgo(diff: number) {
   }
 }
 
-function PriceTicker(props: { price: Price | undefined; currentTime: Date }) {
+function PriceTicker(props: {
+  price: Price | undefined;
+  currentTime: Date;
+  tokenName: string;
+}) {
   const price = props.price;
 
   if (price === undefined) {
@@ -57,63 +72,70 @@ function PriceTicker(props: { price: Price | undefined; currentTime: Date }) {
     const now = props.currentTime.getTime() / 1000;
 
     return (
-      <span>
-        <span style={{ color: "green" }}>
-          {" "}
-          {price.getPriceAsNumberUnchecked().toFixed(3) +
-            " ± " +
-            price.getConfAsNumberUnchecked().toFixed(3)}{" "}
-        </span>
-        <span style={{ color: "grey" }}>
-          last updated {timeAgo(now - price.publishTime)} ago
-        </span>
-      </span>
+      <div>
+        <p>
+          Pyth {props.tokenName} price:{" "}
+          <span style={{ color: "green" }}>
+            {" "}
+            {price.getPriceAsNumberUnchecked().toFixed(3) +
+              " ± " +
+              price.getConfAsNumberUnchecked().toFixed(3)}{" "}
+          </span>
+        </p>
+        <p>
+          <span style={{ color: "grey" }}>
+            last updated {timeAgo(now - price.publishTime)} ago
+          </span>
+        </p>
+      </div>
     );
   }
 }
 
 /// React component that shows the offchain price and confidence interval
 function PriceText(props: {
+  rate: ExchangeRateMeta | undefined;
   price: Record<HexString, Price>;
   currentTime: Date;
 }) {
   let basePrice = props.price[CONFIG.baseToken.pythPriceFeedId];
   let quotePrice = props.price[CONFIG.quoteToken.pythPriceFeedId];
 
-  let exchangeRate: number | undefined = undefined;
-  let lastUpdatedTime: Date | undefined = undefined;
-  if (basePrice !== undefined && quotePrice !== undefined) {
-    exchangeRate =
-      basePrice.getPriceAsNumberUnchecked() /
-      quotePrice.getPriceAsNumberUnchecked();
-    lastUpdatedTime = new Date(
-      Math.max(basePrice.publishTime, quotePrice.publishTime) * 1000
-    );
-  }
+  const [showTooltip, setShowTooltip] = useState(false);
 
   return (
     <div>
-      <p>
-        Current Exchange Rate:{" "}
-        {exchangeRate !== undefined ? (
-          exchangeRate.toFixed(4)
-        ) : (
-          <span style={{ color: "grey" }}>"loading"</span>
-        )}
-        <br />
-        Last updated at:{" "}
-        {lastUpdatedTime !== undefined
-          ? lastUpdatedTime.toISOString()
-          : "loading"}
-        <br />
-        <br />
-        Pyth {CONFIG.baseToken.name} price:{" "}
-        <PriceTicker price={basePrice} currentTime={props.currentTime} />
-        <br />
-        Pyth {CONFIG.quoteToken.name} price:{" "}
-        <PriceTicker price={quotePrice} currentTime={props.currentTime} />
-        <br />
-      </p>
+      {props.rate !== undefined ? (
+        <div>
+          Current Exchange Rate: {props.rate.rate.toFixed(4)}{" "}
+          <span
+            className="icon-container"
+            onMouseEnter={() => setShowTooltip(true)}
+            onMouseLeave={() => setShowTooltip(false)}
+          >
+            (details)
+            {showTooltip && (
+              <div className="tooltip">
+                <PriceTicker
+                  price={basePrice}
+                  currentTime={props.currentTime}
+                  tokenName={CONFIG.baseToken.name}
+                />
+                <PriceTicker
+                  price={quotePrice}
+                  currentTime={props.currentTime}
+                  tokenName={CONFIG.quoteToken.name}
+                />
+              </div>
+            )}
+          </span>
+          <p>Last updated at: {props.rate.lastUpdatedTime.toISOString()}</p>
+        </div>
+      ) : (
+        <div>
+          <p>Exchange rate is loading...</p>
+        </div>
+      )}
     </div>
   );
 }
@@ -156,10 +178,148 @@ function PoolStatistics(props: { web3: Web3 | undefined }) {
   );
 }
 
+function OrderEntry(props: {
+  web3: Web3 | undefined;
+  account: string | null;
+  isBuy: boolean;
+  approxPrice: number | undefined;
+}) {
+  const [qty, setQty] = useState<string>("1");
+  const [qtyBn, setQtyBn] = useState<BigNumber | undefined>(
+    BigNumber.from("1")
+  );
+  const [authorizedQty, setAuthorizedQty] = useState<BigNumber>(
+    BigNumber.from("0")
+  );
+  const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
+
+  const [spentToken, setSpentToken] = useState<TokenConfig>(CONFIG.baseToken);
+  const [approxQuoteSize, setApproxQuoteSize] = useState<number | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    if (props.isBuy) {
+      setSpentToken(CONFIG.quoteToken);
+    } else {
+      setSpentToken(CONFIG.baseToken);
+    }
+  }, [props.isBuy]);
+
+  useEffect(() => {
+    async function helper() {
+      if (props.web3 !== undefined && props.account !== null) {
+        setAuthorizedQty(
+          await getApprovedQuantity(
+            props.web3!,
+            spentToken.erc20Address,
+            props.account!
+          )
+        );
+      } else {
+        setAuthorizedQty(BigNumber.from("0"));
+      }
+    }
+
+    helper();
+  }, [props.web3, props.account, spentToken]);
+
+  useEffect(() => {
+    try {
+      const qtyBn = BigNumber.from(qty);
+      setQtyBn(qtyBn);
+    } catch (error) {
+      setQtyBn(undefined);
+    }
+  }, [qty]);
+
+  useEffect(() => {
+    if (qtyBn !== undefined) {
+      setIsAuthorized(authorizedQty.gte(qtyBn));
+    } else {
+      setIsAuthorized(false);
+    }
+  }, [qtyBn, authorizedQty]);
+
+  useEffect(() => {
+    if (qtyBn !== undefined && props.approxPrice !== undefined) {
+      setApproxQuoteSize(qtyBn.toNumber() * props.approxPrice);
+    } else {
+      setApproxQuoteSize(undefined);
+    }
+  }, [props.approxPrice, qtyBn]);
+
+  return (
+    <div>
+      <div>
+        <p>
+          {props.isBuy ? "Buy" : "Sell"}
+          <input
+            type="text"
+            name="base"
+            value={qty}
+            onChange={(event) => {
+              setQty(event.target.value);
+            }}
+          />
+          {CONFIG.baseToken.name}
+        </p>
+        {qtyBn !== undefined && approxQuoteSize !== undefined ? (
+          props.isBuy ? (
+            <p>
+              Pay {approxQuoteSize.toFixed(3)} {CONFIG.quoteToken.name} to
+              receive {qtyBn.toNumber().toFixed(3)} {CONFIG.baseToken.name}
+            </p>
+          ) : (
+            <p>
+              Pay {qtyBn.toNumber().toFixed(3)} {CONFIG.baseToken.name} to
+              receive {approxQuoteSize.toFixed(3)} {CONFIG.quoteToken.name}
+            </p>
+          )
+        ) : (
+          <p>Transaction details are loading...</p>
+        )}
+      </div>
+
+      <div>
+        {props.account === null || props.web3 === undefined ? (
+          <div>Connect your wallet to swap</div>
+        ) : (
+          <div>
+            1.{" "}
+            <button
+              onClick={async () => {
+                await approveToken(
+                  props.web3!,
+                  spentToken.erc20Address,
+                  props.account!
+                );
+              }}
+              disabled={isAuthorized}
+            >
+              {" "}
+              Approve{" "}
+            </button>
+            2.
+            <button
+              onClick={async () => {
+                await sendSwapTx(props.web3!, props.account!, qty, props.isBuy);
+              }}
+              disabled={!isAuthorized}
+            >
+              {" "}
+              Submit{" "}
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
 function App() {
   const { status, connect, account, ethereum } = useMetaMask();
 
-  const [qty, setQty] = useState<string>("0");
   const [web3, setWeb3] = useState<Web3 | undefined>(undefined);
 
   useEffect(() => {
@@ -184,6 +344,27 @@ function App() {
     }
   );
 
+  const [exchangeRateMeta, setExchangeRateMeta] = useState<
+    ExchangeRateMeta | undefined
+  >(undefined);
+
+  useEffect(() => {
+    let basePrice = pythOffChainPrice[CONFIG.baseToken.pythPriceFeedId];
+    let quotePrice = pythOffChainPrice[CONFIG.quoteToken.pythPriceFeedId];
+
+    if (basePrice !== undefined && quotePrice !== undefined) {
+      const exchangeRate =
+        basePrice.getPriceAsNumberUnchecked() /
+        quotePrice.getPriceAsNumberUnchecked();
+      const lastUpdatedTime = new Date(
+        Math.max(basePrice.publishTime, quotePrice.publishTime) * 1000
+      );
+      setExchangeRateMeta({ rate: exchangeRate, lastUpdatedTime });
+    } else {
+      setExchangeRateMeta(undefined);
+    }
+  }, [pythOffChainPrice]);
+
   const [time, setTime] = useState<Date>(new Date());
 
   useEffect(() => {
@@ -193,9 +374,11 @@ function App() {
     };
   }, []);
 
+  const [isBuy, setIsBuy] = useState<boolean>(true);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="control-panel">
         <h3>Control Panel</h3>
 
         <div>
@@ -229,77 +412,70 @@ function App() {
         <PoolStatistics web3={web3} />
       </header>
 
-      <p>
-        Swap between {CONFIG.baseToken.name} and {CONFIG.quoteToken.name}
-      </p>
-      <PriceText price={pythOffChainPrice} currentTime={time} />
-      <div>
-        <label>
-          Order size:
-          <input
-            type="text"
-            name="base"
-            value={qty}
-            onChange={(event) => {
-              setQty(event.target.value);
-            }}
-          />
-          {CONFIG.baseToken.name}
-        </label>
-      </div>
-
-      <div>
-        <button
-          onClick={async () => {
-            await authorizeTokens(
-              web3!,
-              CONFIG.quoteToken.erc20Address,
-              account!
-            );
-            await authorizeTokens(
-              web3!,
-              CONFIG.baseToken.erc20Address,
-              account!
-            );
-          }}
-          disabled={status !== "connected" || !pythOffChainPrice}
-        >
-          {" "}
-          Authorize ERC20 Transfers{" "}
-        </button>{" "}
-        <button
-          onClick={async () => {
-            await sendSwapTx(web3!, account!, qty, true);
-          }}
-          disabled={status !== "connected" || !pythOffChainPrice}
-        >
-          {" "}
-          Buy{" "}
-        </button>{" "}
-        <button
-          onClick={async () => {
-            await sendSwapTx(web3!, account!, qty, false);
-          }}
-          disabled={status !== "connected" || !pythOffChainPrice}
-        >
-          {" "}
-          Sell{" "}
-        </button>{" "}
+      <div className={"App-main"}>
+        <h3>
+          Swap between {CONFIG.baseToken.name} and {CONFIG.quoteToken.name}
+        </h3>
+        <PriceText
+          price={pythOffChainPrice}
+          currentTime={time}
+          rate={exchangeRateMeta}
+        />
+        <div className="tab-header">
+          <div
+            className={`tab-item ${isBuy ? "active" : ""}`}
+            onClick={() => setIsBuy(true)}
+          >
+            Buy
+          </div>
+          <div
+            className={`tab-item ${!isBuy ? "active" : ""}`}
+            onClick={() => setIsBuy(false)}
+          >
+            Sell
+          </div>
+        </div>
+        <div className="tab-content">
+          {isBuy && (
+            <OrderEntry
+              web3={web3}
+              account={account}
+              isBuy={isBuy}
+              approxPrice={exchangeRateMeta?.rate}
+            />
+          )}
+          {!isBuy && (
+            <OrderEntry
+              web3={web3}
+              account={account}
+              isBuy={isBuy}
+              approxPrice={exchangeRateMeta?.rate}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
 }
 
-async function authorizeTokens(
-  web3: Web3,
-  erc20Address: string,
-  sender: string
-) {
+async function approveToken(web3: Web3, erc20Address: string, sender: string) {
   const erc20 = new web3.eth.Contract(ERC20Abi as any, erc20Address);
 
   await erc20.methods
     .approve(CONFIG.swapContractAddress, BigNumber.from("2").pow(256).sub(1))
     .send({ from: sender });
+}
+
+async function getApprovedQuantity(
+  web3: Web3,
+  erc20Address: string,
+  sender: string
+): Promise<BigNumber> {
+  const erc20 = new web3.eth.Contract(ERC20Abi as any, erc20Address);
+  let allowance = BigNumber.from(
+    await erc20.methods.allowance(sender, CONFIG.swapContractAddress).call()
+  );
+  return allowance as BigNumber;
 }
 
 async function sendSwapTx(
