071e7b8e3870e2d2a1dc7af2b47be4933353ed09
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2023-01-31 10:59:29 -0800

    redeploy

--- target_chains/ethereum/examples/oracle_swap/app/src/App.tsx
+++ target_chains/ethereum/examples/oracle_swap/app/src/App.tsx
@@ -32,7 +32,7 @@ const CONFIG = {
       "1fc18861232290221461220bd4e2acd1dcdfbc89c84092c93c18bdc7756c1588",
     decimals: 18,
   },
-  swapContractAddress: "0x405b97aDd1Ae707D8690539e86c2EEd3d044bCcb",
+  swapContractAddress: "0x15F9ccA28688F5E6Cbc8B00A8f33e8cE73eD7B02",
   pythContractAddress: "0xff1a0f4744e8582DF1aE09D5611b887B6a12925C",
   priceServiceUrl: "https://xc-testnet.pyth.network",
   mintQty: 100,
