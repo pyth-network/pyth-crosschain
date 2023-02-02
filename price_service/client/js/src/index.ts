7a9a161ab6bd9f898cdc7a477ab38edf550c0ac2
Ali Behjati <bahjatia@gmail.com>
2023-02-02 11:34:37 +0000

    [price-service/client] Add e2 test

--- price_service/client/js/src/index.ts
+++ price_service/client/js/src/index.ts
@@ -6,6 +6,7 @@ export {
 
 export {
   HexString,
+  PriceFeedMetadata,
   PriceFeed,
   Price,
   UnixTimestamp,
--- third_party/pyth/p2w_autoattest.py
+++ third_party/pyth/p2w_autoattest.py
@@ -114,7 +114,7 @@ mapping_reload_interval_mins: 1 # Very fast for testing purposes
 min_rpc_interval_ms: 0 # RIP RPC
 max_batch_jobs: 1000 # Where we're going there's no oomkiller
 default_attestation_conditions:
-  min_interval_secs: 60
+  min_interval_secs: 10
 symbol_groups:
   - group_name: fast_interval_only
     conditions:
