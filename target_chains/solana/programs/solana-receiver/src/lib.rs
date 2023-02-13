0cff03fd12628844dbb557474b9fd4b7fbadce28
Yunhao Zhang <yz2327@cornell.edu>
2023-02-13 16:34:48 -0500

    Attempt to deserialize posted vaa, but compile fails due to conflicting solana version

--- target_chains/solana/programs/solana-receiver/src/lib.rs
+++ target_chains/solana/programs/solana-receiver/src/lib.rs
@@ -1,15 +1,42 @@
-use anchor_lang::prelude::*;
+use {
+    anchor_lang::prelude::*,
+    state::AnchorVaa,
+    wormhole::Chain::{
+        self,
+        Solana,
+    },
+    pyth_wormhole_attester_sdk::PriceAttestation,
+};
+pub mod state;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+#[derive(Accounts)]
+pub struct DecodePostedVaa<'info> {
+    #[account(mut)]
+    pub payer:          Signer<'info>,
+    #[account(constraint = Chain::from(posted_vaa.emitter_chain) == Solana @ ErrorCode::EmitterChainNotSolana,
+              constraint = (&posted_vaa.magic == b"vaa" || &posted_vaa.magic == b"msg" || &posted_vaa.magic == b"msu") @ErrorCode::PostedVaaHeaderWrongMagicNumber )]
+    pub posted_vaa:     Account<'info, AnchorVaa>,
+}
+
 #[program]
-pub mod solana_receiver {
+pub mod pyth_solana_receiver {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn decode_posted_vaa(ctx: Context<DecodePostedVaa>) -> Result<()> {
+        let posted_vaa = &ctx.accounts.posted_vaa;
+        //let payload = ExecutorPayload::try_from_slice(&posted_vaa.payload)?;
+        let payload = PriceAttestation::deserialize(&posted_vaa.payload)?;
+
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The emitter of the VAA is not Solana.")]
+    EmitterChainNotSolana,
+    #[msg("Posted VAA has wrong magic number in header.")]
+    PostedVaaHeaderWrongMagicNumber,
+}
---
+++ target_chains/solana/programs/solana-receiver/src/state.rs
@@ -0,0 +1,57 @@
+use {
+    anchor_lang::prelude::*,
+    std::{
+        io::Write,
+        ops::Deref,
+        str::FromStr,
+    },
+    wormhole_solana::VAA,
+};
+
+// The current chain's wormhole bridge owns the VAA accounts
+impl Owner for AnchorVaa {
+    // #[cfg(not(feature = "pythtest"))]
+    // fn owner() -> Pubkey {
+    //     Pubkey::from_str("H3fxXJ86ADW2PNuDDmZJg6mzTtPxkYCpNuQUTgmJ7AjU").unwrap()
+    // }
+
+    // #[cfg(feature = "pythtest")]
+    fn owner() -> Pubkey {
+        // wormhole address on solana devnet
+        Pubkey::from_str("3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5").unwrap()
+    }
+}
+
+impl AccountDeserialize for AnchorVaa {
+    // Manual implementation because this account does not have an anchor discriminator
+    fn try_deserialize(buf: &mut &[u8]) -> anchor_lang::Result<Self> {
+        Self::try_deserialize_unchecked(buf)
+    }
+
+    // Manual implementation because this account does not have an anchor discriminator
+    fn try_deserialize_unchecked(buf: &mut &[u8]) -> Result<Self> {
+        AnchorDeserialize::deserialize(buf)
+            .map_err(|_| anchor_lang::error::ErrorCode::AccountDidNotDeserialize.into())
+    }
+}
+
+impl AccountSerialize for AnchorVaa {
+    // Make this fail, this is readonly VAA it should never be serialized by this program
+    fn try_serialize<W: Write>(&self, _writer: &mut W) -> Result<()> {
+        Err(anchor_lang::error::ErrorCode::AccountDidNotSerialize.into())
+    }
+}
+
+impl Deref for AnchorVaa {
+    type Target = VAA;
+
+    fn deref(&self) -> &Self::Target {
+        &self.vaa
+    }
+}
+
+#[derive(Clone, AnchorDeserialize, AnchorSerialize)]
+pub struct AnchorVaa {
+    pub magic: [u8; 3],
+    pub vaa:   VAA,
+}
