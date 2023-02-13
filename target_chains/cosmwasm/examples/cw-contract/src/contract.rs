2a6972b628ee18016503b61c46e1955c35e26e68
0xfirefist <dkalra@pyth.network>
2023-02-14 00:11:31 +0530

    add testing examples

--- target_chains/cosmwasm/examples/cw-contract/src/contract.rs
+++ target_chains/cosmwasm/examples/cw-contract/src/contract.rs
@@ -128,3 +128,156 @@ fn query_fetch_valid_time_period(deps: Deps) -> StdResult<Duration> {
     let duration = get_valid_time_period(&deps.querier, state.pyth_contract_addr)?;
     Ok(duration)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use cosmwasm_std::testing::{
+        mock_dependencies,
+        mock_env,
+        MockApi,
+        MockQuerier,
+        MockStorage,
+    };
+    use cosmwasm_std::{
+        from_binary,
+        Addr,
+        Coin,
+        OwnedDeps,
+        QuerierResult,
+        SystemError,
+        SystemResult,
+        Timestamp,
+        WasmQuery,
+    };
+    use pyth_sdk_cw::testing::MockPyth;
+    use pyth_sdk_cw::{
+        Price,
+        PriceFeed,
+        PriceIdentifier,
+        UnixTimestamp,
+    };
+    use std::convert::TryFrom;
+    use std::time::Duration;
+
+    // Dummy contract address for testing.
+    // For real deployments, see list of contract addresses here https://docs.pyth.network/pythnet-price-feeds/cosmwasm
+    const PYTH_CONTRACT_ADDR: &str = "pyth_contract_addr";
+    // For real deployments, see list of price feed ids here https://pyth.network/developers/price-feed-ids
+    const PRICE_ID: &str = "63f341689d98a12ef60a5cff1d7f85c70a9e17bf1575f0e7c0b2512d48b1c8b3";
+
+    fn default_state() -> State {
+        State {
+            pyth_contract_addr: Addr::unchecked(PYTH_CONTRACT_ADDR),
+            price_feed_id:      PriceIdentifier::from_hex(PRICE_ID).unwrap(),
+        }
+    }
+
+    fn setup_test(
+        state: &State,
+        mock_pyth: &MockPyth,
+        block_timestamp: UnixTimestamp,
+    ) -> (OwnedDeps<MockStorage, MockApi, MockQuerier>, Env) {
+        let mut dependencies = mock_dependencies();
+
+        let mock_pyth_copy = (*mock_pyth).clone();
+        dependencies
+            .querier
+            .update_wasm(move |x| handle_wasm_query(&mock_pyth_copy, x));
+
+        STATE.save(dependencies.as_mut().storage, state).unwrap();
+
+        let mut env = mock_env();
+        env.block.time = Timestamp::from_seconds(u64::try_from(block_timestamp).unwrap());
+
+        (dependencies, env)
+    }
+
+    // Create a handler like this in your test to handle pyth queries. If needed, other contracts
+    // can be configured in this handler via additional cases.
+    fn handle_wasm_query(pyth: &MockPyth, wasm_query: &WasmQuery) -> QuerierResult {
+        match wasm_query {
+            WasmQuery::Smart { contract_addr, msg } if *contract_addr == PYTH_CONTRACT_ADDR => {
+                pyth.handle_wasm_query(msg)
+            }
+            WasmQuery::Smart { contract_addr, .. } => {
+                SystemResult::Err(SystemError::NoSuchContract {
+                    addr: contract_addr.clone(),
+                })
+            }
+            WasmQuery::Raw { contract_addr, .. } => {
+                SystemResult::Err(SystemError::NoSuchContract {
+                    addr: contract_addr.clone(),
+                })
+            }
+            WasmQuery::ContractInfo { contract_addr, .. } => {
+                SystemResult::Err(SystemError::NoSuchContract {
+                    addr: contract_addr.clone(),
+                })
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    #[test]
+    fn test_get_price() {
+        // Arbitrary unix timestamp to coordinate the price feed timestamp and the block time.
+        let current_unix_time = 10_000_000;
+
+        let mut mock_pyth = MockPyth::new(Duration::from_secs(60), Coin::new(1, "foo"), &[]);
+        let price_feed = PriceFeed::new(
+            PriceIdentifier::from_hex(PRICE_ID).unwrap(),
+            Price {
+                price:        100,
+                conf:         10,
+                expo:         -1,
+                publish_time: current_unix_time,
+            },
+            Price {
+                price:        200,
+                conf:         20,
+                expo:         -1,
+                publish_time: current_unix_time,
+            },
+        );
+
+        mock_pyth.add_feed(price_feed);
+
+        let (deps, env) = setup_test(&default_state(), &mock_pyth, current_unix_time);
+
+        let msg = QueryMsg::FetchPrice {};
+        let result = query(deps.as_ref(), env, msg)
+            .and_then(|binary| from_binary::<FetchPriceResponse>(&binary));
+
+        assert_eq!(result.map(|r| r.current_price.price), Ok(100));
+    }
+
+    #[test]
+    fn test_query_fetch_valid_time_period() {
+        // Arbitrary unix timestamp to coordinate the price feed timestamp and the block time.
+        let current_unix_time = 10_000_000;
+
+        let mock_pyth = MockPyth::new(Duration::from_secs(60), Coin::new(1, "foo"), &[]);
+        let (deps, env) = setup_test(&default_state(), &mock_pyth, current_unix_time);
+
+        let msg = QueryMsg::FetchValidTimePeriod {};
+        let result = query(deps.as_ref(), env, msg)
+            .and_then(|binary| from_binary::<Duration>(&binary));
+
+        assert_eq!(result.map(|r| r.as_secs()), Ok(60));
+    }
+
+    #[test]
+    fn test_query_fetch_update_fee() {
+        // Arbitrary unix timestamp to coordinate the price feed timestamp and the block time.
+        let current_unix_time = 10_000_000;
+
+        let mock_pyth = MockPyth::new(Duration::from_secs(60), Coin::new(1, "foo"), &[]);
+        let (deps, env) = setup_test(&default_state(), &mock_pyth, current_unix_time);
+
+        let msg = QueryMsg::FetchUpdateFee { vaas: vec![Binary(vec![1,2,3])] };
+        let result = query(deps.as_ref(), env, msg)
+            .and_then(|binary| from_binary::<Coin>(&binary));
+        assert_eq!(result.map( |r| r.to_string()), Ok(String::from("1foo")))
+    }
+}
