3f282807d7ae56756a90ca17799a9db3b70bd2ff
Guillermo Bescos Alapont <gbescos@stanford.edu>
2023-02-14 18:21:46 +0000

    Cleanup

--- governance/xc_admin/packages/xc_admin_frontend/components/tabs/Proposals.tsx
+++ governance/xc_admin/packages/xc_admin_frontend/components/tabs/Proposals.tsx
@@ -273,7 +273,6 @@ const Proposal = ({
       <div className="col-span-3 my-2 space-y-4 bg-[#1E1B2F] p-4 lg:col-span-1">
         <h4 className="h4 mb-4 font-semibold">Results</h4>
         <hr className="border-gray-700" />
-
         <div className="grid grid-cols-3 justify-center gap-4 text-center align-middle">
           <div>
             <div className="font-bold">Confirmed</div>
