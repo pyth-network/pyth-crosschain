b4237f7b591e900047f17b51629f8d5b33d27d42
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2023-02-25 07:51:09 -0800

    fix

--- price_pusher/src/injective.ts
+++ price_pusher/src/injective.ts
@@ -1,4 +1,4 @@
-import { HexString } from "@pythnetwork/pyth-common-js";
+import { HexString } from "@pythnetwork/price-service-client";
 import { ChainPricePusher, PriceInfo, PriceListener } from "./interface";
 import { DurationInSeconds } from "./utils";
 import { PriceConfig } from "./price-config";
