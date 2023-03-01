947691d19a2fea555d23c736777258fe3609b496
0xfirefist <dkalra@pyth.network>
2023-03-02 01:52:13 +0530

    add config

--- price_pusher/src/index.ts
+++ price_pusher/src/index.ts
@@ -6,4 +6,9 @@ import { hideBin } from "yargs/helpers";
 import injective from "./injective/command";
 import evm from "./evm/command";
 
-yargs(hideBin(process.argv)).command(evm).command(injective).help().argv;
+yargs(hideBin(process.argv))
+  .config("config")
+  .global("config")
+  .command(evm)
+  .command(injective)
+  .help().argv;
