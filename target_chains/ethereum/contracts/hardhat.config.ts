d06c10502d01c828fb5726826900e8774395cdd4
guibescos <59208140+guibescos@users.noreply.github.com>
2023-03-17 09:47:59 -0500

    [evm] Verifying shimmer_testnet and neon_devnet with hardhat (#703)
    * Add shimmer testnet and neon_devnet
    
    * zksync comment
    
    * Add verify

--- target_chains/ethereum/contracts/hardhat.config.ts
+++ target_chains/ethereum/contracts/hardhat.config.ts
@@ -1,8 +1,8 @@
-import "@matterlabs/hardhat-zksync-deploy";
-import "@matterlabs/hardhat-zksync-solc";
 import "@nomiclabs/hardhat-etherscan";
 import "@openzeppelin/hardhat-upgrades";
-import "@matterlabs/hardhat-zksync-verify";
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+// import "@matterlabs/hardhat-zksync-verify"; UNCOMMENT THIS AND INSTALL THIS PACKAGE TO VERIFY ZKSYNC
 
 module.exports = {
   zksolc: {
@@ -36,6 +36,44 @@ module.exports = {
       verifyURL:
         "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
     },
+    neon_devnet: {
+      url: "https://devnet.neonevm.org",
+      chainId: 245022926,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+    },
+    shimmer_testnet: {
+      url: "https://json-rpc.evm.testnet.shimmer.network",
+      chainId: 1070,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: {
+      neon_devnet: "there_should_be_a_dummy_value_here_to_avoid_error",
+      shimmer_testnet: "there_should_be_a_dummy_value_here_to_avoid_error",
+    },
+    customChains: [
+      {
+        network: "neon_devnet",
+        chainId: 245022926,
+        urls: {
+          apiURL: "https://devnet-api.neonscan.org/hardhat/verify",
+          browserURL: "https://devnet.neonscan.org",
+        },
+      },
+      {
+        network: "shimmer_testnet",
+        chainId: 1070,
+        urls: {
+          apiURL: "https://explorer.evm.testnet.shimmer.network/api",
+          browserURL: "https://explorer.evm.testnet.shimmer.network",
+        },
+      },
+    ],
   },
   solidity: {
     version: "0.8.4",
