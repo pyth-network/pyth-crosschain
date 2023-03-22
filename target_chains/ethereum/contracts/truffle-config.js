439eac9e2bd2024933c3c4edb9b2ef2709329a58
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2023-03-22 12:28:41 -0700

    zkevm deployment

--- target_chains/ethereum/contracts/truffle-config.js
+++ target_chains/ethereum/contracts/truffle-config.js
@@ -11,6 +11,9 @@ function payerProvider(url) {
     new HDWalletProvider({
       mnemonic: process.env.MNEMONIC,
       providerOrUrl: url,
+      // This option makes deployments more reliable (by avoiding rate limiting errors) at the cost of
+      // taking a little longer.
+      pollingInterval: 12000,
     });
 }
 
