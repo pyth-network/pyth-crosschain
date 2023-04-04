7686aed3d129514beb010107ea0389a9560ed9d1
0xfirefist <dkalra@pyth.network>
2023-04-04 17:21:00 +0530

    add more documentation

--- target_chains/cosmwasm/tools/src/pyth-wrapper.ts
+++ target_chains/cosmwasm/tools/src/pyth-wrapper.ts
@@ -18,10 +18,23 @@ export type PriceFeedResponse = {
   ema_price: Price;
 };
 
+/**
+ * `PythWrapperQuerier` wraps the ChainQuerier which can be a querier for any cosmwasm chain.
+ * The ChainQuerier is contract independent, and it doesn't work with pyth specific objects which we know of.
+ *
+ * `PythWrapperQuerier` uses the ChainQuerier to query Pyth contracts but it works with Pyth specific objects
+ */
 export class PythWrapperQuerier {
   constructor(private chainQuerier: ChainQuerier) {}
 
-  // get update fee
+  /**
+   * Get the fee required to update the Pyth on-chain contract with the given VAAs.
+   * This function will invoke the corresponding function on the contract itself and
+   * return the result. The result is represented as both a quantity of tokens and a
+   * denomination which may vary depending on the chain being queried.
+   *
+   * @throws an error if it fails
+   */
   async getUpdateFee(contractAddr: string, vaas: string[]): Promise<Coin> {
     try {
       const updateFeeResponse = await this.chainQuerier.getSmartContractState({
@@ -39,7 +52,15 @@ export class PythWrapperQuerier {
     }
   }
 
-  // get price feed
+  /**
+   * Get the price feed stored in the Pyth on-chain contract with the given id.
+   * This function will invoke the corresponding function on the contract itself and
+   * return the result.
+   *
+   * It returns the PriceFeedResponse. See {@link PriceFeedResponse}
+   *
+   * @throws an error if it fails
+   */
   async getPriceFeed(
     contractAddr: string,
     id: string
@@ -61,15 +82,10 @@ export class PythWrapperQuerier {
   }
 }
 
-export type Fund = {
-  denom: string;
-  amount: string;
-};
-
 export type ExecuteUpdatePriceFeedsRequest = {
   contractAddr: string;
-  vaas: string;
-  fund: Fund;
+  vaas: string[];
+  fund: Coin;
 };
 
 export type ExecuteGovernanceInstructionRequest = {
@@ -77,8 +93,28 @@ export type ExecuteGovernanceInstructionRequest = {
   vaa: string;
 };
 
+/**
+ * `PythWrapperExecutor` wraps the ChainExecutor which can be a executor for any cosmwasm chain.
+ * The ChainExecutor is contract independent, and it doesn't work with pyth specific objects which we know of.
+ *
+ * `PythWrapperExecutor` uses the ChainExecutor to execute Pyth contracts but it works with Pyth specific objects
+ */
 export class PythWrapperExecutor {
   constructor(private chainExecutor: ChainExecutor) {}
+
+  /**
+   * Update the price feed stored in Pyth Contracts on chain. This function will send an execute request
+   * to the Pyth contracts with the funds required to update a price feed.
+   *
+   * @param {ExecuteUpdatePriceFeedsRequest} req
+   * @param {string} req.contractAddr
+   * @param {string[]} req.vaas - The vaas for the price feed update
+   * @param {Coin} req.fund - Update fee
+
+   * @returns {ExecuteContractResponse} - The broadcasted transaction hash.
+   *
+   * @throws an error if it fails
+   */
   async executeUpdatePriceFeeds(
     req: ExecuteUpdatePriceFeedsRequest
   ): Promise<ExecuteContractResponse> {
@@ -94,6 +130,18 @@ export class PythWrapperExecutor {
       funds: [fund],
     });
   }
+
+  /**
+   * Execute the governance instruction on Pyth contracts on chain for the given VAA.
+   *
+   * @param {ExecuteGovernanceInstructionRequest} req
+   * @param {string} req.contractAddr
+   * @param {string} req.vaa - The govenance instruction vaa.
+
+   * @returns {ExecuteContractResponse} - The broadcasted transaction hash.
+   *
+   * @throws an error if it fails
+   */
   async executeGovernanceInstruction(
     req: ExecuteGovernanceInstructionRequest
   ): Promise<ExecuteContractResponse> {
