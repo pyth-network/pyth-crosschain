934ba97e5a5bcc27cabb414c7d54a1ebd8c52a5b
optke3 <optke3@gmail.com>
2023-05-01 04:59:46 +0000

    init pyth ts script

--- target_chains/sui/scripts/init_pyth.ts
+++ target_chains/sui/scripts/init_pyth.ts
@@ -11,18 +11,12 @@ import {
   testnetConnection,
   Connection,
 } from "@mysten/sui.js";
-import { Console } from "console";
 
 const provider = new JsonRpcProvider(new Connection({ fullnode: "http://0.0.0.0:9000" }))
 dotenv.config({"path":"~/.env"})
 
 async function main() {
-    // const guardianPrivateKey = process.env.TESTNET_GUARDIAN_PRIVATE_KEY;
-    // if (guardianPrivateKey === undefined) {
-    //   throw new Error("TESTNET_GUARDIAN_PRIVATE_KEY unset in environment");
-    // }
     const walletPrivateKey = process.env.SUI_DEVNET;
-    //const walletPrivateKey = "AARb87p4OlmRjUBCZOBy8iLGTWt1PVZ6gowPx7Lit+Tn";
     if (walletPrivateKey === undefined) {
       throw new Error("SUI_DEVNET unset in environment");
     }
@@ -33,65 +27,54 @@ async function main() {
         ),
      provider
     );
-    console.log(wallet)
 
     // Note: set these before calling init_pyth
-    const deployerCap = "0x480cb17f5776ef3bdf6d83cca798d8a34f5fba29ae701487b56ce0955af249f8"
-    const upgradeCap = "0x558bc846ca736cd69ddf6769402c1509f74c41200ecb635f0ec0cd3acf3feeb7"
+    const pythPackage = "0xb94d0d4a7ce7934e6bae2e430d5d7d5ba58c25d229aa1338ad543f0445bdfc7f"
+    const deployerCap = "0x80bbb9430366fcd6d8b22769988612e202055f12787cc2abdafed5e51f78f756"
+    const upgradeCap = "0xffdc98c07d8cd0d5327209153bd01fedb0b15862da99eb3a837931a44202d48b"
 
-    init_pyth(wallet, deployerCap, upgradeCap)
+    init_pyth(wallet, pythPackage, deployerCap, upgradeCap)
 }
 
 main();
 
 /// Use a programmable transaction block to call
-/// the Pyth setup::init_and_share_state function.
+/// the Pyth pyth::pyth::init_pyth function.
 async function init_pyth(
     signer: RawSigner,
+    pythPackage: string,
     deployerCap: string,
     upgradeCap: string
   ) {
-    const PythPackage = "0x147415b6d9215a2233649ca24f5d51b94bb9a249b381b03fb4a639fc506ebbc6"
 
     const tx = new TransactionBlock();
 
-    // init_and_share_state(
-    //     deployer: DeployerCap,
-    //     upgrade_cap: UpgradeCap,
-    //     stale_price_threshold: u64,
-    //     base_update_fee: u64,
-    //     governance_data_source: DataSource,
-    //     sources: vector<DataSource>,
-    //     ctx: &mut TxContext
-    // )
-
-    // // fun new(emitter_chain: u64, emitter_address: ExternalAddress)
-
-    let [governance_data_source] = tx.moveCall({
-        target: `${PythPackage}::data_source::new`,
-        arguments: [
-            tx.pure(5), // emitter chain
-            tx.pure("0x0000000000000000000000000000000000000000000000000000000000000002") // emitter address
-        ]
-    })
-
     tx.moveCall({
-      target: `${PythPackage}::setup::init_and_share_state`,
+      target: `${pythPackage}::pyth::init_pyth`,
       arguments: [
         tx.object(deployerCap),
         tx.object(upgradeCap),
         tx.pure(1000), // stale price threshold
+        tx.pure(121), // governance emitter chain id
+        tx.pure("3"), // governance emitter chain address
+        tx.pure([121]), // data source emitter chain ids
+        tx.pure(["3"]), // data source addresses
         tx.pure(0), // base update fee
-        governance_data_source, // governance data source
-        governance_data_source // vec<DataSources>
       ],
     });
 
-    return signer.signAndExecuteTransactionBlock({
+    tx.setGasBudget(1_000_000_000n);
+
+    let result = await signer.signAndExecuteTransactionBlock({
       transactionBlock: tx,
       options: {
+        showInput: true,
         showEffects: true,
         showEvents: true,
+        showObjectChanges: true,
+        showBalanceChanges: true,
       },
     });
+    console.log(result)
+    return result
   }
