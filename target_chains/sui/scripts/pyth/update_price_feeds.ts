32b3da885793e428519f5e17713f1d00f5de76c8
optke3 <optke3@gmail.com>
2023-05-06 01:38:23 +0000

    update update_price_feeds demo to auto-retrieve price info object ids

--- target_chains/sui/scripts/pyth/update_price_feeds.ts
+++ target_chains/sui/scripts/pyth/update_price_feeds.ts
@@ -5,6 +5,7 @@
 /// https://pyth.network/developers/price-feed-ids#pyth-evm-testnet
 import dotenv from "dotenv";
 import { PriceServiceConnection } from "@pythnetwork/price-service-client";
+import fs from "fs";
 
 import {
   RawSigner,
@@ -19,10 +20,14 @@ dotenv.config({ path: "~/.env" });
 
 import { REGISTRY, NETWORK } from "../registry";
 
-// Network dependent settings.
-let network = NETWORK.TESTNET; // <= NOTE: Update this when changing network
-const walletPrivateKey = process.env.SUI_TESTNET; // <= NOTE: Update this when changing network
-const price_connection_url = "https://xc-mainnet.pyth.network"; // <= NOTE: Update this when changing network
+// ================== Network dependent settings ==================
+let network = NETWORK.TESTNET;
+const walletPrivateKey = process.env.SUI_TESTNET;
+const price_connection_url = "https://xc-testnet.pyth.network";
+// ================================================================
+
+const PATH_TO_PRICE_ID_TO_OBJECT_MAP = "./generated/price_id_to_object_id.testnet.json"
+
 const registry = REGISTRY[network];
 const provider = new JsonRpcProvider(
   new Connection({ fullnode: registry["RPC_URL"] })
@@ -44,31 +49,29 @@ async function main() {
   );
   console.log(wallet.getAddress());
 
-  // update a single price feed
-  const _deployer =
-    "0xf0bd738bf137f4cb0601713e95bb9307a5efe8d27f34c9e34f419e17b4e74099"; // testnet
-
   // Price feeds IDs of feeds we are interested in updating.
-  const _price_feed_ids = [
+  // For a full list of testnet price feed ids, see:
+  // https://pyth.network/developers/price-feed-ids#pyth-evm-testnet
+  const price_feed_ids = [
     "0x8b62866fcd3a25ff9118506444e9fe5171e67c61a049f4b4fdacdbc31ae862bb",
     "0x0e60a64dcbd660e87a08eb2cc95e8d84d1126fd7354d377b3fc5730352f4b8b2",
     "0x651071f8c7ab2321b6bdd3bc79b94a50841a92a6e065f9e3b8b9926a8fb5a5d1",
+    // INSERT YOUR PRICE FEED ID HERE!
   ];
 
   // Batch attestation VAA for price feed IDs above.
-  const vaas = [
-    "AQAAAAABAEV7/X8bjY4dpfT1vfNn6yxAvQUGDw5+cmUcs0qKddnPOc76sAVA76QqcRJlGGNJFOyRSVxEtejb5ufqIx99BrkBZFWgEwAAAAAAGqJ4OdZBsHdDwMtfaMUfjNMdLAdivsANxvzSVDPvGrW2AAAAAAFZmE0BUDJXSAADAAEAAQIABQCdbQGa55bF3PAaj/dFvylmM/7ksc5l+Znf6Dh2cLQS04AZeG8x6F01mM7Nq4ELJ4feD8IsLPmLTxb7Hlv1Z6CkMQAAAAACxD7OAAAAAAABS2L////4AAAAAALD2yUAAAAAAAC9PQEAAAABAAAAAgAAAABkVaATAAAAAGRVoBMAAAAAZFWgEgAAAAACxBwjAAAAAAABKLcAAAAAZFWgCqmeZwvnpS94J0Hz0p7r3Wf8PzJV4TpTWn+5piu04/t3i2KGb806Jf+RGFBkROn+UXHmfGGgSfS0/azbwxroYrsAAAAACRX/7wAAAAAAAVK3////+AAAAAAJE7maAAAAAAAB6zMBAAAAAQAAAAIAAAAAZFWgEwAAAABkVaATAAAAAGRVoBIAAAAACRX/7wAAAAAAAVK3AAAAAGRVoAl+Yw45hhOUDjYjAl2plplx0/MalDbZej7hWqfLWEavS/cxEtStJv03zxjKmz9+bxpWK1lly8JuRIilQFUvGAcuAAAAAAAFEN8AAAAAAAAB8/////gAAAAAAAUSKgAAAAAAAAMHAQAAAAEAAAACAAAAAGRVoBMAAAAAZFWgEwAAAABkVaASAAAAAAAFEN8AAAAAAAAB8wAAAABkVaAKD+5d3AdqlvGg7TGQdX5DE6SESe17memDpYdZE5ORzgVs1cmcbYbfkosFYvJmF93vQAgY+JBzkfMIeWXmt4awswAAAAAALezCAAAAAAAACqr////4AAAAAAAt7kYAAAAAAAAHkAEAAAABAAAAAgAAAABkVaATAAAAAGRVoBMAAAAAZFWgEgAAAAAALezCAAAAAAAACqoAAAAAZFWgCqlo4j3CfwMhZ7N8DkPz3CwQlXKtkOtI8gyvkMqhah5m6wDh+FhUnhIDT/iAt1kkVqcbQjeq9K6xbmPNm2i6TX4AAAAAAO38OAAAAAAAALYI////+AAAAAAA7b1wAAAAAAAA3X0BAAAAAQAAAAIAAAAAZFWgEwAAAABkVaATAAAAAGRVoBIAAAAAAO38OAAAAAAAALYIAAAAAGRVoAs=",
-    "AQAAAAABAL2tDgf95A1ZZ6txlEL7q0i3Z4TU4roV1R9GXgclvIjYTAjjHRs/L/7gYHT3PZbZtijeDF4SA/NWRdOm7Ou3Jh0AZFWgEgAAAAAAGqJ4OdZBsHdDwMtfaMUfjNMdLAdivsANxvzSVDPvGrW2AAAAAAFZmCcBUDJXSAADAAEAAQIABQCd3ssQGAihxJoCHqR91Gm6hxzC1qaZ9WHNw9ZBE8+3JSzgf5gw3WObdv08NB8yPQoEVi9ELuW6se/mmiZdWrm5SgAAAAASpRb2AAAAAAAHfSr////4AAAAABKnkVAAAAAAAAYUoAEAAAABAAAAAgAAAABkVaASAAAAAGRVoBIAAAAAZFWgDQAAAAASpPNaAAAAAAAHoMYAAAAAZFWgCpgvJzHwmiUBF9/OljYDtO98hjoXAKGzTbL2eEfaGAB07Cv2owh9IizpYK/PdbQrlphdWAqA/NTEXHal1eVxPMcAAAAAEIt9KQAAAAAADryf////+AAAAAAQjvkSAAAAAAANJGMBAAAAAQAAAAIAAAAAZFWgEgAAAABkVaASAAAAAGRVoA0AAAAAEIt9KQAAAAAADryfAAAAAGRVoArgvDRLEovOfekzHp3Oq8h2QavUrI/L2wFq/JJbqR6pdsMjubPm3Qmp2TCQMiaJ8PtEu9kp6HAgKP8d3YOLFNQhAAAAAAVc3xQAAAAAAAE9Hf////gAAAAABV5cXAAAAAAAAt83AQAAAAEAAAACAAAAAGRVoBIAAAAAZFWgEgAAAABkVaAOAAAAAAVc3xQAAAAAAAE9HQAAAABkVaAKN4h5fPE+mjHvS5A7tgO6f+dLq4kFpk+q6kT4CFhV98sOYKZNy9Zg6HoI6yzJXo2E0RJv1zVNN3s/xXMDUvS4sgAAAAAROYh0AAAAAAAGXEz////4AAAAABE6ZQQAAAAAAAT1XAEAAAABAAAAAgAAAABkVaASAAAAAGRVoBIAAAAAZFWgDQAAAAAROYh0AAAAAAAGXEwAAAAAZFWgCZYEjXggMUU+2EGxbUKzbCBTU3iULMwlQU5r80iFSllYeFijwqbNRdm2cTuzYfZlrr0puVH+/pd7tNuvDkYGqm8AAAAAEcOf7AAAAAAABsZF////+AAAAAARxww0AAAAAAAF6zcBAAAAAQAAAAIAAAAAZFWgEgAAAABkVaASAAAAAGRVoA0AAAAAEcOf7AAAAAAABsZFAAAAAGRVoAk=",
-    "AQAAAAABAEEJ0wacl2Xe33BLe7OtE35aHtvaQGuwRB6j92QYHx21RqT5sw8eFP1/l/TAQN3f6zrJMZWu5kkS+pmGR4Vn4t4AZFWgGAAAAAAAGqJ4OdZBsHdDwMtfaMUfjNMdLAdivsANxvzSVDPvGrW2AAAAAAFZmIoBUDJXSAADAAEAAQIABQCdQxzC/Q70r0vHyF//ri9j1Rsm0WIXloLRSa5hmxIhwAv8MJRn3vpLGYxrW9WcCNtLnfsn3bzDLzFWDyF7T/j8KwAAAC6Z97mTAAAAAAnuxGn////4AAAALlf1XtAAAAAACaYFYAEAAAABAAAAAgAAAABkVaAYAAAAAGRVoBcAAAAAZFWgFQAAAC6bvuq2AAAAAArRGrYAAAAAZFWgFfQqr4hMexRUiUFwvgqvHbObS3jTpWon/Um9iznvLDPXZRBx+MerIyG2vdO8eblKUIQakqbgZfnjuLmSao+1pdEAAAAwGwan8gAAAAAhJEgs////+AAAAC/cuqOQAAAAABnu+soBAAAAAQAAAAIAAAAAZFWgGAAAAABkVaAXAAAAAGRVoBUAAAAwGHAYOQAAAABOb34ZAAAAAGRVoBcYAesDgDrwJEUj7iqGw/J7Emq+iQTbS0WoKttf4hcItMqAum3DLgjQbxqohgEe7R13x3vp63YcwQ1yt9Ci/VemAAAALruXSnAAAAAABQWNkP////gAAAAudzafIAAAAAAE4btdAQAAAAEAAAACAAAAAGRVoBgAAAAAZFWgFwAAAABkVaAVAAAALr2FMz0AAAAABdksvAAAAABkVaAXfd8Ngq9THwrxCdXpzp7Ce6nwDp7oq3HJEq//oW1xWDa3q9Jadt2v/fhHIk8DGYzLknI/kLJCnPM/Duy5bjUqhgAAAC64Ff23AAAAACK8F3b////4AAAALnHJXrAAAAAAIgFtzgEAAAABAAAAAgAAAABkVaAYAAAAAGRVoBcAAAAAZFWgFQAAAC64Ff23AAAAACK8F3YAAAAAZFWgFdWlwvMOBr1vOOAcLEyM3XynwcEtR6czbkWfxttBcbrmYP1hstkOukfygVBaiIabZhM9ncWPIDsBn1qkfxs5ND4AAAAtI2bqGgAAAAAisDf5////+AAAAC0htMOYAAAAACKu/ZABAAAAAQAAAAIAAAAAZFWgGAAAAABkR/zyAAAAAGRH/PAAAAAtI2bqGgAAAAAisDf5AAAAAGRH/PI=",
-  ];
+  const vaas = await connection.getLatestVaas(price_feed_ids);
 
+  const price_feed_id_to_price_info_map = JSON.parse(fs.readFileSync(PATH_TO_PRICE_ID_TO_OBJECT_MAP, 'utf8'));
+  console.log(price_feed_id_to_price_info_map)
   // Price info objects corresponding to the price feeds we want to update.
-  const price_info_object_ids = [
-    "0x0d2fbbf69315c2bf139ef2db39fa9290944b714aaf651924be1206fa66a076e7",
-    "0xacfd46e1f0fccfe3b5b2c7959ab628f9543b590555286e6ed7b351bc66a18688",
-    "0x8e0d74c3364b0be0b805fab72974edb9872451737848cf6a28a948e5ade1c8ee",
-  ];
+  let price_info_object_ids = []
+  for (let id of price_feed_ids){
+    let sliced_id = id.slice(2) // removed 0x prefix
+    price_info_object_ids = price_info_object_ids.concat(price_feed_id_to_price_info_map[sliced_id])
+  }
 
+  console.log("price info objects to be updated: ", price_info_object_ids)
   update_price_feeds(wallet, registry, vaas, price_info_object_ids);
 }
 
