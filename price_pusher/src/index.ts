3fc996d6f91d1492cc4f0c491a12ff0db9e8468d
Dev Kalra <kalradev@outlook.com>
2023-05-15 21:14:28 +0530

    [price-pusher] support for aptos (#815)
    * aptos price listener
    
    * price pusher aptos
    
    * add comment
    
    * update package lock
    
    * remove eslint disable comments
    
    * bump version
    
    * npm i at root
    
    * update readme
    
    * address feedback
    
    * update readme
    
    * json fix

--- price_pusher/src/index.ts
+++ price_pusher/src/index.ts
@@ -3,10 +3,12 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import injective from "./injective/command";
 import evm from "./evm/command";
+import aptos from "./aptos/command";
 
 yargs(hideBin(process.argv))
   .config("config")
   .global("config")
   .command(evm)
   .command(injective)
+  .command(aptos)
   .help().argv;
