e7b6dabdd830e8b6a78c81601298529f2543b7d6
guibescos <59208140+guibescos@users.noreply.github.com>
2023-05-30 09:46:36 -0500

    Increase payload to 50MB (#848)

--- governance/xc_admin/packages/proposer_server/src/index.ts
+++ governance/xc_admin/packages/proposer_server/src/index.ts
@@ -43,7 +43,8 @@ const RPC_URLS: Record<Cluster | "localnet", string> = {
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.post("/api/propose", async (req: Request, res: Response) => {
   try {
