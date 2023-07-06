0d130a64c7d754d0be6f4829498ed6758f6c1f72
optke3 <optke3@gmail.com>
2023-07-06 20:27:06 +0000

    update some script imports

--- target_chains/sui/scripts/pyth/pyth_deploy.ts
+++ target_chains/sui/scripts/pyth/pyth_deploy.ts
@@ -8,7 +8,7 @@ import {
   JsonRpcProvider,
   Ed25519Keypair,
   Connection,
-} from "@optke3/sui.js";
+} from "@mysten/sui.js";
 import { execSync } from "child_process";
 
 import dotenv from "dotenv";
