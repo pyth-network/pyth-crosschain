ecfb9076b8af966140cc56e6f50da535bf699915
Amin Moghaddam <amin@pyth.network>
2023-07-31 14:19:25 +0100

    Add abi diff command and small documentation fix

--- target_chains/aptos/cli/src/commands/aptos.ts
+++ target_chains/aptos/cli/src/commands/aptos.ts
@@ -367,6 +367,52 @@ export const builder: (args: Argv<any>) => Argv<any> = (yargs) =>
         await executeTransaction(argv.network, txPayload);
       }
     )
+    .command(
+      "diff-abi <addr-1> <addr-2>",
+      "Finds the differences between the ABIs of two published packages, can be used to make sure that the upgrade will be backward compatible",
+      (_yargs) => {
+        return yargs
+          .positional("addr-1", {
+            type: "string",
+            required: true,
+          })
+          .positional("addr-2", {
+            type: "string",
+            required: true,
+          })
+          .option("network", NETWORK_OPTION);
+      },
+      async (argv) => {
+        const endpoint = networks.get(argv.network)!.endpoint;
+        const addr1 = argv["addr-1"];
+        const addr2 = argv["addr-2"];
+        const url = `${endpoint}/accounts/${addr1}/resource/0x1::code::PackageRegistry`;
+        const response = await (await fetch(url)).json();
+        for (const module of response.data.packages[0].modules) {
+          const moduleName = module.name;
+          const addr1Module = `${endpoint}/accounts/${addr1}/module/${moduleName}`;
+          const addr2Module = `${endpoint}/accounts/${addr2}/module/${moduleName}`;
+          const module1Response = await (await fetch(addr1Module)).text();
+          const module2Response = await (await fetch(addr2Module)).text();
+          const module1Stripped = module1Response.replace(
+            new RegExp(addr1, "g"),
+            "0x0"
+          );
+          const module2Stripped = module2Response.replace(
+            new RegExp(addr2, "g"),
+            "0x0"
+          );
+          if (
+            JSON.stringify(JSON.parse(module1Stripped).abi) !==
+            JSON.stringify(JSON.parse(module2Stripped).abi)
+          ) {
+            console.log(`Module ${moduleName} ABI changed`);
+          } else {
+            console.log(`Module ${moduleName} ABI not changed`);
+          }
+        }
+      }
+    )
     .demandCommand();
 
 function getSender(network: string) {
