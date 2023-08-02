26756f77f85d99223d5dde628492f9a53a83f533
Amin Moghaddam <amin@pyth.network>
2023-08-02 14:46:23 +0100

    Remove .env files per network and create a script that outputs the same file

---
+++ target_chains/ethereum/contracts/create-env.js
@@ -0,0 +1,33 @@
+/**
+ * This script is used to generate the .env file for a specific network.
+ * You can call it like this:
+ *  node create-env.js <contract-id>
+ */
+
+const { DefaultStore, EvmContract } = require("contract_manager");
+
+async function main() {
+  const contractId = process.argv[2];
+  const contract = DefaultStore.contracts[contractId];
+  if (!contract) {
+    throw new Error(`Contract ${contractId} not found`);
+  }
+  if (!(contract instanceof EvmContract)) {
+    throw new Error(`${contractId} is not an EVM contract`);
+  }
+  const migrationDir = (await (
+    await contract.getWormholeContract()
+  ).isWormholeReceiver())
+    ? "prod-receiver"
+    : "prod";
+  const cluster = contract.getChain().isMainnet() ? "mainnet" : "testnet";
+  console.log(
+    `MIGRATIONS_DIR=./migrations/${migrationDir}\n` +
+      `MIGRATIONS_NETWORK=${contract.getChain().getId()}\n` +
+      `WORMHOLE_CHAIN_NAME=${contract.getChain().wormholeChainName}\n` +
+      `CLUSTER=${cluster}\n` +
+      `VALID_TIME_PERIOD_SECONDS=${await contract.getValidTimePeriod()}`
+  );
+}
+
+main();
