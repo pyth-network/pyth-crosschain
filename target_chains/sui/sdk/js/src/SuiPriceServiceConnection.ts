12050e1d25f50b07bd726aa8e720c27f17192c95
optke3 <optke3@gmail.com>
2023-08-08 20:39:34 +0000

    simplify SuiPriceServiceConnection

--- target_chains/sui/sdk/js/src/SuiPriceServiceConnection.ts
+++ target_chains/sui/sdk/js/src/SuiPriceServiceConnection.ts
@@ -3,17 +3,7 @@ import {
   HexString,
 } from "@pythnetwork/price-service-client";
 import { Buffer } from "buffer";
-import axios from "axios";
 
-/**
- * There are two types of price update messages for Pyth: batch price attestation messages and accumulator messages.
- * The Sui contract supports both, but batch price attestations may be deprecated in the future. It is cheaper
- * to use accumulator messages to do price updates. Update data for these two kinds of messages are retrieved
- * from different endpoints.
- *
- * Examples:
- *
- */
 export class SuiPriceServiceConnection extends PriceServiceConnection {
   /**
    * Gets price update data (batch price attestation VAAs) which then can be submitted to the Pyth contract to update the prices.
