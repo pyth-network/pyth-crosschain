bf0868e0777ab41b0f698fe30f4700b9e620c127
Amin Moghaddam <amin@pyth.network>
2023-08-23 13:28:50 +0200

    Utility script to fetch fees on different contracts

---
+++ contract_manager/scripts/fetch_fees.ts
@@ -0,0 +1,35 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import {
+  AptosContract,
+  CosmWasmContract,
+  DefaultStore,
+  EvmContract,
+} from "../src";
+import { PriceServiceConnection } from "@pythnetwork/price-service-client";
+
+const parser = yargs(hideBin(process.argv))
+  .usage("Usage: $0")
+  .options({
+    testnet: {
+      type: "boolean",
+      default: false,
+      desc: "Fetch testnet contract fees instead of mainnet",
+    },
+  });
+
+async function main() {
+  const argv = await parser.argv;
+  for (const contract of Object.values(DefaultStore.contracts)) {
+    if (contract.getChain().isMainnet() === argv.testnet) continue;
+    if (
+      contract instanceof AptosContract ||
+      contract instanceof EvmContract ||
+      contract instanceof CosmWasmContract
+    ) {
+      console.log(`${contract.getId()} ${await contract.getTotalFee()}`);
+    }
+  }
+}
+
+main();
