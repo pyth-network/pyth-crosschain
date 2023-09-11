c8cebbf144132f7fbe7606a669d8e8ff83bd8707
Guillermo Bescos <g.bescos@yahoo.com>
2023-09-11 16:34:03 +0100

    Update string logic

--- governance/xc_admin/packages/xc_admin_frontend/utils/lamportsToSol.ts
+++ governance/xc_admin/packages/xc_admin_frontend/utils/lamportsToSol.ts
@@ -1,5 +1,13 @@
-import { LAMPORTS_PER_SOL } from '@solana/web3.js'
+const TRAILING_ZEROS = new RegExp(/\.?0+$/)
+const SOL_DECIMALS = 9
 
 export function lamportsToSol(lamports: bigint): string {
-  return (lamports / BigInt(LAMPORTS_PER_SOL)).toString()
+  const padded = lamports.toString().padStart(SOL_DECIMALS + 1, '0')
+  return (
+    padded.slice(0, padded.length - SOL_DECIMALS) +
+    ('.' + padded.slice(padded.length - SOL_DECIMALS)).replace(
+      TRAILING_ZEROS,
+      ''
+    )
+  )
 }
