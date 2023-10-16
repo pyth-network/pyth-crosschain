0383ee00aae21bc6b378413ac6a7ffa807691ec2
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2023-10-16 12:19:11 -0700

    more stuff

--- pyth-rng/src/config/generate.rs
+++ pyth-rng/src/config/generate.rs
@@ -11,12 +11,13 @@ pub struct GenerateOptions {
 
     /// Submit a randomness request to this provider
     #[arg(long = "provider")]
-    #[arg(env = "PYTH_PROVIDER")]
     #[arg(default_value = "0x368397bDc956b4F23847bE244f350Bde4615F25E")]
     pub provider: String,
 
     #[arg(long = "url")]
-    #[arg(env = "PYTH_PROVIDER_URL")]
     #[arg(default_value = super::DEFAULT_HTTP_ADDR)]
     pub url: String,
+
+    #[arg(short = 'b')]
+    pub blockhash: bool
 }
--- pyth-rng/src/ethereum/generate.rs
+++ pyth-rng/src/ethereum/generate.rs
@@ -16,12 +16,11 @@ pub async fn generate(opts: &GenerateOptions) -> Result<(), Box<dyn Error>> {
     let user_randomness = rand::random::<[u8; 32]>();
     let provider = opts.provider.parse::<Address>()?;
 
-    // FIXME: blockchas
-    // Request a random number
-    let sequence_number = contract.request_wrapper(&provider, &user_randomness, false).await?;
+    // Request a random number on the contract
+    let sequence_number = contract.request_wrapper(&provider, &user_randomness, opts.blockhash).await?;
     println!("Requested the random number with sequence number {:#?}", sequence_number);
 
-    // Get the committed value from the hash chain
+    // Get the committed value from the provider
     let client = reqwest::Client::new();
     let request_url = client.get(format!("{}/v1/revelation", &opts.url)).query(&[("sequence", sequence_number)]).build()?;
     let resp = client.execute(request_url)
@@ -32,6 +31,7 @@ pub async fn generate(opts: &GenerateOptions) -> Result<(), Box<dyn Error>> {
     println!("Retrieved the provider's random value. Server response: {:#?}", resp);
     let provider_randomness = resp.value;
 
+    // Submit the provider's and our values to the contract to reveal the random number.
     let random_value = contract.reveal_wrapper(&provider, sequence_number, &user_randomness, &provider_randomness).await?;
 
     println!("Generated random number: {:#?}", random_value);
