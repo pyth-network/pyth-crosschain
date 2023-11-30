f8ad8924755f25e45b887750338219d3d641d734
0xfirefist <kalradev@outlook.com>
2023-11-30 15:25:18 +0530

    minimise the amount of code

--- target_chains/ethereum/contracts/forge-test/utils/EntropyTestContracts/EntropyDifferentMagic.t.sol
+++ target_chains/ethereum/contracts/forge-test/utils/EntropyTestContracts/EntropyDifferentMagic.t.sol
@@ -6,57 +6,22 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@pythnetwork/entropy-sdk-solidity/EntropyErrors.sol";
 
-import "../../../contracts/entropy/EntropyGovernance.sol";
-import "../../../contracts/entropy/Entropy.sol";
-
 contract EntropyDifferentMagic is
     Initializable,
     OwnableUpgradeable,
-    UUPSUpgradeable,
-    Entropy,
-    EntropyGovernance
+    UUPSUpgradeable
 {
-    event ContractUpgraded(
-        address oldImplementation,
-        address newImplementation
-    );
-
-    // The contract will have an owner and an admin
-    // The owner will have all the power over it.
-    // The admin can set some config parameters only.
-    function initialize(
-        address owner,
-        address admin,
-        uint128 pythFeeInWei,
-        address defaultProvider,
-        bool prefillRequestStorage
-    ) public initializer {
+    function initialize() public initializer {
         __Ownable_init();
         __UUPSUpgradeable_init();
-
-        Entropy._initialize(
-            admin,
-            pythFeeInWei,
-            defaultProvider,
-            prefillRequestStorage
-        );
-
-        // We need to transfer the ownership from deployer to the new owner
-        transferOwnership(owner);
     }
 
-    /// Ensures the contract cannot be uninitialized and taken over.
-    /// @custom:oz-upgrades-unsafe-allow constructor
+    // /// Ensures the contract cannot be uninitialized and taken over.
+    // /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
-    // Only allow the owner to upgrade the proxy to a new implementation.
+    // // Only allow the owner to upgrade the proxy to a new implementation.
     function _authorizeUpgrade(address) internal override onlyOwner {}
-
-    // There are some actions which both and admin and owner can perform
-    function _authoriseAdminAction() internal view override {
-        if (msg.sender != owner() && msg.sender != _state.admin)
-            revert EntropyErrors.Unauthorized();
-    }
 
     function magicCheck() internal view {
         // Calling a method using `this.<method>` will cause a contract call that will use
