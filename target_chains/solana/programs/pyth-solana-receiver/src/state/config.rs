f88875e5f61fa47926cdda490fbedcb29ecf521c
Guillermo Bescos <g.bescos@yahoo.com>
2023-12-13 19:07:23 +0700

    Update size of config

--- target_chains/solana/programs/pyth-solana-receiver/src/state/config.rs
+++ target_chains/solana/programs/pyth-solana-receiver/src/state/config.rs
@@ -19,5 +19,46 @@ pub struct DataSource {
 }
 
 impl Config {
-    pub const LEN: usize = 1000; // PLACEHOLDER LEN, DECIDE HOW BIG WE WANT TO MAKE IT
+    pub const LEN: usize = 370; // This is two times the current size of a Config account with 2 data sources, to leave space for more fields
+}
+
+#[cfg(test)]
+pub mod tests {
+    use {
+        super::DataSource,
+        crate::state::config::Config,
+        anchor_lang::{
+            AnchorSerialize,
+            Discriminator,
+        },
+        solana_program::pubkey::Pubkey,
+    };
+
+    #[test]
+    fn check_size() {
+        let test_config = Config {
+            governance_authority:          Pubkey::new_unique(),
+            target_governance_authority:   Some(Pubkey::new_unique()),
+            wormhole:                      Pubkey::new_unique(),
+            valid_data_sources:            vec![
+                DataSource {
+                    chain:   1,
+                    emitter: Pubkey::new_unique(),
+                },
+                DataSource {
+                    chain:   2,
+                    emitter: Pubkey::new_unique(),
+                },
+            ],
+            single_update_fee_in_lamports: 0,
+        };
+
+        assert_eq!(
+            test_config.try_to_vec().unwrap().len(),
+            32 + 1 + 32 + 32 + 4 + 1 + 33 + 1 + 33 + 8
+        );
+        assert!(
+            Config::discriminator().len() + test_config.try_to_vec().unwrap().len() <= Config::LEN
+        );
+    }
 }
