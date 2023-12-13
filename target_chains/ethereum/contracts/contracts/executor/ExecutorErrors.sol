b0fd53bad5f9664964bb858be27927e8f2a87f17
0xfirefist <kalradev@outlook.com>
2023-12-13 16:41:57 +0530

    better comment

--- target_chains/ethereum/contracts/contracts/executor/ExecutorErrors.sol
+++ target_chains/ethereum/contracts/contracts/executor/ExecutorErrors.sol
@@ -14,6 +14,6 @@ library ExecutorErrors {
     error DeserializationError();
     // The message is not intended for this contract.
     error InvalidGovernanceTarget();
-    // The message is not intended for a valid contract address.
+    // The target address for the contract call is not a contract
     error InvalidContractTarget();
 }
