abcac59ad2c515849e29c59d5f6291edc17334a6
Guillermo Bescos <g.bescos@yahoo.com>
2023-12-13 17:40:54 +0700

    More cleanup

--- target_chains/solana/cli/src/cli.rs
+++ target_chains/solana/cli/src/cli.rs
@@ -10,7 +10,23 @@ use clap::{
 )]
 pub struct Cli {
     #[clap(subcommand)]
-    pub action: Action,
+    pub action:   Action,
+    #[clap(
+        short = 'k',
+        long,
+        default_value = "~/.config/solana/id.json",
+        help = "Keypair of the payer of transactions"
+    )]
+    pub keypair:  String,
+    #[clap(
+        short = 'u',
+        long,
+        default_value = "http://localhost:8899",
+        help = "RPC endpoint of the solana"
+    )]
+    pub url:      String,
+    #[clap(short = 'w', long, help = "Wormhole address")]
+    pub wormhole: String,
 }
 
 #[derive(Subcommand, Debug)]
@@ -18,36 +34,7 @@ pub enum Action {
     #[clap(about = "Verify, post and receive the price VAA on solana")]
     PostAndReceiveVAA {
         #[clap(short = 'v', long, help = "Price VAA from Pythnet")]
-        vaa:     String,
-        #[clap(
-            short = 'k',
-            long,
-            default_value = "~/.config/solana/id.json",
-            help = "Keypair of the payer of transactions"
-        )]
-        keypair: String,
-        #[clap(
-            short = 'u',
-            long,
-            default_value = "http://localhost:8899",
-            help = "RPC endpoint of the solana"
-        )]
-        url:     String,
+        vaa: String,
     },
-    Initialize {
-        #[clap(
-            short = 'k',
-            long,
-            default_value = "~/.config/solana/id.json",
-            help = "Keypair of the payer of transactions"
-        )]
-        keypair: String,
-        #[clap(
-            short = 'u',
-            long,
-            default_value = "http://localhost:8899",
-            help = "RPC endpoint of the solana"
-        )]
-        url:     String,
-    },
+    Initialize {},
 }
