a1fe742030b17a4a07301efa604b94beeb44d37b
0xfirefist <kalradev@outlook.com>
2024-01-05 01:23:26 +0530

    call contract with value

--- target_chains/ethereum/contracts/forge-test/Executor.t.sol
+++ target_chains/ethereum/contracts/forge-test/Executor.t.sol
@@ -41,7 +41,8 @@ contract ExecutorTest is Test, WormholeTestUtils {
     function testExecute(
         address callAddress,
         bytes memory callData,
-        uint64 sequence
+        uint64 sequence,
+        uint value
     ) internal returns (bytes memory vaa) {
         bytes memory payload = abi.encodePacked(
             uint32(0x5054474d),
@@ -50,6 +51,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
             CHAIN_ID,
             address(executor),
             callAddress,
+            value,
             callData
         );
 
@@ -66,7 +68,8 @@ contract ExecutorTest is Test, WormholeTestUtils {
     }
 
     function getTestUpgradeVaa(
-        address newImplementation
+        address newImplementation,
+        uint value
     ) internal returns (bytes memory vaa) {
         bytes memory payload = abi.encodePacked(
             uint32(0x5054474d),
@@ -75,6 +78,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
             CHAIN_ID,
             address(executor),
             address(executor),
+            value,
             abi.encodeWithSelector(
                 ExecutorUpgradable.upgradeTo.selector,
                 newImplementation
@@ -101,19 +105,19 @@ contract ExecutorTest is Test, WormholeTestUtils {
     }
 
     function testUpgradeCallSucceedsForContractWithCorrectMagic() public {
-        bytes memory vaa = getTestUpgradeVaa(address(executor2));
+        bytes memory vaa = getTestUpgradeVaa(address(executor2), 0);
         executor.execute(vaa);
     }
 
     function testUpgradeCallFailsForNotUUPSContract() public {
-        bytes memory vaa = getTestUpgradeVaa(address(callable));
+        bytes memory vaa = getTestUpgradeVaa(address(callable), 0);
 
         vm.expectRevert("ERC1967Upgrade: new implementation is not UUPS");
         executor.execute(vaa);
     }
 
     function testUpgradeCallFailsForInvalidMagic() public {
-        bytes memory vaa = getTestUpgradeVaa(address(executorInvalidMagic));
+        bytes memory vaa = getTestUpgradeVaa(address(executorInvalidMagic), 0);
 
         vm.expectRevert(ExecutorErrors.InvalidMagicValue.selector);
         executor.execute(vaa);
@@ -127,12 +131,66 @@ contract ExecutorTest is Test, WormholeTestUtils {
         testExecute(
             address(callable),
             abi.encodeWithSelector(ICallable.foo.selector),
-            1
+            1,
+            0
         );
         assertEq(callable.fooCount(), c + 1);
         assertEq(callable.lastCaller(), address(executor));
         // Sanity check to make sure the check above is meaningful.
         assert(address(executor) != address(this));
+    }
+
+    function testCallWithValueSucceeds() public {
+        callable.reset();
+
+        uint32 c = callable.fooCount();
+        assertEq(callable.lastCaller(), address(bytes20(0)));
+
+        uint value = 1;
+        vm.deal(address(executor), value);
+
+        testExecute(
+            address(callable),
+            abi.encodeWithSelector(ICallable.fooPayable.selector),
+            1,
+            value
+        );
+        assertEq(callable.fooCount(), c + 1);
+        assertEq(callable.lastCaller(), address(executor));
+        // Sanity check to make sure the check above is meaningful.
+        assert(address(executor) != address(this));
+    }
+
+    function testCallWithValueInsufficientBalance() public {
+        callable.reset();
+
+        assertEq(callable.lastCaller(), address(bytes20(0)));
+
+        uint value = 5;
+        vm.deal(address(executor), 1);
+
+        bytes memory payload = abi.encodePacked(
+            uint32(0x5054474d),
+            PythGovernanceInstructions.GovernanceModule.EvmExecutor,
+            Executor.ExecutorAction.Execute,
+            CHAIN_ID,
+            address(executor),
+            address(callable),
+            value,
+            abi.encodeWithSelector(ICallable.fooPayable.selector)
+        );
+
+        bytes memory vaa = generateVaa(
+            uint32(block.timestamp),
+            OWNER_CHAIN_ID,
+            OWNER_EMITTER,
+            1,
+            payload,
+            NUM_SIGNERS
+        );
+
+        vm.expectRevert();
+        executor.execute(vaa);
     }
 
     function testCallWithArgsSucceeds() public {
@@ -143,7 +201,29 @@ contract ExecutorTest is Test, WormholeTestUtils {
         testExecute(
             address(callable),
             abi.encodeWithSelector(ICallable.fooWithArgs.selector, 17),
-            1
+            1,
+            0
+        );
+        assertEq(callable.fooCount(), c + 17);
+        assertEq(callable.lastCaller(), address(executor));
+        // Sanity check to make sure the check above is meaningful.
+        assert(address(executor) != address(this));
+    }
+
+    function testCallWithArgsAndValueSucceeds() public {
+        callable.reset();
+
+        uint32 c = callable.fooCount();
+        assertEq(callable.lastCaller(), address(bytes20(0)));
+
+        uint value = 1;
+        vm.deal(address(executor), value);
+
+        testExecute(
+            address(callable),
+            abi.encodeWithSelector(ICallable.fooPayableWithArgs.selector, 17),
+            1,
+            value
         );
         assertEq(callable.fooCount(), c + 17);
         assertEq(callable.lastCaller(), address(executor));
@@ -156,7 +236,8 @@ contract ExecutorTest is Test, WormholeTestUtils {
         testExecute(
             address(callable),
             abi.encodeWithSelector(ICallable.foo.selector),
-            1
+            1,
+            0
         );
         assertEq(callable.fooCount(), c + 1);
     }
@@ -178,6 +259,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
                 CHAIN_ID,
                 address(executor),
                 address(callable),
+                uint(0),
                 abi.encodeWithSelector(ICallable.foo.selector)
             );
 
@@ -205,6 +287,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
             CHAIN_ID,
             address(executor),
             address(callable),
+            uint(0),
             abi.encodeWithSelector(ICallable.foo.selector)
         );
 
@@ -229,6 +312,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
             CHAIN_ID,
             address(executor),
             address(callable),
+            uint(0),
             abi.encodeWithSelector(ICallable.foo.selector)
         );
 
@@ -249,7 +333,8 @@ contract ExecutorTest is Test, WormholeTestUtils {
         testExecute(
             address(callable),
             abi.encodeWithSelector(ICallable.foo.selector),
-            3
+            3,
+            0
         );
 
         bytes memory payload = abi.encodePacked(
@@ -259,6 +344,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
             CHAIN_ID,
             address(executor),
             address(callable),
+            uint(0),
             abi.encodeWithSelector(ICallable.foo.selector)
         );
 
@@ -278,7 +364,8 @@ contract ExecutorTest is Test, WormholeTestUtils {
         testExecute(
             address(callable),
             abi.encodeWithSelector(ICallable.foo.selector),
-            4
+            4,
+            0
         );
         assertEq(callable.fooCount(), 1);
     }
@@ -291,6 +378,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
             CHAIN_ID,
             address(executor),
             address(callable),
+            uint(0),
             abi.encodeWithSelector(ICallable.foo.selector)
         );
 
@@ -320,6 +408,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
             uint16(3),
             address(executor),
             address(callable),
+            uint(0),
             abi.encodeWithSelector(ICallable.foo.selector)
         );
 
@@ -344,6 +433,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
             CHAIN_ID,
             address(0x1),
             address(callable),
+            uint(0),
             abi.encodeWithSelector(ICallable.foo.selector)
         );
 
@@ -368,6 +458,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
             CHAIN_ID,
             address(executor),
             address(callable),
+            uint(0),
             abi.encodeWithSelector(ICallable.foo.selector)
         );
 
@@ -392,6 +483,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
             CHAIN_ID,
             address(executor),
             address(callable),
+            uint(0),
             abi.encodeWithSelector(ICallable.reverts.selector)
         );
 
@@ -416,6 +508,7 @@ contract ExecutorTest is Test, WormholeTestUtils {
             CHAIN_ID,
             address(executor),
             address(100),
+            uint(0),
             abi.encodeWithSelector(ICallable.foo.selector)
         );
 
@@ -436,7 +529,11 @@ contract ExecutorTest is Test, WormholeTestUtils {
 interface ICallable {
     function foo() external;
 
+    function fooPayable() external payable;
+
     function fooWithArgs(uint32 inc) external;
+
+    function fooPayableWithArgs(uint32 inc) external payable;
 
     function reverts() external;
 
@@ -459,7 +556,17 @@ contract TestCallable is ICallable {
         lastCaller = msg.sender;
     }
 
+    function fooPayable() external payable override {
+        fooCount += 1;
+        lastCaller = msg.sender;
+    }
+
     function fooWithArgs(uint32 inc) external override {
+        fooCount += inc;
+        lastCaller = msg.sender;
+    }
+
+    function fooPayableWithArgs(uint32 inc) external payable override {
         fooCount += inc;
         lastCaller = msg.sender;
     }
