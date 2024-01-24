7db31aa7b59762e4b218dc65deffc5d91790296f
Dev Kalra <kalradev@outlook.com>
2024-01-24 17:38:39 +0530

    [governance] add chain ids (#1241)
    * add chain ids
    
    * pre commit fix

--- governance/xc_admin/packages/xc_admin_common/src/chains.ts
+++ governance/xc_admin/packages/xc_admin_common/src/chains.ts
@@ -118,6 +118,9 @@ export const RECEIVER_CHAINS = {
   blast_s2_testnet: 50047,
   hedera_testnet: 50048,
   filecoin_calibration: 50049, // Filecoin testnet
+  lightlink_pegasus_testnet: 50050,
+  sei_evm_devnet: 50051,
+  fantom_sonic_testnet: 50052,
 };
 
 // If there is any overlapping value the receiver chain will replace the wormhole
