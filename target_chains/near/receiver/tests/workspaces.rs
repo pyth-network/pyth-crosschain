995d2fe592b9219074eddc3ff1edff87bf6d9dc1
Guillermo Bescos <g.bescos@yahoo.com>
2024-01-24 18:38:12 +0000

    Checkpoint

--- target_chains/near/receiver/tests/workspaces.rs
+++ target_chains/near/receiver/tests/workspaces.rs
@@ -24,13 +24,10 @@ use {
         accumulators::{
             merkle::MerkleTree,
             Accumulator,
-        },
-        hashers::keccak256_160::Keccak160,
-        messages::{
+        }, hashers::keccak256_160::Keccak160, messages::{
             Message,
             PriceFeedMessage,
-        },
-        wire::{
+        }, test_utils::create_accumulator_message, wire::{
             to_vec,
             v1::{
                 AccumulatorUpdateData,
@@ -41,7 +38,7 @@ use {
                 WormholePayload,
             },
             PrefixedVec,
-        },
+        }
     },
     serde_json::json,
     std::io::{
@@ -986,68 +983,68 @@ async fn test_accumulator_updates() {
         Message::PriceFeedMessage(msg)
     }
 
-    fn create_accumulator_message_from_updates(
-        price_updates: Vec<MerklePriceUpdate>,
-        tree: MerkleTree<Keccak160>,
-        emitter_address: [u8; 32],
-        emitter_chain: u16,
-    ) -> Vec<u8> {
-        let mut root_hash = [0u8; 20];
-        root_hash.copy_from_slice(&to_vec::<_, BigEndian>(&tree.root).unwrap()[..20]);
-        let wormhole_message = WormholeMessage::new(WormholePayload::Merkle(WormholeMerkleRoot {
-            slot:      0,
-            ring_size: 0,
-            root:      root_hash,
-        }));
+    // fn create_accumulator_message_from_updates(
+    //     price_updates: Vec<MerklePriceUpdate>,
+    //     tree: MerkleTree<Keccak160>,
+    //     emitter_address: [u8; 32],
+    //     emitter_chain: u16,
+    // ) -> Vec<u8> {
+    //     let mut root_hash = [0u8; 20];
+    //     root_hash.copy_from_slice(&to_vec::<_, BigEndian>(&tree.root).unwrap()[..20]);
+    //     let wormhole_message = WormholeMessage::new(WormholePayload::Merkle(WormholeMerkleRoot {
+    //         slot:      0,
+    //         ring_size: 0,
+    //         root:      root_hash,
+    //     }));
 
-        let vaa = wormhole_sdk::Vaa {
-            emitter_chain: emitter_chain.into(),
-            emitter_address: wormhole_sdk::Address(emitter_address),
-            sequence: 2,
-            payload: (),
-            ..Default::default()
-        };
+    //     let vaa = wormhole_sdk::Vaa {
+    //         emitter_chain: emitter_chain.into(),
+    //         emitter_address: wormhole_sdk::Address(emitter_address),
+    //         sequence: 2,
+    //         payload: (),
+    //         ..Default::default()
+    //     };
 
-        let vaa = {
-            let mut cur = Cursor::new(Vec::new());
-            serde_wormhole::to_writer(&mut cur, &vaa).expect("Failed to serialize VAA");
-            cur.write_all(&to_vec::<_, BigEndian>(&wormhole_message).unwrap())
-                .expect("Failed to write Payload");
-            cur.into_inner()
-        };
+    //     let vaa = {
+    //         let mut cur = Cursor::new(Vec::new());
+    //         serde_wormhole::to_writer(&mut cur, &vaa).expect("Failed to serialize VAA");
+    //         cur.write_all(&to_vec::<_, BigEndian>(&wormhole_message).unwrap())
+    //             .expect("Failed to write Payload");
+    //         cur.into_inner()
+    //     };
 
-        let accumulator_update_data = AccumulatorUpdateData::new(Proof::WormholeMerkle {
-            vaa:     PrefixedVec::from(vaa),
-            updates: price_updates,
-        });
+    //     let accumulator_update_data = AccumulatorUpdateData::new(Proof::WormholeMerkle {
+    //         vaa:     PrefixedVec::from(vaa),
+    //         updates: price_updates,
+    //     });
 
-        to_vec::<_, BigEndian>(&accumulator_update_data).unwrap()
-    }
+    //     to_vec::<_, BigEndian>(&accumulator_update_data).unwrap()
+    // }
 
-    fn create_accumulator_message(all_feeds: &[Message], updates: &[Message]) -> Vec<u8> {
-        let all_feeds_bytes: Vec<_> = all_feeds
-            .iter()
-            .map(|f| to_vec::<_, BigEndian>(f).unwrap())
-            .collect();
-        let all_feeds_bytes_refs: Vec<_> = all_feeds_bytes.iter().map(|f| f.as_ref()).collect();
-        let tree = MerkleTree::<Keccak160>::new(all_feeds_bytes_refs.as_slice()).unwrap();
-        let mut price_updates: Vec<MerklePriceUpdate> = vec![];
-        for update in updates {
-            let proof = tree
-                .prove(&to_vec::<_, BigEndian>(update).unwrap())
-                .unwrap();
-            price_updates.push(MerklePriceUpdate {
-                message: PrefixedVec::from(to_vec::<_, BigEndian>(update).unwrap()),
-                proof,
-            });
-        }
-        create_accumulator_message_from_updates(
-            price_updates,
-            tree,
-            [1; 32],
-            wormhole_sdk::Chain::Any.into(),
-        )
-    }
+    // fn create_accumulator_message(all_feeds: &[Message], updates: &[Message]) -> Vec<u8> {
+    //     let all_feeds_bytes: Vec<_> = all_feeds
+    //         .iter()
+    //         .map(|f| to_vec::<_, BigEndian>(f).unwrap())
+    //         .collect();
+    //     let all_feeds_bytes_refs: Vec<_> = all_feeds_bytes.iter().map(|f| f.as_ref()).collect();
+    //     let tree = MerkleTree::<Keccak160>::new(all_feeds_bytes_refs.as_slice()).unwrap();
+    //     let mut price_updates: Vec<MerklePriceUpdate> = vec![];
+    //     for update in updates {
+    //         let proof = tree
+    //             .prove(&to_vec::<_, BigEndian>(update).unwrap())
+    //             .unwrap();
+    //         price_updates.push(MerklePriceUpdate {
+    //             message: PrefixedVec::from(to_vec::<_, BigEndian>(update).unwrap()),
+    //             proof,
+    //         });
+    //     }
+    //     create_accumulator_message_from_updates(
+    //         price_updates,
+    //         tree,
+    //         [1; 32],
+    //         wormhole_sdk::Chain::Any.into(),
+    //     )
+    // }
 
     let (_, contract, _) = initialize_chain().await;
 
