b4a883f159c360efc150ea0e90dfb1fbb0cdedff
Dev Kalra <kalradev@outlook.com>
2024-01-26 14:34:30 +0530

    uint 256 (#1250)

--- governance/xc_admin/packages/xc_admin_common/src/governance_payload/BufferLayoutExt.ts
+++ governance/xc_admin/packages/xc_admin_common/src/governance_payload/BufferLayoutExt.ts
@@ -1,25 +1,20 @@
 import { Layout } from "@solana/buffer-layout";
+import { toBigIntBE, toBufferBE } from "bigint-buffer";
 
-export class UInt64BE extends Layout<bigint> {
+export class UIntBE extends Layout<bigint> {
+  // span is the number of bytes to read
   constructor(span: number, property?: string) {
     super(span, property);
   }
 
-  // Note: we can not use read/writeBigUInt64BE because it is not supported in the browsers
   override decode(b: Uint8Array, offset?: number): bigint {
     let o = offset ?? 0;
     const buffer = Buffer.from(b.slice(o, o + this.span));
-    const hi32 = buffer.readUInt32BE();
-    const lo32 = buffer.readUInt32BE(4);
-    return BigInt(lo32) + (BigInt(hi32) << BigInt(32));
+    return toBigIntBE(buffer);
   }
 
   override encode(src: bigint, b: Uint8Array, offset?: number): number {
-    const buffer = Buffer.alloc(this.span);
-    const hi32 = Number(src >> BigInt(32));
-    const lo32 = Number(src & BigInt(0xffffffff));
-    buffer.writeUInt32BE(hi32, 0);
-    buffer.writeUInt32BE(lo32, 4);
+    const buffer = toBufferBE(src, this.span);
     b.set(buffer, offset);
     return this.span;
   }
@@ -45,8 +40,13 @@ export class HexBytes extends Layout<string> {
 }
 
 /** A big-endian u64, returned as a bigint. */
-export function u64be(property?: string | undefined): UInt64BE {
-  return new UInt64BE(8, property);
+export function u64be(property?: string | undefined): UIntBE {
+  return new UIntBE(8, property);
+}
+
+/** A big-endian u256, returned as a bigint. */
+export function u256be(property?: string | undefined): UIntBE {
+  return new UIntBE(32, property);
 }
 
 /** An array of numBytes bytes, returned as a hexadecimal string. */
