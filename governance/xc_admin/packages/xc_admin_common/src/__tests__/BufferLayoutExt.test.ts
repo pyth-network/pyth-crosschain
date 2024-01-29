b9564550b84200ae69f0ce0d9fb407794758c659
0xfirefist <kalradev@outlook.com>
2024-01-29 15:18:31 +0530

    remove unneccessary test

--- governance/xc_admin/packages/xc_admin_common/src/__tests__/BufferLayoutExt.test.ts
+++ governance/xc_admin/packages/xc_admin_common/src/__tests__/BufferLayoutExt.test.ts
@@ -1,21 +1,6 @@
 import fc from "fast-check";
 import { u64be } from "../governance_payload/BufferLayoutExt";
 
-test("Buffer layout extension fc test", (done) => {
-  const u64 = u64be();
-  const uint8Array = new Uint8Array([0, 0, 0, 0, 161, 189, 5, 128]);
-
-  expect(u64.decode(uint8Array)).toBe(2713519488n);
-
-  let encodedUint8Array = new Uint8Array(8);
-  u64.encode(2713519488n, encodedUint8Array);
-  expect(
-    Buffer.from(encodedUint8Array).equals(Buffer.from(uint8Array))
-  ).toBeTruthy();
-
-  done();
-});
-
 test("Buffer layout extension fc tests", (done) => {
   const u64 = u64be();
   fc.assert(
