e491fb95ca890b8d7fea888e6e814c358d967987
Guillermo Bescos <g.bescos@yahoo.com>
2024-01-30 14:41:34 +0000

    Add another test

--- target_chains/solana/programs/pyth-solana-receiver/src/error.rs
+++ target_chains/solana/programs/pyth-solana-receiver/src/error.rs
@@ -2,16 +2,9 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum ReceiverError {
+    // Pyth payload errors
     #[msg("The tuple emitter chain, emitter doesn't match one of the valid data sources.")]
     InvalidDataSource,
-    #[msg("The posted VAA account has the wrong owner.")]
-    WrongVaaOwner,
-    #[msg("The posted VAA has wrong magic number.")]
-    PostedVaaHeaderWrongMagicNumber,
-    #[msg("An error occurred when deserializing the VAA.")]
-    DeserializeVaaFailed,
-    #[msg("An error occurred when deserializing the updates.")]
-    DeserializeUpdateFailed,
     #[msg("An error occurred when deserializing the message")]
     DeserializeMessageFailed,
     #[msg("Received an invalid wormhole message")]
@@ -20,19 +13,18 @@ pub enum ReceiverError {
     InvalidPriceUpdate,
     #[msg("This type of message is not supported currently")]
     UnsupportedMessageType,
-    #[msg("The signer is not authorized to perform this governance action")]
-    GovernanceAuthorityMismatch,
-    #[msg("The signer is not authorized to accept the governance authority")]
-    TargetGovernanceAuthorityMismatch,
-    #[msg("The governance authority needs to request a transfer first")]
-    NonexistentGovernanceAuthorityTransferRequest,
     #[msg("Funds are insufficient to pay the receiving fee")]
     InsufficientFunds,
-    #[msg("The number of guardian signatures is below the minimum")]
-    InsufficientGuardianSignatures,
+    // Wormhole contract encoded vaa error (from post_updates)
+    #[msg("The posted VAA account has the wrong owner.")]
+    WrongVaaOwner,
+    // Wormhole signatures verification errors (from post_updates_atomic)
+    #[msg("An error occurred when deserializing the VAA.")]
+    DeserializeVaaFailed,
     #[msg("The Guardian Set account doesn't match the PDA derivation")]
     InvalidGuardianSetPda,
-    // Wormhole errors
+    #[msg("The number of guardian signatures is below the minimum")]
+    InsufficientGuardianSignatures,
     #[msg("Invalid VAA version")]
     InvalidVaaVersion,
     #[msg("Guardian set version in the VAA doesn't match the guardian set passed")]
@@ -47,4 +39,11 @@ pub enum ReceiverError {
     InvalidGuardianKeyRecovery,
     #[msg("The guardian set account is owned by the wrong program")]
     WrongGuardianSetOwner,
+    // Governance errors
+    #[msg("The signer is not authorized to perform this governance action")]
+    GovernanceAuthorityMismatch,
+    #[msg("The signer is not authorized to accept the governance authority")]
+    TargetGovernanceAuthorityMismatch,
+    #[msg("The governance authority needs to request a transfer first")]
+    NonexistentGovernanceAuthorityTransferRequest,
 }
--- target_chains/solana/programs/pyth-solana-receiver/tests/test_post_updates.rs
+++ target_chains/solana/programs/pyth-solana-receiver/tests/test_post_updates.rs
@@ -3,7 +3,9 @@ use {
         setup_pyth_receiver,
         ProgramTestFixtures,
     },
+    program_simulator::into_transation_error,
     pyth_solana_receiver::{
+        error::ReceiverError,
         instruction::PostUpdates,
         sdk::deserialize_accumulator_update_data,
         state::price_update::{
@@ -18,6 +20,7 @@ use {
             create_dummy_price_feed_message,
         },
     },
+    solana_program::pubkey::Pubkey,
     solana_sdk::{
         signature::Keypair,
         signer::Signer,
@@ -103,5 +106,39 @@ async fn test_post_updates() {
     assert_eq!(
         Message::PriceFeedMessage(price_update_account.price_message),
         feed_2
+    );
+}
+
+#[tokio::test]
+async fn test_post_updates_wrong_vaa() {
+    let feed_1 = create_dummy_price_feed_message(100);
+    let feed_2 = create_dummy_price_feed_message(200);
+    let message = create_accumulator_message(&[feed_1, feed_2], &[feed_1, feed_2], false);
+    let (vaa, merkle_price_updates) = deserialize_accumulator_update_data(message).unwrap();
+
+    let ProgramTestFixtures {
+        mut program_simulator,
+        encoded_vaa_addresses: _,
+    } = setup_pyth_receiver(vec![serde_wormhole::from_slice(&vaa).unwrap()]).await;
+
+    let poster = program_simulator.get_funded_keypair().await.unwrap();
+    let price_update_keypair = Keypair::new();
+
+    assert_eq!(
+        program_simulator
+            .process_ix(
+                PostUpdates::populate(
+                    poster.pubkey(),
+                    Pubkey::new_unique(),
+                    price_update_keypair.pubkey(),
+                    merkle_price_updates[0].clone(),
+                ),
+                &vec![&poster, &price_update_keypair],
+                None,
+            )
+            .await
+            .unwrap_err()
+            .unwrap(),
+        into_transation_error(ReceiverError::WrongVaaOwner)
     );
 }
