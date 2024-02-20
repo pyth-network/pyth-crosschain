dc687d108874d4d09fea26857a95ebb642d01b71
Guillermo Bescos <g.bescos@yahoo.com>
2024-02-20 15:46:03 +0000

    Checkpoint

--- target_chains/solana/sdk/js/src/index.ts
+++ target_chains/solana/sdk/js/src/index.ts
@@ -1,17 +1,16 @@
 import { AnchorProvider, Program, Wallet } from "@coral-xyz/anchor";
 import { Connection } from "@solana/web3.js";
-import {
-  PythSolanaReceiver,
-  IDL as PythSolanaReceiverIdl,
-} from "../../../idl/pyth_solana_receiver";
+import { PythSolanaReceiver } from "./idl/pyth_solana_receiver";
+import Idl from "./idl/pyth_solana_receiver.json";
 import {
   WormholeCoreBridgeSolana,
   IDL as WormholeCoreBridgeSolanaIdl,
-} from "../../../idl/wormhole_core_bridge_solana";
+} from "./idl/wormhole_core_bridge_solana";
 import {
   DEFAULT_RECEIVER_PROGRAM_ID,
   DEFAULT_WORMHOLE_PROGRAM_ID,
 } from "./address";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
 
 export class PythSolanaReceiverConnection {
   readonly connection: Connection;
@@ -31,7 +30,7 @@ export class PythSolanaReceiverConnection {
     this.wallet = wallet;
     this.provider = new AnchorProvider(this.connection, this.wallet, {});
     this.receiver = new Program<PythSolanaReceiver>(
-      PythSolanaReceiverIdl as PythSolanaReceiver,
+      Idl as PythSolanaReceiver,
       DEFAULT_RECEIVER_PROGRAM_ID,
       this.provider
     );
@@ -40,5 +39,11 @@ export class PythSolanaReceiverConnection {
       DEFAULT_WORMHOLE_PROGRAM_ID,
       this.provider
     );
+  }
+
+  async postPriceUpdate(vaa: string): Promise<PublicKey> {
+    const vaa2 = Buffer.from(vaa, "base64");
+    console.log(vaa2.toString("hex"));
+    return SystemProgram.programId;
   }
 }
