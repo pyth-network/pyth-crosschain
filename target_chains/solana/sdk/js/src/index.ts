d8757d52842e44e772d7c546fbe53e9dc3abe7ee
Guillermo Bescos <g.bescos@yahoo.com>
2024-02-20 16:57:19 +0000

    Cleanup

--- target_chains/solana/sdk/js/src/index.ts
+++ target_chains/solana/sdk/js/src/index.ts
@@ -1,5 +1,5 @@
 import { AnchorProvider, Program, Wallet } from "@coral-xyz/anchor";
-import { Connection } from "@solana/web3.js";
+import { Connection, TransactionInstruction } from "@solana/web3.js";
 import { PythSolanaReceiver } from "./idl/pyth_solana_receiver";
 import Idl from "./idl/pyth_solana_receiver.json";
 import {
@@ -10,7 +10,9 @@ import {
   DEFAULT_RECEIVER_PROGRAM_ID,
   DEFAULT_WORMHOLE_PROGRAM_ID,
 } from "./address";
-import { PublicKey, SystemProgram } from "@solana/web3.js";
+import { PublicKey, Keypair } from "@solana/web3.js";
+import { parseAccumulatorUpdateData } from "@pythnetwork/price-service-sdk";
+import { VAA_SPLIT_INDEX, VAA_START } from "./constants";
 
 export class PythSolanaReceiverConnection {
   readonly connection: Connection;
@@ -42,8 +44,31 @@ export class PythSolanaReceiverConnection {
   }
 
   async postPriceUpdate(vaa: string): Promise<PublicKey> {
-    const vaa2 = Buffer.from(vaa, "base64");
-    console.log(vaa2.toString("hex"));
-    return SystemProgram.programId;
+    const accumulatorUpdateData = parseAccumulatorUpdateData(
+      Buffer.from(vaa, "base64")
+    );
+
+    const encodedVaaKeypair = new Keypair();
+    const encodedVaaSize = accumulatorUpdateData.vaa.length + VAA_START;
+
+    const firstTransactionInstructions: TransactionInstruction[] = [
+      await this.wormhole.account.encodedVaa.createInstruction(
+        encodedVaaKeypair,
+        encodedVaaSize
+      ),
+    ];
+
+    await this.wormhole.methods
+      .writeEncodedVaa({
+        index: 0,
+        data: accumulatorUpdateData.vaa.subarray(0, VAA_SPLIT_INDEX),
+      })
+      .accounts({
+        draftVaa: encodedVaaKeypair.publicKey,
+      })
+      .preInstructions(firstTransactionInstructions)
+      .rpc();
+
+    return encodedVaaKeypair.publicKey;
   }
 }
