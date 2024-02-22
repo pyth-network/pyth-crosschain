d5a3df6531ef3eb823ff3c5da88c8172c58e2cbb
Guillermo Bescos <g.bescos@yahoo.com>
2024-02-22 13:41:37 +0000

    Add errors

--- target_chains/solana/pyth_solana_receiver_state/src/error.rs
+++ target_chains/solana/pyth_solana_receiver_state/src/error.rs
@@ -3,7 +3,7 @@ use anchor_lang::error_code;
 #[error_code]
 pub enum GetPriceError {
     #[msg("This price feed update's age exceeds the requested maximum age")]
-    PriceTooOld = 10000, // Big number to avoid conflicts with the SDK user's error codes
+    PriceTooOld = 10000, // Big number to avoid conflicts with the SDK user's program error codes
     #[msg("The price feed update doesn't match the requested feed id")]
     MismatchedFeedId,
     #[msg("This price feed update has a lower verification level than the one requested")]
