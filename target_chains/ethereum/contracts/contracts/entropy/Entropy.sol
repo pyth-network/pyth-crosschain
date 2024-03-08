fc1d251f2d215c0f3e8c511e54d445cc44e1f950
0xfirefist <kalradev@outlook.com>
2024-03-08 16:25:44 +0530

    remove gas limit comment

--- target_chains/ethereum/contracts/contracts/entropy/Entropy.sol
+++ target_chains/ethereum/contracts/contracts/entropy/Entropy.sol
@@ -160,7 +160,7 @@ abstract contract Entropy is IEntropy, EntropyState {
         providerInfo.accruedFeesInWei -= amount;
 
         // Interaction with an external contract or token transfer
-        (bool sent, ) = msg.sender.call{value: amount}("");
+        (bool sent, ) = msg.sender.call{ value: amount }("");
         require(sent, "withdrawal to msg.sender failed");
     }
 
@@ -351,7 +351,6 @@ abstract contract Entropy is IEntropy, EntropyState {
             bytes32(uint256(0))
         );
 
-        // TODO: add some gas limit
         IEntropyConsumer(callAddress).entropyCallback(
             sequenceNumber,
             randomNumber
