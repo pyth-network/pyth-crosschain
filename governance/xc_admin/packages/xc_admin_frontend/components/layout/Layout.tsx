f247a78a928e3f8264c2ad8e13fb56259b53a721
Daniel Chew <cctdaniel@outlook.com>
2024-03-12 12:52:19 +0900

    fix footer placement

--- governance/xc_admin/packages/xc_admin_frontend/components/layout/Layout.tsx
+++ governance/xc_admin/packages/xc_admin_frontend/components/layout/Layout.tsx
@@ -4,9 +4,9 @@ import Header from './Header'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative overflow-hidden">
+    <div className="flex flex-col min-h-screen relative overflow-hidden">
       <Header />
-      <main>{children}</main>
+      <main className="flex-grow">{children}</main>
       <Footer />
     </div>
   )
