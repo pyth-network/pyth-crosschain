ff5b3634bcab8607e5807160b2f046ea4cd8b2da
Ali Behjati <bahjatia@gmail.com>
2024-03-21 13:20:24 +0100

    chore(target_chains/solana): add verifiable build script
    This change adds a Dockerfile based on Anchor's default verifiable
    build dockerfile and a script to build the program using that
    Dockerfile. The script also prints the sha256sum of the program which
    can be used to verify on-chain deployments.

---
+++ target_chains/solana/scripts/build_verifiable_program.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+set -euo pipefail
+
+# Root of the repository
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+
+echo "Building the image for the receiver program"
+docker build --platform linux/x86_64 -t solana-receiver-builder -f $REPO_ROOT/target_chains/solana/Dockerfile $REPO_ROOT
+
+echo "Building the receiver program"
+docker run --platform linux/x86_64 --rm -v $REPO_ROOT/target_chains/solana/artifacts:/artifacts solana-receiver-builder
+
+echo "Successfully built the receiver program."
+echo "The artifacts are available at $REPO_ROOT/target_chains/solana/artifacts"
+
+CHECKSUM=$(sha256sum $REPO_ROOT/target_chains/solana/artifacts/pyth_solana_receiver.so | awk '{print $1}')
+echo "sha256sum of the pyth_solana_receiver program: $CHECKSUM"
