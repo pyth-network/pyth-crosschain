8c445100e6d799b0dce0d5a759552b8a191f3aea
Guillermo Bescos <g.bescos@yahoo.com>
2024-04-01 16:50:17 +0100

    Checkpoint

---
+++ price_pusher/src/solana/command.ts
@@ -0,0 +1,63 @@
+import { Options } from "yargs";
+import * as options from "../options";
+import { readPriceConfigFile } from "../price-config";
+import { PriceServiceConnection } from "@pythnetwork/price-service-client";
+import { PythPriceListener } from "../pyth-price-listener";
+
+export default {
+  command: "solana",
+  describe: "run price pusher for solana",
+  builder: {
+    endpoint: {
+      description: "Solana RPC API endpoint",
+      type: "string",
+      required: true,
+    } as Options,
+    "keypair-file": {
+      description: "Path to keypair file",
+      type: "string",
+      required: true,
+    } as Options,
+    ...options.priceConfigFile,
+    ...options.priceServiceEndpoint,
+    ...options.pythContractAddress,
+    ...options.pollingFrequency,
+    ...options.pushingFrequency,
+  },
+  handler: function (argv: any) {
+    const {
+      endpoint,
+      keypairFile,
+      priceConfigFile,
+      priceServiceEndpoint,
+      pythContractAddress,
+      pushingFrequency,
+      pollingFrequency,
+    } = argv;
+
+    const priceConfigs = readPriceConfigFile(priceConfigFile);
+
+    const priceServiceConnection = new PriceServiceConnection(
+      priceServiceEndpoint,
+      {
+        logger: {
+          // Log only warnings and errors from the price service client
+          info: () => undefined,
+          warn: console.warn,
+          error: console.error,
+          debug: () => undefined,
+          trace: () => undefined,
+        },
+      }
+    );
+
+    const priceItems = priceConfigs.map(({ id, alias }) => ({ id, alias }));
+
+    const pythListener = new PythPriceListener(
+      priceServiceConnection,
+      priceItems
+    );
+
+    console.log("hello world");
+  },
+};
