71d1dab689490292c38ff6075ebc85f8159624c0
0xfirefist <kalradev@outlook.com>
2024-04-11 11:57:30 +0530

    refactor scripts

--- contract_manager/scripts/upgrade_evm_entropy_contracts.ts
+++ contract_manager/scripts/upgrade_evm_entropy_contracts.ts
@@ -9,19 +9,33 @@ import {
   makeCacheFunction,
 } from "./common";
 
-const CACHE_FILE = ".cache-upgrade-evm-entropy-contract";
-const runIfNotCached = makeCacheFunction(CACHE_FILE);
+const EXECUTOR_CACHE_FILE = ".cache-upgrade-evm-executor-contract";
+const ENTROPY_CACHE_FILE = ".cache-upgrade-evm-entropy-contract";
 
 const parser = yargs(hideBin(process.argv))
   .usage(
-    "Deploys a new EntropyUpgradable contract to a set of chains and creates a governance proposal for it.\n" +
-      `Uses a cache file (${CACHE_FILE}) to avoid deploying contracts twice\n` +
+    "Deploys a new Upgradeable contract for Executor or Entropy to a set of chains where Entropy is deployed and creates a governance proposal for it.\n" +
+      `Uses a cache file to avoid deploying contracts twice\n` +
       "Usage: $0 --chain <chain_1> --chain <chain_2> --private-key <private_key> --ops-key-path <ops_key_path> --std-output <std_output>"
   )
-  .options(COMMON_UPGRADE_OPTIONS);
+  .options({
+    ...COMMON_UPGRADE_OPTIONS,
+    "contract-type": {
+      type: "string",
+      choices: ["executor", "entropy"],
+      demandOption: true,
+    },
+  });
 
 async function main() {
   const argv = await parser.argv;
+  const cacheFile =
+    argv["contract-type"] === "executor"
+      ? EXECUTOR_CACHE_FILE
+      : ENTROPY_CACHE_FILE;
+
+  const runIfNotCached = makeCacheFunction(cacheFile);
+
   const selectedChains = getSelectedChains(argv);
 
   const vault =
@@ -29,11 +43,7 @@ async function main() {
       "mainnet-beta_FVQyHcooAtThJ83XFrNnv74BcinbRH3bRmfFamAHBfuj"
     ];
 
-  console.log("Using cache file", CACHE_FILE);
-  console.log(
-    "Upgrading on chains",
-    selectedChains.map((c) => c.getId())
-  );
+  console.log("Using cache file", cacheFile);
 
   const payloads: Buffer[] = [];
   for (const contract of Object.values(DefaultStore.entropy_contracts)) {
@@ -55,9 +65,11 @@ async function main() {
       console.log(
         `Deployed contract at ${address} on ${contract.chain.getId()}`
       );
-      const payload = await contract.generateUpgradeEntropyContractPayload(
-        address
-      );
+      const payload =
+        argv["contract-type"] === "executor"
+          ? await contract.generateUpgradeExecutorContractsPayload(address)
+          : await contract.generateUpgradeEntropyContractPayload(address);
+
       console.log(payload.toString("hex"));
       payloads.push(payload);
     }
@@ -67,8 +79,8 @@ async function main() {
   const wallet = await loadHotWallet(argv["ops-key-path"]);
   console.log("Using wallet ", wallet.publicKey.toBase58());
   await vault.connect(wallet);
-  const proposal = await vault.proposeWormholeMessage(payloads);
-  console.log("Proposal address", proposal.address.toBase58());
+  // const proposal = await vault.proposeWormholeMessage(payloads);
+  // console.log("Proposal address", proposal.address.toBase58());
 }
 
 main();
