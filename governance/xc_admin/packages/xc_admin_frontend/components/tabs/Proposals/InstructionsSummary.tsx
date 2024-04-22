b6f6d4f2ce0e78eb1fa97e35cca321b53e2ddbc8
keyvan <keyvankhademi@gmail.com>
2024-04-22 15:52:23 -0700

    feat: improve the proposal row ui

--- governance/xc_admin/packages/xc_admin_frontend/components/tabs/Proposals/InstructionsSummary.tsx
+++ governance/xc_admin/packages/xc_admin_frontend/components/tabs/Proposals/InstructionsSummary.tsx
@@ -1,13 +1,6 @@
 import { PythCluster } from '@pythnetwork/client'
-import { AccountMeta } from '@solana/web3.js'
-import {
-  ExecutePostedVaa,
-  MultisigInstruction,
-  MultisigParser,
-  PythGovernanceActionImpl,
-  WormholeMultisigInstruction,
-  SetDataSources,
-} from 'xc_admin_common'
+import { MultisigInstruction } from 'xc_admin_common'
+import { getInstructionsSummary } from './utils'
 
 export const InstructionsSummary = ({
   instructions,
@@ -16,33 +9,7 @@ export const InstructionsSummary = ({
   instructions: MultisigInstruction[]
   cluster: PythCluster
 }) => {
-  const instructionsCount = instructions.reduce((acc, instruction) => {
-    if (instruction instanceof WormholeMultisigInstruction) {
-      const governanceAction = instruction.governanceAction
-      if (governanceAction instanceof ExecutePostedVaa) {
-        const innerInstructions = governanceAction.instructions
-        innerInstructions.forEach((innerInstruction) => {
-          const multisigParser = MultisigParser.fromCluster(cluster)
-          const parsedInstruction = multisigParser.parseInstruction({
-            programId: innerInstruction.programId,
-            data: innerInstruction.data as Buffer,
-            keys: innerInstruction.keys as AccountMeta[],
-          })
-          acc[parsedInstruction.name] = (acc[parsedInstruction.name] ?? 0) + 1
-        })
-      } else if (governanceAction instanceof PythGovernanceActionImpl) {
-        acc[governanceAction.action] = (acc[governanceAction.action] ?? 0) + 1
-      } else if (governanceAction instanceof SetDataSources) {
-        acc[governanceAction.actionName] =
-          (acc[governanceAction.actionName] ?? 0) + 1
-      } else {
-        acc['unknown'] = (acc['unknown'] ?? 0) + 1
-      }
-    } else {
-      acc[instruction.name] = (acc[instruction.name] ?? 0) + 1
-    }
-    return acc
-  }, {} as Record<string, number>)
+  const instructionsCount = getInstructionsSummary({ instructions, cluster })
 
   return (
     <div className="space-y-4">
