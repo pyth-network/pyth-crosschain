c2846a7ac546ae0341712c8598c9042a665d7e1a
keyvan <keyvankhademi@gmail.com>
2024-04-22 12:33:36 -0700

    fix: revert back the cluster default

--- governance/xc_admin/packages/xc_admin_frontend/contexts/ClusterContext.tsx
+++ governance/xc_admin/packages/xc_admin_frontend/contexts/ClusterContext.tsx
@@ -1,7 +1,7 @@
 import { PythCluster } from '@pythnetwork/client/lib/cluster'
 import { ReactNode, createContext, useMemo, useState } from 'react'
 
-export const DEFAULT_CLUSTER: PythCluster = 'pythtest-conformance'
+export const DEFAULT_CLUSTER: PythCluster = 'mainnet-beta'
 
 export const ClusterContext = createContext<{
   cluster: PythCluster
