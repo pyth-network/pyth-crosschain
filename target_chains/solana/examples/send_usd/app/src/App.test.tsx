7b7d50a72ee20fb9769e6cb5e631e80fb046a288
keyvan <keyvankhademi@gmail.com>
2024-04-23 15:25:27 -0700

    fix: test

--- target_chains/solana/examples/send_usd/app/src/App.test.tsx
+++ target_chains/solana/examples/send_usd/app/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Click to send a 1 usd to the hard-coded account/i);
   expect(linkElement).toBeInTheDocument();
 });
