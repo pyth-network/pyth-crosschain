1d9f7e3fcd28249fbbc130b84e673c1f1dede06e
0xfirefist <kalradev@outlook.com>
2024-04-30 20:05:03 +0530

    deploy price feed and entropy to taiko

--- governance/xc_admin/packages/xc_admin_common/src/chains.ts
+++ governance/xc_admin/packages/xc_admin_common/src/chains.ts
@@ -145,14 +145,14 @@ export const RECEIVER_CHAINS = {
   boba_sepolia: 50068,
   astar_zkyoto_testnet: 50069,
   xion_testnet: 50070,
-  olive_testnet: 50071,
+  taiko_hekla: 50071,
 };
 
 // If there is any overlapping value the receiver chain will replace the wormhole
 // value and that is an expected behavior.
 export const CHAINS = { ...WORMHOLE_CHAINS, ...RECEIVER_CHAINS };
 export declare type ChainName = keyof typeof CHAINS;
-export declare type ChainId = typeof CHAINS[ChainName];
+export declare type ChainId = (typeof CHAINS)[ChainName];
 
 export function toChainId(chainName: ChainName): ChainId {
   return CHAINS[chainName];
