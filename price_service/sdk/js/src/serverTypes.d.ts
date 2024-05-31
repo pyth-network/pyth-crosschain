cda6b2cd9654d33db26ec7b64cfc126b63315f54
Daniel Chew <cctdaniel@outlook.com>
2024-05-31 19:01:54 +0900

    precommit

--- price_service/sdk/js/src/serverTypes.d.ts
+++ price_service/sdk/js/src/serverTypes.d.ts
@@ -3,7 +3,6 @@
  * Do not make direct changes to the file.
  */
 
-
 export interface paths {
   "/api/get_price_feed": {
     /**
@@ -273,7 +272,6 @@ export type $defs = Record<string, never>;
 export type external = Record<string, never>;
 
 export interface operations {
-
   /**
    * **Deprecated: use /v2/updates/price/{publish_time} instead**
    * @deprecated
