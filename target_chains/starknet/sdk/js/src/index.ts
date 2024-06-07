8dedc6d8ade556f92ad88af37b304d6cdea7a8b6
Pavel Strakhov <ri@idzaaus.org>
2024-06-07 15:11:15 +0100

    feat(target_chains/starknet): add JS SDK

---
+++ target_chains/starknet/sdk/js/src/index.ts
@@ -0,0 +1,50 @@
+/** ABI of the Pyth contract. Pass it as the first argument of `new Contract()` when using starknet-js. */
+export { default as PYTH_ABI } from "./abi/pyth.json";
+/** ABI of the ERC20 contract. Pass it as the first argument of `new Contract()` when using starknet-js. */
+export { default as ERC20_ABI } from "./abi/erc20.json";
+
+/** Address of the ERC20 contract that Pyth uses to accept fees in STRK. */
+export const STRK_TOKEN_ADDRESS =
+  "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d";
+
+/** Address of the ERC20 contract that Pyth uses to accept fees in ETH. */
+export const ETH_TOKEN_ADDRESS =
+  "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
+
+/** Address of the Pyth contract on Starknet Sepolia (testnet). */
+export const PYTH_CONTRACT_ADDRESS_SEPOLIA =
+  "0x07f2b07b6b5365e7ee055bda4c0ecabd867e6d3ee298d73aea32b027667186d6";
+
+/** A byte array encoded in a format compatible with starknet-js and with the Pyth contract. */
+export class ByteBuffer {
+  num_last_bytes = 0;
+  data: string[] = [];
+
+  /** Create a `ByteBuffer` from an array of `bytes31`.
+   * Use `ByteBuffer::fromHex` to create `ByteBuffer` from HEX representation of binary data.
+   */
+  constructor(num_last_bytes: number, data: string[]) {
+    this.num_last_bytes = num_last_bytes;
+    this.data = data;
+  }
+
+  /** Create a `ByteBuffer` from HEX representation of binary data. */
+  public static fromHex(hexData: string): ByteBuffer {
+    const buffer = Buffer.from(hexData, "base64");
+
+    let pos = 0;
+    const data = [];
+    while (pos < buffer.length) {
+      if (pos + 31 <= buffer.length) {
+        const slice = buffer.subarray(pos, pos + 31);
+        data.push("0x" + slice.toString("hex"));
+      } else {
+        const slice = buffer.subarray(pos);
+        data.push("0x" + slice.toString("hex"));
+        return new ByteBuffer(buffer.length - pos, data);
+      }
+      pos += 31;
+    }
+    return new ByteBuffer(0, data);
+  }
+}
