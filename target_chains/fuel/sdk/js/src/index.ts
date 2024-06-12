5dd4f1e2eb8322116b9f236cc392e5dc559384d3
Daniel Chew <cctdaniel@outlook.com>
2024-06-12 22:50:00 +0900

    add fuel js sdk

---
+++ target_chains/fuel/sdk/js/src/index.ts
@@ -0,0 +1,12 @@
+import PYTH_CONTRACT_ABI from "./abi/pyth-contract-abi.json";
+
+/** Address of the Pyth contract on Fuel Sepolia (testnet). */
+export const PYTH_CONTRACT_ADDRESS_SEPOLIA =
+  "0xdf202803b7a452a1ed2e09291731dbac5ceb13d036de3a9a31f74e6057c909af";
+
+/** Asset ID of ETH on Fuel. */
+export const FUEL_ETH_ASSET_ID =
+  "0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07";
+
+export { PYTH_CONTRACT_ABI };
+export * from "./types";
