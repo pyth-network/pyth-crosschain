1f71eae438615a82906007faaf3f039b101ae14f
Ali Behjati <bahjatia@gmail.com>
2024-06-25 13:11:00 +0200

    refactor(appts/price_pusher): fix warnings

--- apps/price_pusher/src/sui/sui.ts
+++ apps/price_pusher/src/sui/sui.ts
@@ -112,12 +112,6 @@ export class SuiPricePusher implements IPricePusher {
     private readonly provider: SuiClient,
     private logger: Logger,
     private priceServiceConnection: PriceServiceConnection,
-    private pythPackageId: string,
-    private pythStateId: string,
-    private wormholePackageId: string,
-    private wormholeStateId: string,
-    endpoint: string,
-    keypair: Ed25519Keypair,
     private gasBudget: number,
     private gasPool: SuiObjectRef[],
     private pythClient: SuiPythClient
@@ -180,14 +174,6 @@ export class SuiPricePusher implements IPricePusher {
     }
 
     const provider = new SuiClient({ url: endpoint });
-    const pythPackageId = await SuiPricePusher.getPackageId(
-      provider,
-      pythStateId
-    );
-    const wormholePackageId = await SuiPricePusher.getPackageId(
-      provider,
-      wormholeStateId
-    );
 
     const gasPool = await SuiPricePusher.initializeGasPool(
       keypair,
@@ -208,12 +194,6 @@ export class SuiPricePusher implements IPricePusher {
       provider,
       logger,
       priceServiceConnection,
-      pythPackageId,
-      pythStateId,
-      wormholePackageId,
-      wormholeStateId,
-      endpoint,
-      keypair,
       gasBudget,
       gasPool,
       pythClient
@@ -337,7 +317,7 @@ export class SuiPricePusher implements IPricePusher {
     ignoreGasObjects: string[],
     logger: Logger
   ): Promise<SuiObjectRef[]> {
-    const signerAddress = await signer.toSuiAddress();
+    const signerAddress = signer.toSuiAddress();
 
     if (ignoreGasObjects.length > 0) {
       logger.info(
