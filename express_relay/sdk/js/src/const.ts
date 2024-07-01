0b30a91defbd2d9d7ac17efdd11ed7b3fc5e3231
ani <anirudhtx@gmail.com>
2024-07-01 12:36:07 -0500

    remove development configs

--- express_relay/sdk/js/src/const.ts
+++ express_relay/sdk/js/src/const.ts
@@ -4,14 +4,6 @@ export const OPPORTUNITY_ADAPTER_CONFIGS: Record<
   string,
   OpportunityAdapterConfig
 > = {
-  development: {
-    chain_id: 31337,
-    opportunity_adapter_factory: "0x610178dA211FEF7D417bC0e6FeD39F05609AD788",
-    opportunity_adapter_init_bytecode_hash:
-      "0xd53b8e32ab2ecba07c3e3a17c3c5e492c62e2f7051b89e5154f52e6bfeb0e38f",
-    permit2: "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318",
-    weth: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
-  },
   op_sepolia: {
     chain_id: 11155420,
     opportunity_adapter_factory: "0xfA119693864b2F185742A409c66f04865c787754",
