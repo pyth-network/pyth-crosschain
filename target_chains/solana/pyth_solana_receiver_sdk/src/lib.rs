cbfa9d30b7ab0ac186252b580e5f573bdec8e046
mubariz <mubarizharoonmulpk@gmail.com>
2024-07-13 16:18:56 +0500

    changed anchor serailze/deserailize to borsh

--- target_chains/solana/pyth_solana_receiver_sdk/src/lib.rs
+++ target_chains/solana/pyth_solana_receiver_sdk/src/lib.rs
@@ -1,13 +1,8 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use pythnet_sdk::wire::v1::MerklePriceUpdate;
 use {
-    anchor_lang::{
-        declare_id,
-        prelude::*,
-    },
-    pythnet_sdk::wire::v1::MerklePriceUpdate,
-    solana_program::{
-        pubkey,
-        pubkey::Pubkey,
-    },
+    anchor_lang::{declare_id, prelude::*},
+    solana_program::{pubkey, pubkey::Pubkey},
 };
 
 pub mod config;
@@ -21,15 +16,15 @@ declare_id!("rec5EKMGg6MxZYaMdyBfgwp4d5rB9T1VQH5pJv5LtFJ");
 
 pub const PYTH_PUSH_ORACLE_ID: Pubkey = pubkey!("pythWSnswVUd12oZpeFP8e9CVaEqJg25g1Vtc2biRsT");
 
-#[derive(Debug, AnchorSerialize, AnchorDeserialize, Clone)]
-pub struct PostUpdateAtomicParams {
-    pub vaa:                 Vec<u8>,
-    pub merkle_price_update: MerklePriceUpdate,
-    pub treasury_id:         u8,
-}
-
-#[derive(Debug, AnchorSerialize, AnchorDeserialize, Clone)]
+#[derive(Debug, BorshSerialize, BorshDeserialize, Clone)]
 pub struct PostUpdateParams {
     pub merkle_price_update: MerklePriceUpdate,
-    pub treasury_id:         u8,
+    pub treasury_id: u8,
+}
+
+#[derive(Debug, BorshSerialize, BorshDeserialize, Clone)]
+pub struct PostUpdateAtomicParams {
+    pub vaa: Vec<u8>,
+    pub merkle_price_update: MerklePriceUpdate,
+    pub treasury_id: u8,
 }
