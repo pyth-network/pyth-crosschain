0db40907504579f2d8b6520fcbfb2e952c3ab170
Pavel Strakhov <ri@idzaaus.org>
2024-07-15 15:44:55 +0100

    chore(target_chains/starknet): add mainnet deployment info (#1770)
    * chore(target_chains/starknet): add mainnet deployment info
    
    * chore(target_chains/starknet): bump sdk version

--- target_chains/starknet/contracts/deploy/deploy.sh
+++ target_chains/starknet/contracts/deploy/deploy.sh
@@ -37,6 +37,16 @@ elif [ "${PYTH_DEPLOY_MODE}" == "sepolia" ]; then
 
     # ETH
     fee_token_address2=0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7
+elif [ "${PYTH_DEPLOY_MODE}" == "mainnet" ]; then
+    export STARKNET_RPC=https://starknet-mainnet.public.blastapi.io/rpc/v0_6
+
+    chain_id=60051 # starknet_mainnet
+
+    # STRK
+    fee_token_address1=0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d
+
+    # ETH
+    fee_token_address2=0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7
 else
     >&2 echo "Unsupported PYTH_DEPLOY_MODE"
     exit 1
