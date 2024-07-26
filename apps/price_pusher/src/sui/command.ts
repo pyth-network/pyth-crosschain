eea9b15cd3b9aec363738ce9505bebb3e11f7778
Goraniya Karan <karangoraniya@gmail.com>
2024-07-26 17:25:51 +0530

    feat: update Prettier settings to use double quotes 🚀

--- apps/price_pusher/src/sui/command.ts
+++ apps/price_pusher/src/sui/command.ts
@@ -1,66 +1,66 @@
-import { PriceServiceConnection } from '@pythnetwork/price-service-client';
-import * as options from '../options';
-import { readPriceConfigFile } from '../price-config';
-import fs from 'fs';
-import { PythPriceListener } from '../pyth-price-listener';
-import { Controller } from '../controller';
-import { Options } from 'yargs';
-import { SuiPriceListener, SuiPricePusher } from './sui';
-import { Ed25519Keypair } from '@mysten/sui/keypairs/ed25519';
-import pino from 'pino';
+import { PriceServiceConnection } from "@pythnetwork/price-service-client";
+import * as options from "../options";
+import { readPriceConfigFile } from "../price-config";
+import fs from "fs";
+import { PythPriceListener } from "../pyth-price-listener";
+import { Controller } from "../controller";
+import { Options } from "yargs";
+import { SuiPriceListener, SuiPricePusher } from "./sui";
+import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
+import pino from "pino";
 
 export default {
-  command: 'sui',
+  command: "sui",
   describe:
-    'Run price pusher for sui. Most of the arguments below are' +
+    "Run price pusher for sui. Most of the arguments below are" +
     "network specific, so there's one set of values for mainnet and" +
-    'another for testnet. See config.sui..sample.json for the ' +
-    'appropriate values for your network. ',
+    "another for testnet. See config.sui..sample.json for the " +
+    "appropriate values for your network. ",
   builder: {
     endpoint: {
       description:
-        'RPC endpoint URL for sui. The pusher will periodically' +
-        'poll for updates. The polling interval is configurable via the ' +
-        '`polling-frequency` command-line argument.',
-      type: 'string',
+        "RPC endpoint URL for sui. The pusher will periodically" +
+        "poll for updates. The polling interval is configurable via the " +
+        "`polling-frequency` command-line argument.",
+      type: "string",
       required: true,
     } as Options,
-    'pyth-state-id': {
+    "pyth-state-id": {
       description:
-        'Pyth State Id. Can be found here' +
-        'https://docs.pyth.network/documentation/pythnet-price-feeds/sui',
-      type: 'string',
+        "Pyth State Id. Can be found here" +
+        "https://docs.pyth.network/documentation/pythnet-price-feeds/sui",
+      type: "string",
       required: true,
     } as Options,
-    'wormhole-state-id': {
+    "wormhole-state-id": {
       description:
-        'Wormhole State Id. Can be found here' +
-        'https://docs.pyth.network/documentation/pythnet-price-feeds/sui',
-      type: 'string',
+        "Wormhole State Id. Can be found here" +
+        "https://docs.pyth.network/documentation/pythnet-price-feeds/sui",
+      type: "string",
       required: true,
     } as Options,
-    'num-gas-objects': {
-      description: 'Number of gas objects in the pool.',
-      type: 'number',
+    "num-gas-objects": {
+      description: "Number of gas objects in the pool.",
+      type: "number",
       required: true,
       default: 30,
     } as Options,
-    'ignore-gas-objects': {
+    "ignore-gas-objects": {
       description:
-        'Gas objects to ignore when merging gas objects on startup -- use this for locked objects.',
-      type: 'array',
+        "Gas objects to ignore when merging gas objects on startup -- use this for locked objects.",
+      type: "array",
       required: false,
       default: [],
     } as Options,
-    'gas-budget': {
-      description: 'Gas budget for each price update',
-      type: 'number',
+    "gas-budget": {
+      description: "Gas budget for each price update",
+      type: "number",
       required: true,
       default: 500_000_000,
     } as Options,
-    'account-index': {
-      description: 'Index of the account to use derived by the mnemonic',
-      type: 'number',
+    "account-index": {
+      description: "Index of the account to use derived by the mnemonic",
+      type: "number",
       required: true,
       default: 0,
     } as Options,
@@ -99,7 +99,7 @@ export default {
       priceServiceEndpoint,
       {
         logger: logger.child(
-          { module: 'PriceServiceConnection' },
+          { module: "PriceServiceConnection" },
           { level: priceServiceConnectionLogLevel }
         ),
         priceFeedRequestConfig: {
@@ -107,7 +107,7 @@ export default {
         },
       }
     );
-    const mnemonic = fs.readFileSync(mnemonicFile, 'utf-8').trim();
+    const mnemonic = fs.readFileSync(mnemonicFile, "utf-8").trim();
     const keypair = Ed25519Keypair.deriveKeypair(
       mnemonic,
       `m/44'/784'/${accountIndex}'/0'/0'`
@@ -123,7 +123,7 @@ export default {
     const pythListener = new PythPriceListener(
       priceServiceConnection,
       priceItems,
-      logger.child({ module: 'PythPriceListener' })
+      logger.child({ module: "PythPriceListener" })
     );
 
     const suiListener = new SuiPriceListener(
@@ -131,12 +131,12 @@ export default {
       wormholeStateId,
       endpoint,
       priceItems,
-      logger.child({ module: 'SuiPriceListener' }),
+      logger.child({ module: "SuiPriceListener" }),
       { pollingFrequency }
     );
     const suiPusher = await SuiPricePusher.createWithAutomaticGasPool(
       priceServiceConnection,
-      logger.child({ module: 'SuiPricePusher' }),
+      logger.child({ module: "SuiPricePusher" }),
       pythStateId,
       wormholeStateId,
       endpoint,
@@ -151,7 +151,7 @@ export default {
       pythListener,
       suiListener,
       suiPusher,
-      logger.child({ module: 'Controller' }, { level: controllerLogLevel }),
+      logger.child({ module: "Controller" }, { level: controllerLogLevel }),
       { pushingFrequency }
     );
 
