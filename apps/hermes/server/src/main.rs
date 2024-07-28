e1413f38b1ac2d327be23c2b6dc0798d6ae57fe6
aoikurokawa <ht.so.ik0102@gmail.com>
2024-07-28 11:15:03 +0900

    fix: update

--- apps/hermes/server/src/main.rs
+++ apps/hermes/server/src/main.rs
@@ -1,3 +1,6 @@
+#![feature(never_type)]
+#![feature(btree_cursors)]
+
 use {
     anyhow::Result,
     clap::{
@@ -5,17 +8,33 @@ use {
         Parser,
     },
     futures::future::join_all,
-    hermes::{
-        api,
-        config,
-        metrics_server,
-        network,
-        state,
-        EXIT,
-    },
+    lazy_static::lazy_static,
     std::io::IsTerminal,
-    tokio::spawn,
+    tokio::{
+        spawn,
+        sync::watch,
+    },
 };
+
+mod api;
+mod config;
+mod metrics_server;
+mod network;
+mod serde;
+mod state;
+
+lazy_static! {
+    /// A static exit flag to indicate to running threads that we're shutting down. This is used to
+    /// gracefully shut down the application.
+    ///
+    /// We make this global based on the fact the:
+    /// - The `Sender` side does not rely on any async runtime.
+    /// - Exit logic doesn't really require carefully threading this value through the app.
+    /// - The `Receiver` side of a watch channel performs the detection based on if the change
+    ///   happened after the subscribe, so it means all listeners should always be notified
+    ///   correctly.
+    pub static ref EXIT: watch::Sender<bool> = watch::channel(false).0;
+}
 
 /// Initialize the Application. This can be invoked either by real main, or by the Geyser plugin.
 #[tracing::instrument]
