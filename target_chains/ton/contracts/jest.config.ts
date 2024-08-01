c679bac7cd748658068af7a3c3bc271efb5e33af
Daniel Chew <cctdaniel@outlook.com>
2024-08-01 12:49:17 +0900

    precommit

--- target_chains/ton/contracts/jest.config.ts
+++ target_chains/ton/contracts/jest.config.ts
@@ -1,9 +1,9 @@
-import type { Config } from 'jest';
+import type { Config } from "jest";
 
 const config: Config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
 
 export default config;
