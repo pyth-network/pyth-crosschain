8e3e723fbac9a4c25a6a723447eaddf63068b0ab
Daniel Chew <cctdaniel@outlook.com>
2024-08-20 21:06:34 +0900

    fix bug in getParseAndVerifyWormholeVm

--- target_chains/ton/contracts/wrappers/WormholeTest.compile.ts
+++ target_chains/ton/contracts/wrappers/WormholeTest.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from "@ton/blueprint";
 
 export const compile: CompilerConfig = {
   lang: "func",
-  targets: ["contracts/tests/WormholeUnitTest.fc"],
+  targets: ["contracts/tests/WormholeTest.fc"],
 };
