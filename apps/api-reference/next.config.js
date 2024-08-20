3ff6c670c8678fc1e01b5643d2f55d63ee77022f
Aditya Arora <arora.aditya520@gmail.com>
2024-08-20 13:01:14 -0400

    (fix:api-reference) Change default method to getPriceNoOlderThan (#1833)

--- apps/api-reference/next.config.js
+++ apps/api-reference/next.config.js
@@ -12,7 +12,7 @@ export default {
   redirects: async () => [
     {
       source: "/price-feeds/evm",
-      destination: "/price-feeds/evm/parsePriceFeedUpdates",
+      destination: "/price-feeds/evm/getPriceNoOlderThan",
       permanent: false,
     },
   ],
