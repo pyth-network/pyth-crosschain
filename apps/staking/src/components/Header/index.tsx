7770e8eb5b03844bbc55752fab4b16e1d6a4f1f2
Connor Prussin <connor@prussin.net>
2024-09-12 17:03:13 -0700

    Revert "feat(staking) address some feedback items"

--- apps/staking/src/components/Header/index.tsx
+++ apps/staking/src/components/Header/index.tsx
@@ -1,9 +1,7 @@
 import clsx from "clsx";
 import type { HTMLAttributes } from "react";
 
-import { CurrentStakeAccount } from "./current-stake-account";
 import Logo from "./logo.svg";
-import Logomark from "./logomark.svg";
 import { MaxWidth } from "../MaxWidth";
 import { WalletButton } from "../WalletButton";
 
@@ -16,13 +14,9 @@ export const Header = ({
     {...props}
   >
     <div className="border-b border-neutral-600/50 bg-pythpurple-800 sm:border-x">
-      <MaxWidth className="flex h-16 items-center justify-between gap-2 sm:-mx-4">
-        <Logo className="hidden max-h-full py-4 text-pythpurple-100 sm:block" />
-        <Logomark className="max-h-full py-4 text-pythpurple-100 sm:hidden" />
-        <div className="flex flex-none flex-row items-stretch gap-8">
-          <CurrentStakeAccount />
-          <WalletButton className="flex-none" />
-        </div>
+      <MaxWidth className="flex h-16 items-center justify-between gap-8 sm:-mx-4">
+        <Logo className="max-h-full py-4 text-pythpurple-100" />
+        <WalletButton className="flex-none" />
       </MaxWidth>
     </div>
   </header>
