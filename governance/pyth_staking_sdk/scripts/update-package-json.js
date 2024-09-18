93f1221fed0316ed00e9a12ade1a2c910d891513
keyvan <keyvankhademi@gmail.com>
2024-09-18 13:05:07 -0700

    feat(staking): make sdk ready for release

---
+++ governance/pyth_staking_sdk/scripts/update-package-json.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the directory of the current module
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const distPackageJsonPath = path.join(__dirname, '..', 'dist', 'package.json');
+
+// Read the original package.json
+const packageJson = JSON.parse(fs.readFileSync(distPackageJsonPath, 'utf8'));
+
+// Modify the exports field
+packageJson.exports = {
+  ".": "./src/index.js"
+};
+
+// Write the modified package.json to the dist folder
+fs.writeFileSync(distPackageJsonPath, JSON.stringify(packageJson, null, 2));
