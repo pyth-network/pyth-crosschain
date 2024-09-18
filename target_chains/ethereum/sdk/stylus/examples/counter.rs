e0026609adc2a02d58415a1dd4f332673363e32e
Angry Coding <mailidanielifechukwu@gmail.com>
2024-09-18 15:01:28 +0000

    chore: init stylus project

---
+++ target_chains/ethereum/sdk/stylus/examples/counter.rs
@@ -0,0 +1,78 @@
+//! Example on how to interact with a deployed `stylus-hello-world` contract using defaults.
+//! This example uses ethers-rs to instantiate the contract using a Solidity ABI.
+//! Then, it attempts to check the current counter value, increment it via a tx,
+//! and check the value again. The deployed contract is fully written in Rust and compiled to WASM
+//! but with Stylus, it is accessible just as a normal Solidity smart contract is via an ABI.
+
+use ethers::{
+    middleware::SignerMiddleware,
+    prelude::abigen,
+    providers::{Http, Middleware, Provider},
+    signers::{LocalWallet, Signer},
+    types::Address,
+};
+use dotenv::dotenv;
+use eyre::eyre;
+use std::io::{BufRead, BufReader};
+use std::str::FromStr;
+use std::sync::Arc;
+
+/// Your private key file path.
+const PRIV_KEY_PATH: &str = "PRIV_KEY_PATH";
+
+/// Stylus RPC endpoint url.
+const RPC_URL: &str = "RPC_URL";
+
+/// Deployed pragram address.
+const STYLUS_CONTRACT_ADDRESS: &str = "STYLUS_CONTRACT_ADDRESS";
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    dotenv().ok();
+    let priv_key_path =
+        std::env::var(PRIV_KEY_PATH).map_err(|_| eyre!("No {} env var set", PRIV_KEY_PATH))?;
+    let rpc_url = std::env::var(RPC_URL).map_err(|_| eyre!("No {} env var set", RPC_URL))?;
+    let contract_address = std::env::var(STYLUS_CONTRACT_ADDRESS)
+        .map_err(|_| eyre!("No {} env var set", STYLUS_CONTRACT_ADDRESS))?;
+    abigen!(
+        Counter,
+        r#"[
+            function number() external view returns (uint256)
+            function setNumber(uint256 number) external
+            function increment() external
+        ]"#
+    );
+
+    let provider = Provider::<Http>::try_from(rpc_url)?;
+    let address: Address = contract_address.parse()?;
+
+    let privkey = read_secret_from_file(&priv_key_path)?;
+    let wallet = LocalWallet::from_str(&privkey)?;
+    let chain_id = provider.get_chainid().await?.as_u64();
+    let client = Arc::new(SignerMiddleware::new(
+        provider,
+        wallet.clone().with_chain_id(chain_id),
+    ));
+
+    let counter = Counter::new(address, client);
+    let num = counter.number().call().await;
+    println!("Counter number value = {:?}", num);
+
+    let pending = counter.increment();
+    if let Some(receipt) = pending.send().await?.await? {
+        println!("Receipt = {:?}", receipt);
+    }
+    println!("Successfully incremented counter via a tx");
+
+    let num = counter.number().call().await;
+    println!("New counter number value = {:?}", num);
+    Ok(())
+}
+
+fn read_secret_from_file(fpath: &str) -> eyre::Result<String> {
+    let f = std::fs::File::open(fpath)?;
+    let mut buf_reader = BufReader::new(f);
+    let mut secret = String::new();
+    buf_reader.read_line(&mut secret)?;
+    Ok(secret.trim().to_string())
+}
---
+++ target_chains/ethereum/sdk/stylus/src/lib.rs
@@ -0,0 +1,68 @@
+//!
+//! Stylus Hello World
+//!
+//! The following contract implements the Counter example from Foundry.
+//!
+//! ```
+//! contract Counter {
+//!     uint256 public number;
+//!     function setNumber(uint256 newNumber) public {
+//!         number = newNumber;
+//!     }
+//!     function increment() public {
+//!         number++;
+//!     }
+//! }
+//! ```
+//!
+//! The program is ABI-equivalent with Solidity, which means you can call it from both Solidity and Rust.
+//! To do this, run `cargo stylus export-abi`.
+//!
+//! Note: this code is a template-only and has not been audited.
+//!
+
+// Allow `cargo stylus export-abi` to generate a main function.
+#![cfg_attr(not(feature = "export-abi"), no_main)]
+extern crate alloc;
+
+/// Import items from the SDK. The prelude contains common traits and macros.
+use stylus_sdk::{alloy_primitives::U256, prelude::*};
+
+// Define some persistent storage using the Solidity ABI.
+// `Counter` will be the entrypoint.
+sol_storage! {
+    #[entrypoint]
+    pub struct Counter {
+        uint256 number;
+    }
+}
+
+/// Declare that `Counter` is a contract with the following external methods.
+#[public]
+impl Counter {
+    /// Gets the number from storage.
+    pub fn number(&self) -> U256 {
+        self.number.get()
+    }
+
+    /// Sets a number in storage to a user-specified value.
+    pub fn set_number(&mut self, new_number: U256) {
+        self.number.set(new_number);
+    }
+
+    /// Sets a number in storage to a user-specified value.
+    pub fn mul_number(&mut self, new_number: U256) {
+        self.number.set(new_number * self.number.get());
+    }
+
+    /// Sets a number in storage to a user-specified value.
+    pub fn add_number(&mut self, new_number: U256) {
+        self.number.set(new_number + self.number.get());
+    }
+
+    /// Increments `number` and updates its value in storage.
+    pub fn increment(&mut self) {
+        let number = self.number.get();
+        self.set_number(number + U256::from(1));
+    }
+}
