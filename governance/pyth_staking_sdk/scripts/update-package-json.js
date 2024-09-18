c5121fdc0983781d9ce4702d8985834157f4c432
keyvan <keyvankhademi@gmail.com>
2024-09-18 13:23:32 -0700

    fix

--- governance/pyth_staking_sdk/scripts/update-package-json.js
+++ governance/pyth_staking_sdk/scripts/update-package-json.js
@@ -1,18 +1,18 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // Get the directory of the current module
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const distPackageJsonPath = path.join(__dirname, '..', 'dist', 'package.json');
+const distPackageJsonPath = path.join(__dirname, "..", "dist", "package.json");
 
 // Read the original package.json
-const packageJson = JSON.parse(fs.readFileSync(distPackageJsonPath, 'utf8'));
+const packageJson = JSON.parse(fs.readFileSync(distPackageJsonPath, "utf8"));
 
 // Modify the exports field
 packageJson.exports = {
-  ".": "./src/index.js"
+  ".": "./src/index.js",
 };
 
 // Write the modified package.json to the dist folder
