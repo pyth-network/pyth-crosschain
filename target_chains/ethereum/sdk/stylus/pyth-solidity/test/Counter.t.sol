da11d45b76a6326c6165aef6335d290ab77be036
Angry Coding <mailidanielifechukwu@gmail.com>
2024-10-03 09:15:27 +0000

    mock pyth : created mock solidity pyth contract

--- target_chains/ethereum/sdk/stylus/pyth-solidity/test/Counter.t.sol
+++ target_chains/ethereum/sdk/stylus/pyth-solidity/test/Counter.t.sol
@@ -2,23 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract MockPythSampleTest is Test {
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
-    }
-
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
     }
 }
