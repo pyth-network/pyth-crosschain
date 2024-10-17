a5f9664e2638881a2109af060fd2951aa3b4250c
Daniel Chew <cctdaniel@outlook.com>
2024-10-17 13:26:29 +0900

    init

---
+++ apps/benchmarks_keeper/src/main.rs
@@ -0,0 +1,15 @@
+use benchmarks_keeper::{config::Config, run};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let config = Config::new()?;
+    run(config).await?;
+
+    Ok(())
+}
