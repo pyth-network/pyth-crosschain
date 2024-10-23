ec367ded06129d282b556c16a6cb5a19a8ffe9e8
Aditya Arora <arora.aditya520@gmail.com>
2024-10-23 16:11:29 -0400

    chore(entropy) Deploy on abstract

--- target_chains/ethereum/contracts/deploy/zkSyncDeployEntropy.ts
+++ target_chains/ethereum/contracts/deploy/zkSyncDeployEntropy.ts
@@ -4,8 +4,15 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 import { CHAINS } from "@pythnetwork/xc-admin-common";
 import { assert } from "chai";
-import { DefaultStore, EvmChain, EvmWormholeContract } from "@pythnetwork/contract-manager";
-import { findWormholeContract, deployWormholeContract } from "./zkSyncDeployWormhole";
+import {
+  DefaultStore,
+  EvmChain,
+  EvmWormholeContract,
+} from "@pythnetwork/contract-manager";
+import {
+  findWormholeContract,
+  deployWormholeContract,
+} from "./zkSyncDeployWormhole";
 
 // import {Wallet as ZkWallet} from "zksync-ethers";      // Use These packages if "zksync-web3" doesn't work
 // import { Deployer as ZkDeployer } from "@matterlabs/hardhat-zksync";
@@ -19,17 +26,22 @@ function envOrErr(name: string): string {
   }
   return res;
 }
-
-
+export const ENTROPY_DEFAULT_PROVIDER = {
+  mainnet: "0x52DeaA1c84233F7bb8C8A45baeDE41091c616506",
+  testnet: "0x6CC14824Ea2918f5De5C2f75A9Da968ad4BD6344",
+};
+export const ENTROPY_DEFAULT_KEEPER = {
+  mainnet: "0xbcab779fca45290288c35f5e231c37f9fa87b130",
+  testnet: "0xa5A68ed167431Afe739846A22597786ba2da85df",
+};
 
 export default async function (hre: HardhatRuntimeEnvironment) {
   // Initialize the wallet.
   const wallet = Wallet.fromMnemonic(envOrErr("MNEMONIC"));
+  const isMainnet = envOrErr("MAINNET") === "true";
 
   // Create deployer object and load the artifact of the contract we want to deploy.
   const deployer = new Deployer(hre, wallet);
-
-  
 
   const {
     wormholeGovernanceChainId,
@@ -37,8 +49,6 @@ export default async function (hre: HardhatRuntimeEnvironment) {
     wormholeInitialSigners,
     governanceEmitter,
     governanceChainId,
-    emitterAddresses,
-    emitterChainIds,
   } = getDefaultConfig(envOrErr("MIGRATIONS_NETWORK"));
 
   const chainName = envOrErr("MIGRATIONS_NETWORK");
@@ -50,20 +60,58 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   if (!wormholeReceiverContractAddress) {
     console.log(`Wormhole contract not found for chain ${chainName}`);
     console.log("Deploying Wormhole contract...");
-    wormholeReceiverContractAddress  = await deployWormholeContract(deployer, chainName, wormholeGovernanceChainId, wormholeGovernanceContract, wormholeInitialSigners, wormholeReceiverChainId);
+    wormholeReceiverContractAddress = await deployWormholeContract(
+      deployer,
+      chainName,
+      wormholeGovernanceChainId,
+      wormholeGovernanceContract,
+      wormholeInitialSigners,
+      wormholeReceiverChainId
+    );
   }
 
-  console.log("WormholeReceiver contract address:", wormholeReceiverContractAddress);
-  
+  console.log(
+    "WormholeReceiver contract address:",
+    wormholeReceiverContractAddress
+  );
+
   // // TODO: Top up accounts if necessary
 
-  const executorContractAddress = await deployExecutorContract(deployer, wormholeReceiverContractAddress, wormholeReceiverChainId, governanceEmitter);
+  const executorContractAddress = await deployExecutorContract(
+    deployer,
+    wormholeReceiverContractAddress,
+    wormholeReceiverChainId,
+    governanceChainId,
+    governanceEmitter
+  );
 
+  console.log("Executor contract address:", executorContractAddress);
+
+  const entropyContractAddress = await deployEntropyContract(
+    deployer,
+    executorContractAddress,
+    wormholeReceiverChainId,
+    isMainnet
+  );
+
+  console.log("Entropy contract address:", entropyContractAddress);
 }
 
-async function deployExecutorContract(deployer: Deployer, wormholeReceiverContractAddress: string, wormholeReceiverChainId: number, governanceEmitter: string) {
-  const executorImplArtifact = await deployer.loadArtifact("ExecutorUpgradable");
+async function deployExecutorContract(
+  deployer: Deployer,
+  wormholeReceiverContractAddress: string,
+  wormholeReceiverChainId: number,
+  governanceChainId: string,
+  governanceEmitter: string
+) {
+  const executorImplArtifact = await deployer.loadArtifact(
+    "ExecutorUpgradable"
+  );
   const executorImplContract = await deployer.deploy(executorImplArtifact);
+  console.log(
+    "Deployed ExecutorImpl contract on",
+    executorImplContract.address
+  );
 
   const executorInitData = executorImplContract.interface.encodeFunctionData(
     "initialize",
@@ -71,8 +119,8 @@ async function deployExecutorContract(deployer: Deployer, wormholeReceiverContra
       wormholeReceiverContractAddress,
       0, // lastExecutedSequence,
       wormholeReceiverChainId,
+      governanceChainId,
       governanceEmitter,
-      governanceEmitter
     ]
   );
 
@@ -86,4 +134,35 @@ async function deployExecutorContract(deployer: Deployer, wormholeReceiverContra
   console.log(`Deployed Executor contract on ${executorProxyContract.address}`);
 
   return executorProxyContract.address;
+}
+
+async function deployEntropyContract(
+  deployer: Deployer,
+  executorContractAddress: string,
+  chainId: number,
+  isMainnet: boolean
+) {
+  const entropyImplArtifact = await deployer.loadArtifact("EntropyUpgradable");
+  const entropyImplContract = await deployer.deploy(entropyImplArtifact);
+
+  const entropyInitData = entropyImplContract.interface.encodeFunctionData(
+    "initialize",
+    [
+      executorContractAddress,
+      executorContractAddress,
+      1,
+      isMainnet
+        ? ENTROPY_DEFAULT_PROVIDER.mainnet
+        : ENTROPY_DEFAULT_PROVIDER.testnet,
+      true,
+    ]
+  );
+
+  const entropyProxyArtifact = await deployer.loadArtifact("ERC1967Proxy");
+  const entropyProxyContract = await deployer.deploy(entropyProxyArtifact, [
+    entropyImplContract.address,
+    entropyInitData,
+  ]);
+
+  return entropyProxyContract.address;
 }
--- target_chains/ethereum/contracts/deploy/zkSyncDeployWormhole.ts
+++ target_chains/ethereum/contracts/deploy/zkSyncDeployWormhole.ts
@@ -1,23 +1,31 @@
-import { DefaultStore, EvmChain, EvmWormholeContract } from "@pythnetwork/contract-manager";
+import {
+  DefaultStore,
+  EvmChain,
+  EvmWormholeContract,
+} from "@pythnetwork/contract-manager";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 import { CHAINS } from "@pythnetwork/xc-admin-common";
 import { assert } from "chai";
 
 export function findWormholeContract(chainId: string): string | undefined {
-    for (const contract of Object.values(DefaultStore.wormhole_contracts)) {
-      if (
-        contract instanceof EvmWormholeContract &&
-        contract.getChain().getId() === chainId
-      ) {
-        return contract.address;
-      }
+  for (const contract of Object.values(DefaultStore.wormhole_contracts)) {
+    if (
+      contract instanceof EvmWormholeContract &&
+      contract.getChain().getId() === chainId
+    ) {
+      return contract.address;
     }
   }
+}
 
-
-export async function deployWormholeContract(deployer: Deployer, chainName: string, wormholeGovernanceChainId: string, wormholeGovernanceContract: string, wormholeInitialSigners: string[], wormholeReceiverChainId: number): Promise<string> {
-
-
+export async function deployWormholeContract(
+  deployer: Deployer,
+  chainName: string,
+  wormholeGovernanceChainId: string,
+  wormholeGovernanceContract: string,
+  wormholeInitialSigners: string[],
+  wormholeReceiverChainId: number
+): Promise<string> {
   const receiverSetupArtifact = await deployer.loadArtifact("ReceiverSetup");
   const receiverImplArtifact = await deployer.loadArtifact(
     "ReceiverImplementation"
@@ -26,7 +34,7 @@ export async function deployWormholeContract(deployer: Deployer, chainName: stri
     "WormholeReceiver"
   );
   console.log("Deploying WormholeReceiver contract...");
-  
+
   const receiverSetupContract = await deployer.deploy(receiverSetupArtifact);
   console.log("Deployed ReceiverSetup on", receiverSetupContract.address);
   console.log("Deploying ReceiverImplementation contract...");
