28fa93ec8b950af5fe63a34cec54a82ab992f755
Angry Coding <mailidanielifechukwu@gmail.com>
2024-10-28 16:39:04 +0000

    chore:set up benchmark

--- target_chains/ethereum/sdk/stylus/benches/src/main.rs
+++ target_chains/ethereum/sdk/stylus/benches/src/main.rs
@@ -1,3 +1,22 @@
-fn main(){
-    //print_ln!("Hello, world!");
+use benches::{
+    report::BenchmarkReport,
+};
+use futures::FutureExt;
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    // let report = futures::future::try_join_all([
+    //     // access_control::bench().boxed(),
+    //     // erc20::bench().boxed(),
+    //     // erc721::bench().boxed(),
+    //     // merkle_proofs::bench().boxed(),
+    // ])
+    // .await?
+    // .into_iter()
+    // .fold(BenchmarkReport::default(), BenchmarkReport::merge_with);
+
+    println!();
+    //println!("{report}");
+
+    Ok(())
 }
