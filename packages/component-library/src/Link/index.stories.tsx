2ac2b4f1e374da739f820e8f14fd8518b92f974f
Connor Prussin <connor@prussin.net>
2024-10-27 22:46:44 -0700

    feat(component-library): add first round of form components

---
+++ packages/component-library/src/Link/index.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Link as LinkComponent } from "./index.js";
+
+const meta = {
+  component: LinkComponent,
+  argTypes: {
+    children: {
+      control: "text",
+      table: {
+        category: "Contents",
+      },
+    },
+    href: {
+      control: "text",
+      table: {
+        category: "Link",
+      },
+    },
+    target: {
+      control: "text",
+      table: {
+        category: "Link",
+      },
+    },
+    isDisabled: {
+      control: "boolean",
+      table: {
+        category: "State",
+      },
+    },
+  },
+} satisfies Meta<typeof LinkComponent>;
+export default meta;
+
+export const Link = {
+  args: {
+    children: "Link",
+    href: "https://www.pyth.network",
+    target: "_blank",
+    isDisabled: false,
+  },
+} satisfies StoryObj<typeof LinkComponent>;
