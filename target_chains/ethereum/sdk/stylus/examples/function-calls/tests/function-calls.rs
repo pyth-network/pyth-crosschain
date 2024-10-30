28d93161b34dc10906acabb404879b50457861d8
Angry Coding <mailidanielifechukwu@gmail.com>
2024-10-30 12:28:08 +0000

    changes

---
+++ target_chains/ethereum/sdk/stylus/examples/function-calls/tests/function-calls.rs
@@ -0,0 +1,20 @@
+#![cfg(feature = "e2e")]
+
+use alloy::hex;
+use e2e::{receipt,ReceiptExt,Account};
+use eyre::Result;
+
+mod abi;
+
+
+// // ============================================================================
+// // Integration Tests: Function Calls 
+// // ============================================================================
+
+ #[e2e::test]
+async fn constructs(alice: Account) -> Result<()> { 
+   let contract_addr = alice.as_deployer().deploy().await?.address()?; 
+// //   assert_eq!(true, true);
+ 
+  Ok(())
+ }
