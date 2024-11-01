da0a440f2c33511c9f6b5f164fb9fffc2bc21d03
Aditya Arora <arora.aditya520@gmail.com>
2024-11-01 11:59:01 -0400

    chore(pricefeed) Deploy new chains

--- governance/xc_admin/packages/xc_admin_common/src/chains.ts
+++ governance/xc_admin/packages/xc_admin_common/src/chains.ts
@@ -90,6 +90,7 @@ export const RECEIVER_CHAINS = {
   sanko: 60063,
   skate: 60064,
   morph: 60065,
+  superseed_mainnet: 60066,
   // Testnets as a separate chain ids (to use stable data sources and governance for them)
   injective_testnet: 60013,
   osmosis_testnet_4: 60015,
@@ -200,6 +201,9 @@ export const RECEIVER_CHAINS = {
   skate_testnet: 50102,
   movement_porto_testnet: 50103,
   ton_testnet: 50104,
+  kraken_ink_testnet: 50105,
+  ethena_testnet: 50106,
+  superseed_testnet: 50107,
 };
 
 // If there is any overlapping value the receiver chain will replace the wormhole
