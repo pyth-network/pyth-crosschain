5f5bc5e544f2188937ad9627a3de2698c3f75b53
Angry Coding <mailidanielifechukwu@gmail.com>
2024-11-11 16:07:49 +0000

    chore: removed mostu

--- target_chains/ethereum/sdk/stylus/lib/motsu-proc/README.md
+++
@@ -1,5 +0,0 @@
-# Motsu's Procedural Macros
-
-This crate contains procedural macros used in [`motsu`].
-
-[motsu]: ../motsu/README.md
--- target_chains/ethereum/sdk/stylus/lib/motsu/src/prelude.rs
+++
@@ -1,6 +0,0 @@
-//! Common imports for `motsu` tests.
-pub use crate::{
-    context::{acquire_storage, with_context, DefaultStorage},
-    shims::*,
-    storage::reset_storage,
-};
