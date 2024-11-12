3f2d51dd0feabc6330cdfacfa8e6da4636374e24
Daniel Chew <cctdaniel@outlook.com>
2024-11-12 12:50:50 +0900

    update deploy script

--- target_chains/ton/contracts/scripts/deployPyth.ts
+++ target_chains/ton/contracts/scripts/deployPyth.ts
@@ -10,6 +10,7 @@ import {
   MAINNET_UPGRADE_VAAS,
 } from "../tests/utils/wormhole";
 import { BTC_PRICE_FEED_ID, ETH_PRICE_FEED_ID } from "../tests/utils/pyth";
+import { calculateUpdatePriceFeedsFee } from "@pythnetwork/pyth-ton-js";
 
 export async function run(provider: NetworkProvider) {
   const SINGLE_UPDATE_FEE = 1;
@@ -113,12 +114,14 @@ export async function run(provider: NetworkProvider) {
 
   // NOTE: As of 2024/10/14 There's a bug with TON Access (https://ton.access.orbs.network) RPC service where if you provide an update data buffer with length of more than ~320 then the rpc returns error 404 and the function fails
   const updateFee = await main.getUpdateFee(updateData);
-  console.log("Update fee:", updateFee);
 
-  await main.sendUpdatePriceFeeds(
+  const totalFee =
+    calculateUpdatePriceFeedsFee(BigInt(updateFee)) + BigInt(updateFee);
+
+  const result = await main.sendUpdatePriceFeeds(
     provider.sender(),
     updateData,
-    toNano(updateFee)
+    totalFee
   );
   console.log("Price feeds updated successfully.");
 
