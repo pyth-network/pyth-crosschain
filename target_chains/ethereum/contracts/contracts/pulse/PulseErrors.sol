d4caa71e417697c9cd048e20e4369cfeba5814a3
Daniel Chew <cctdaniel@outlook.com>
2024-11-18 20:05:25 +0900

    address comments

--- target_chains/ethereum/contracts/contracts/pulse/PulseErrors.sol
+++ target_chains/ethereum/contracts/contracts/pulse/PulseErrors.sol
@@ -8,6 +8,7 @@ error InsufficientFee();
 error Unauthorized();
 error InvalidCallbackGas();
 error CallbackFailed();
-error InvalidPriceIds(bytes32[] requested, bytes32[] stored);
+error InvalidPriceIds(bytes32 providedPriceIdsHash, bytes32 storedPriceIdsHash);
 error InvalidCallbackGasLimit(uint256 requested, uint256 stored);
 error ExceedsMaxPrices(uint32 requested, uint32 maxAllowed);
+error InsufficientGas();
