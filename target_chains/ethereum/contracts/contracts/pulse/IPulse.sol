b0e7215a142de098444d4284323f21bdd8a645ea
Daniel Chew <cctdaniel@outlook.com>
2024-11-18 16:18:35 +0900

    remove provider

--- target_chains/ethereum/contracts/contracts/pulse/IPulse.sol
+++ target_chains/ethereum/contracts/contracts/pulse/IPulse.sol
@@ -8,7 +8,7 @@ import "./PulseState.sol";
 interface IPulseConsumer {
     function pulseCallback(
         uint64 sequenceNumber,
-        address provider,
+        address updater,
         uint256 publishTime,
         bytes32[] calldata priceIds
     ) external;
@@ -17,66 +17,33 @@ interface IPulseConsumer {
 interface IPulse is PulseEvents {
     // Core functions
     function requestPriceUpdatesWithCallback(
-        address provider,
         uint256 publishTime,
         bytes32[] calldata priceIds,
         uint256 callbackGasLimit
     ) external payable returns (uint64 sequenceNumber);
 
     function executeCallback(
-        address provider,
         uint64 sequenceNumber,
         bytes32[] calldata priceIds,
         bytes[] calldata updateData,
         uint256 callbackGasLimit
     ) external payable;
 
-    // Provider management
-    function register(
-        uint128 feeInWei,
-        uint128 feePerGas,
-        bytes calldata uri
-    ) external;
-
-    function setProviderFee(uint128 newFeeInWei) external;
-
-    function setProviderFeeAsFeeManager(
-        address provider,
-        uint128 newFeeInWei
-    ) external;
-
-    function setProviderUri(bytes calldata uri) external;
-
-    function withdraw(uint128 amount) external;
-
-    function withdrawAsFeeManager(address provider, uint128 amount) external;
-
     // Getters
     function getFee(
-        address provider,
         uint256 callbackGasLimit
     ) external view returns (uint128 feeAmount);
 
     function getPythFeeInWei() external view returns (uint128 pythFeeInWei);
 
-    function getAccruedPythFees()
-        external
-        view
-        returns (uint128 accruedPythFeesInWei);
-
-    function getDefaultProvider() external view returns (address);
-
-    function getProviderInfo(
-        address provider
-    ) external view returns (PulseState.ProviderInfo memory info);
+    function getAccruedFees() external view returns (uint128 accruedFeesInWei);
 
     function getRequest(
-        address provider,
         uint64 sequenceNumber
     ) external view returns (PulseState.Request memory req);
 
-    // Setters
+    // Add these functions to the IPulse interface
     function setFeeManager(address manager) external;
 
-    function setMaxNumPrices(uint32 maxNumPrices) external;
+    function withdrawAsFeeManager(uint128 amount) external;
 }
--- target_chains/ethereum/contracts/contracts/pulse/PulseState.sol
+++ target_chains/ethereum/contracts/contracts/pulse/PulseState.sol
@@ -7,7 +7,6 @@ contract PulseState {
     bytes1 public constant NUM_REQUESTS_MASK = 0x1f;
 
     struct Request {
-        address provider;
         uint64 sequenceNumber;
         uint256 publishTime;
         bytes32[] priceIds;
@@ -15,25 +14,15 @@ contract PulseState {
         address requester;
     }
 
-    struct ProviderInfo {
-        uint64 sequenceNumber;
-        uint128 feeInWei;
-        uint128 accruedFeesInWei;
-        bytes uri;
-        address feeManager;
-        uint32 maxNumPrices;
-        uint128 feePerGas;
-    }
-
     struct State {
         address admin;
         uint128 pythFeeInWei;
-        uint128 accruedPythFeesInWei;
-        address defaultProvider;
+        uint128 accruedFeesInWei;
         address pyth;
+        uint64 currentSequenceNumber;
+        address feeManager;
         Request[32] requests;
         mapping(bytes32 => Request) requestsOverflow;
-        mapping(address => ProviderInfo) providers;
     }
 
     State internal _state;
