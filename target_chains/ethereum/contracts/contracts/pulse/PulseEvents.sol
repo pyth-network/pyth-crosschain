f8e398d179b315bfffaae9fb2c6a9c397d5c7beb
Daniel Chew <cctdaniel@outlook.com>
2024-11-18 16:18:35 +0900

    remove provider

--- target_chains/ethereum/contracts/contracts/pulse/PulseEvents.sol
+++ target_chains/ethereum/contracts/contracts/pulse/PulseEvents.sol
@@ -4,14 +4,11 @@ pragma solidity ^0.8.0;
 import "./PulseState.sol";
 
 interface PulseEvents {
-    // Events
-    event ProviderRegistered(PulseState.ProviderInfo providerInfo);
-
     event PriceUpdateRequested(PulseState.Request request);
 
     event PriceUpdateExecuted(
         uint64 indexed sequenceNumber,
-        address indexed provider,
+        address indexed updater,
         uint256 publishTime,
         bytes32[] priceIds,
         int64[] prices,
@@ -20,42 +17,20 @@ interface PulseEvents {
         uint256[] publishTimes
     );
 
-    event ProviderFeeUpdated(
-        address indexed provider,
-        uint128 oldFeeInWei,
-        uint128 newFeeInWei
-    );
-
-    event ProviderUriUpdated(
-        address indexed provider,
-        bytes oldUri,
-        bytes newUri
-    );
-
-    event ProviderWithdrawn(
-        address indexed provider,
-        address indexed recipient,
-        uint128 amount
-    );
-
-    event ProviderFeeManagerUpdated(
-        address indexed provider,
-        address oldFeeManager,
-        address newFeeManager
-    );
-
-    event ProviderMaxNumPricesUpdated(
-        address indexed provider,
-        uint32 oldMaxNumPrices,
-        uint32 maxNumPrices
-    );
+    event FeesWithdrawn(address indexed recipient, uint128 amount);
 
     event PriceUpdateCallbackFailed(
         uint64 indexed sequenceNumber,
-        address indexed provider,
+        address indexed updater,
         uint256 publishTime,
         bytes32[] priceIds,
         address requester,
         string reason
+    );
+
+    event FeeManagerUpdated(
+        address indexed admin,
+        address oldFeeManager,
+        address newFeeManager
     );
 }
