5a945e2f0683e1e022682857125ff94467f48753
guibescos <59208140+guibescos@users.noreply.github.com>
2024-11-19 11:13:23 +0000

    chore: benchmarks example (#2121)
    * go
    
    * comments

--- target_chains/solana/sdk/js/pyth_solana_receiver/examples/post_price_update_from_benchmarks.ts
+++ target_chains/solana/sdk/js/pyth_solana_receiver/examples/post_price_update_from_benchmarks.ts
@@ -1,15 +1,13 @@
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { HermesClient, PriceUpdate } from "@pythnetwork/hermes-client";
 import { InstructionWithEphemeralSigners, PythSolanaReceiver } from "../";
 import { Wallet } from "@coral-xyz/anchor";
 import fs from "fs";
 import os from "os";
-import { HermesClient } from "@pythnetwork/hermes-client";
 
 // Get price feed ids from https://pyth.network/developers/price-feed-ids#pyth-evm-stable
 const SOL_PRICE_FEED_ID =
   "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d";
-const ETH_PRICE_FEED_ID =
-  "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace";
 
 let keypairFile = "";
 if (process.env["SOLANA_KEYPAIR"]) {
@@ -28,20 +26,20 @@ async function main() {
   const pythSolanaReceiver = new PythSolanaReceiver({ connection, wallet });
 
   // Get the price update from hermes
-  const priceUpdateData = await getPriceUpdateData();
+  const priceUpdateData = await getPriceUpdateDataFromOneDayAgo();
   console.log(`Posting price update: ${priceUpdateData}`);
 
-  // The shard indicates which set of price feed accounts you wish to update.
-  const shardId = 1;
-
-  const transactionBuilder = pythSolanaReceiver.newTransactionBuilder({});
-  // Update the price feed accounts for the feed ids in priceUpdateData (in this example, SOL and ETH) and shard id.
-  await transactionBuilder.addUpdatePriceFeed(priceUpdateData, shardId);
+  // If closeUpdateAccounts = true, the builder will automatically generate instructions to close the ephemeral price update accounts
+  // at the end of the transaction. Closing the accounts will reclaim their rent.
+  // The example is using closeUpdateAccounts = false so you can easily look up the price update account in an explorer.
+  const transactionBuilder = pythSolanaReceiver.newTransactionBuilder({
+    closeUpdateAccounts: false,
+  });
+  // Post the price updates to ephemeral accounts, one per price feed.
+  await transactionBuilder.addPostPriceUpdates(priceUpdateData);
   console.log(
     "The SOL/USD price update will get posted to:",
-    pythSolanaReceiver
-      .getPriceFeedAccountAddress(shardId, SOL_PRICE_FEED_ID)
-      .toBase58()
+    transactionBuilder.getPriceUpdateAccount(SOL_PRICE_FEED_ID).toBase58()
   );
 
   await transactionBuilder.addPriceConsumerInstructions(
@@ -66,17 +64,15 @@ async function main() {
 }
 
 // Fetch price update data from Hermes
-async function getPriceUpdateData() {
-  const priceServiceConnection = new HermesClient(
-    "https://hermes.pyth.network/",
-    {}
-  );
+async function getPriceUpdateDataFromOneDayAgo(): Promise<string[]> {
+  const hermesClient = new HermesClient("https://hermes.pyth.network/", {});
 
-  const response = await priceServiceConnection.getLatestPriceUpdates(
-    [SOL_PRICE_FEED_ID, ETH_PRICE_FEED_ID],
+  const oneDayAgo = Math.floor(Date.now() / 1000) - 86400;
+  const response = await hermesClient.getPriceUpdatesAtTimestamp(
+    oneDayAgo,
+    [SOL_PRICE_FEED_ID],
     { encoding: "base64" }
   );
-
   return response.binary.data;
 }
 
