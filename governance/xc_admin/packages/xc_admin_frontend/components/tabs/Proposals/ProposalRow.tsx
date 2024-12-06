3e6fedc99759b8b5a83b47f5d721a987d7ac70e2
Guillermo Bescos <g.bescos@yahoo.com>
2024-12-06 19:01:56 +0000

    fix rebase

--- governance/xc_admin/packages/xc_admin_frontend/components/tabs/Proposals/ProposalRow.tsx
+++ governance/xc_admin/packages/xc_admin_frontend/components/tabs/Proposals/ProposalRow.tsx
@@ -57,7 +57,6 @@ export const ProposalRow = ({
 
         // set proposal time
         if (!time) {
-<<<<<<< HEAD
           connection
             .getSignaturesForAddress(proposal.publicKey)
             .then((txs) => {
@@ -72,15 +71,6 @@ export const ProposalRow = ({
                 `Error fetching proposal time for ${proposal.publicKey.toBase58()}: ${err}`
               )
             })
-=======
-          connection.getSignaturesForAddress(proposal.publicKey).then((txs) => {
-            if (isCancelled) return
-            const firstBlockTime = txs?.[txs.length - 1]?.blockTime
-            if (firstBlockTime) {
-              setTime(new Date(firstBlockTime * 1000))
-            }
-          })
->>>>>>> c6a78a605 (use getSignaturesForAddress)
         }
 
         // calculate instructions summary
