08e2427568bcc99ebed2bf69b46733e3db06252b
Pavel Strakhov <ri@idzaaus.org>
2024-12-11 10:29:37 +0000

    fix(lazer): serialize Option<Price> as i64 in publisher protocol (#2182)

--- lazer/sdk/rust/protocol/src/publisher.rs
+++ lazer/sdk/rust/protocol/src/publisher.rs
@@ -18,13 +18,64 @@ pub struct PriceFeedData {
     pub source_timestamp_us: TimestampUs,
     /// Timestamp of the last update provided by the publisher.
     pub publisher_timestamp_us: TimestampUs,
-    /// Last known value of the "main" (?) price of this price feed.
+    /// Last known value of the best executable price of this price feed.
     /// `None` if no value is currently available.
+    #[serde(with = "crate::serde_price_as_i64")]
     pub price: Option<Price>,
     /// Last known value of the best bid price of this price feed.
     /// `None` if no value is currently available.
+    #[serde(with = "crate::serde_price_as_i64")]
     pub best_bid_price: Option<Price>,
     /// Last known value of the best ask price of this price feed.
     /// `None` if no value is currently available.
+    #[serde(with = "crate::serde_price_as_i64")]
     pub best_ask_price: Option<Price>,
+}
+
+#[test]
+fn price_feed_data_serde() {
+    let data = [
+        1, 0, 0, 0, // price_feed_id
+        2, 0, 0, 0, 0, 0, 0, 0, // source_timestamp_us
+        3, 0, 0, 0, 0, 0, 0, 0, // publisher_timestamp_us
+        4, 0, 0, 0, 0, 0, 0, 0, // price
+        5, 0, 0, 0, 0, 0, 0, 0, // best_bid_price
+        6, 2, 0, 0, 0, 0, 0, 0, // best_ask_price
+    ];
+
+    let expected = PriceFeedData {
+        price_feed_id: PriceFeedId(1),
+        source_timestamp_us: TimestampUs(2),
+        publisher_timestamp_us: TimestampUs(3),
+        price: Some(Price(4.try_into().unwrap())),
+        best_bid_price: Some(Price(5.try_into().unwrap())),
+        best_ask_price: Some(Price((2 * 256 + 6).try_into().unwrap())),
+    };
+    assert_eq!(
+        bincode::deserialize::<PriceFeedData>(&data).unwrap(),
+        expected
+    );
+    assert_eq!(bincode::serialize(&expected).unwrap(), data);
+
+    let data2 = [
+        1, 0, 0, 0, // price_feed_id
+        2, 0, 0, 0, 0, 0, 0, 0, // source_timestamp_us
+        3, 0, 0, 0, 0, 0, 0, 0, // publisher_timestamp_us
+        4, 0, 0, 0, 0, 0, 0, 0, // price
+        0, 0, 0, 0, 0, 0, 0, 0, // best_bid_price
+        0, 0, 0, 0, 0, 0, 0, 0, // best_ask_price
+    ];
+    let expected2 = PriceFeedData {
+        price_feed_id: PriceFeedId(1),
+        source_timestamp_us: TimestampUs(2),
+        publisher_timestamp_us: TimestampUs(3),
+        price: Some(Price(4.try_into().unwrap())),
+        best_bid_price: None,
+        best_ask_price: None,
+    };
+    assert_eq!(
+        bincode::deserialize::<PriceFeedData>(&data2).unwrap(),
+        expected2
+    );
+    assert_eq!(bincode::serialize(&expected2).unwrap(), data2);
 }
