e2df2742ca905d6d83ccfba7c4dcaee7b5e880a9
Tejas Badadare <tejasbadadare@gmail.com>
2024-12-18 10:47:06 -0800

    fix: update compute budget for postTwapUpdate based on devnet runs

--- target_chains/solana/sdk/js/pyth_solana_receiver/src/compute_budget.ts
+++ target_chains/solana/sdk/js/pyth_solana_receiver/src/compute_budget.ts
@@ -13,7 +13,7 @@ export const POST_UPDATE_COMPUTE_BUDGET = 35000;
 /**
  * A hard-coded budget for the compute units required for the `postUpdate` instruction in the Pyth Solana Receiver program.
  */
-export const POST_TWAP_UPDATE_COMPUTE_BUDGET = 100_000;
+export const POST_TWAP_UPDATE_COMPUTE_BUDGET = 50_000;
 /**
  * A hard-coded budget for the compute units required for the `updatePriceFeed` instruction in the Pyth Push Oracle program.
  */
