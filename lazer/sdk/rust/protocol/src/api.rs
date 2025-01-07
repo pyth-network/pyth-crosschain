ecac040d0f9e4ac15c28846f938381028ccfe85d
Darun Seethammagari <darunrs@gmail.com>
2025-01-07 12:17:11 -0800

    Add latest price api

---
+++ lazer/sdk/rust/protocol/src/api.rs
@@ -0,0 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+use crate::{payload::AggregatedPriceFeedData, router::PriceFeedId};
+
+/// A request sent from the client to the server.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "camelCase")]
+pub enum ApiRequest {
+    LatestPrice(LatestPriceRequest),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct LatestPriceRequest {
+    pub price_feed_ids: Vec<PriceFeedId>,
+}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct LatestPriceResponse {
+    pub price_feed_ids: Vec<AggregatedPriceFeedData>,
+}
