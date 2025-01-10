74e976ffdfedb26d6c04c107def6c0708c59c2b8
Ali Behjati <bahjatia@gmail.com>
2025-01-10 16:38:50 +0100

    refactor!(lazer): update api protocol (#2237)
    * refactor!(lazer): update api protocol
    
    our apis should be similar to each other as much as possible.
    
    * fix: add json binary encoding

--- lazer/sdk/rust/protocol/src/api.rs
+++ lazer/sdk/rust/protocol/src/api.rs
@@ -1,14 +1,22 @@
 use serde::{Deserialize, Serialize};
 
-use crate::{
-    payload::AggregatedPriceFeedData,
-    router::{JsonUpdate, PriceFeedId},
+use crate::router::{
+    Chain, Channel, JsonBinaryEncoding, JsonUpdate, PriceFeedId, PriceFeedProperty,
 };
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct LatestPriceRequest {
     pub price_feed_ids: Vec<PriceFeedId>,
+    pub properties: Vec<PriceFeedProperty>,
+    pub chains: Vec<Chain>,
+    #[serde(default)]
+    pub json_binary_encoding: JsonBinaryEncoding,
+    /// If `true`, the stream update will contain a JSON object containing
+    /// all data of the update.
+    #[serde(default = "default_parsed")]
+    pub parsed: bool,
+    pub channel: Channel,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -18,22 +26,9 @@ pub struct ReducePriceRequest {
     pub price_feed_ids: Vec<PriceFeedId>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct LatestPriceResponse {
-    pub latest_prices: Vec<LatestPrice>,
-}
+pub type LatestPriceResponse = JsonUpdate;
+pub type ReducePriceResponse = JsonUpdate;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct ReducePriceResponse {
-    pub payload: JsonUpdate,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct LatestPrice {
-    pub id: PriceFeedId,
-    pub exponent: i16,
-    pub prices: AggregatedPriceFeedData,
+pub fn default_parsed() -> bool {
+    true
 }
