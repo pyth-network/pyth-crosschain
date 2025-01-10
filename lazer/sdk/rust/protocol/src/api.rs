0848c29928146053bed1a5954c14c87671f57a4c
Ali Behjati <bahjatia@gmail.com>
2025-01-10 15:13:49 +0100

    fix: add json binary encoding

--- lazer/sdk/rust/protocol/src/api.rs
+++ lazer/sdk/rust/protocol/src/api.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 
-use crate::router::{Chain, Channel, JsonUpdate, PriceFeedId, PriceFeedProperty};
+use crate::router::{
+    Chain, Channel, JsonBinaryEncoding, JsonUpdate, PriceFeedId, PriceFeedProperty,
+};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -8,6 +10,8 @@ pub struct LatestPriceRequest {
     pub price_feed_ids: Vec<PriceFeedId>,
     pub properties: Vec<PriceFeedProperty>,
     pub chains: Vec<Chain>,
+    #[serde(default)]
+    pub json_binary_encoding: JsonBinaryEncoding,
     /// If `true`, the stream update will contain a JSON object containing
     /// all data of the update.
     #[serde(default = "default_parsed")]
