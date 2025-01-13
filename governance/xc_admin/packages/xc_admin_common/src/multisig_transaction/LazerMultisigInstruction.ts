405c42a343a66423884755df93a85dbea1f04eb0
Daniel Chew <cctdaniel@outlook.com>
2025-01-13 16:39:39 +0900

    feat: add support for deserializing lazer instructions in xc-admin

---
+++ governance/xc_admin/packages/xc_admin_common/src/multisig_transaction/LazerMultisigInstruction.ts
@@ -0,0 +1,66 @@
+import {
+  MultisigInstruction,
+  MultisigInstructionProgram,
+  UNRECOGNIZED_INSTRUCTION,
+} from "./index";
+import { AnchorAccounts, resolveAccountNames } from "./anchor";
+import { PublicKey, TransactionInstruction } from "@solana/web3.js";
+import { Idl, BorshInstructionCoder } from "@coral-xyz/anchor";
+import * as pythLazerSolanaContractIdl from "../../../../../../lazer/contracts/solana/target/idl/pyth_lazer_solana_contract.json";
+
+export const LAZER_PROGRAM_ID = new PublicKey(
+  "pytd2yyk641x7ak7mkaasSJVXh6YYZnC7wTmtgAyxPt"
+);
+
+export class LazerMultisigInstruction implements MultisigInstruction {
+  readonly program = MultisigInstructionProgram.Lazer;
+  readonly name: string;
+  readonly args: { [key: string]: any };
+  readonly accounts: AnchorAccounts;
+
+  constructor(
+    name: string,
+    args: { [key: string]: any },
+    accounts: AnchorAccounts
+  ) {
+    this.name = name;
+    this.args = args;
+    this.accounts = accounts;
+  }
+
+  static fromInstruction(
+    instruction: TransactionInstruction
+  ): LazerMultisigInstruction {
+    // TODO: This is a hack to transform the IDL to be compatible with the anchor version we are using, we can't upgrade anchor to 0.30.1 because then the existing idls will break
+    const idl = {
+      version: pythLazerSolanaContractIdl.metadata.version,
+      name: pythLazerSolanaContractIdl.metadata.name,
+      instructions: pythLazerSolanaContractIdl.instructions.map((ix) => ({
+        ...ix,
+        accounts: ix.accounts.map((acc) => ({
+          name: acc.name,
+          isMut: acc.writable ?? false,
+          isSigner: acc.signer ?? false,
+        })),
+      })),
+    } as Idl;
+
+    const coder = new BorshInstructionCoder(idl);
+
+    const deserializedData = coder.decode(instruction.data);
+
+    if (deserializedData) {
+      return new LazerMultisigInstruction(
+        deserializedData.name,
+        deserializedData.data,
+        resolveAccountNames(idl, deserializedData.name, instruction)
+      );
+    } else {
+      return new LazerMultisigInstruction(
+        UNRECOGNIZED_INSTRUCTION,
+        { data: instruction.data },
+        { named: {}, remaining: instruction.keys }
+      );
+    }
+  }
+}
--- governance/xc_admin/packages/xc_admin_common/src/multisig_transaction/index.ts
+++ governance/xc_admin/packages/xc_admin_common/src/multisig_transaction/index.ts
@@ -27,6 +27,10 @@ import {
   PRICE_STORE_PROGRAM_ID,
   PriceStoreMultisigInstruction,
 } from "../price_store";
+import {
+  LazerMultisigInstruction,
+  LAZER_PROGRAM_ID,
+} from "./LazerMultisigInstruction";
 
 export const UNRECOGNIZED_INSTRUCTION = "unrecognizedInstruction";
 export enum MultisigInstructionProgram {
@@ -41,6 +45,7 @@ export enum MultisigInstructionProgram {
   SolanaReceiver,
   UnrecognizedProgram,
   PythPriceStore,
+  Lazer,
 }
 
 export function getProgramName(program: MultisigInstructionProgram) {
@@ -67,6 +72,8 @@ export function getProgramName(program: MultisigInstructionProgram) {
       return "Pyth Price Store";
     case MultisigInstructionProgram.UnrecognizedProgram:
       return "Unknown";
+    case MultisigInstructionProgram.Lazer:
+      return "Lazer";
   }
 }
 
@@ -161,6 +168,8 @@ export class MultisigParser {
       return SolanaStakingMultisigInstruction.fromTransactionInstruction(
         instruction
       );
+    } else if (instruction.programId.equals(LAZER_PROGRAM_ID)) {
+      return LazerMultisigInstruction.fromInstruction(instruction);
     } else {
       return UnrecognizedProgram.fromTransactionInstruction(instruction);
     }
