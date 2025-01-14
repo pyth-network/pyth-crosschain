9d08d79aed48bc727610139d0be50f0fbf70e80b
Daniel Chew <cctdaniel@outlook.com>
2025-01-14 14:16:45 +0900

    export storage id

--- lazer/sdk/js/src/constants.ts
+++ lazer/sdk/js/src/constants.ts
@@ -3,3 +3,6 @@ import { PublicKey } from "@solana/web3.js";
 export const LAZER_PROGRAM_ID = new PublicKey(
   "pytd2yyk641x7ak7mkaasSJVXh6YYZnC7wTmtgAyxPt"
 );
+export const STORAGE_ID = new PublicKey(
+  "3rdJbqfnagQ4yx9HXJViD4zc4xpiSqmFsKpPuSCQVyQL"
+);
