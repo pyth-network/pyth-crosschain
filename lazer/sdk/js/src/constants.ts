551c66a8dc7f55e1c264287053b3a6965aee4d7f
Daniel Chew <cctdaniel@outlook.com>
2025-01-14 22:07:28 +0900

    feat(lazer/sdk/js): export constants (#2252)
    * export lazer program id
    
    * bump
    
    * export storage id
    
    * add SOLANA prefix

---
+++ lazer/sdk/js/src/constants.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const SOLANA_LAZER_PROGRAM_ID = new PublicKey(
+  "pytd2yyk641x7ak7mkaasSJVXh6YYZnC7wTmtgAyxPt"
+);
+export const SOLANA_STORAGE_ID = new PublicKey(
+  "3rdJbqfnagQ4yx9HXJViD4zc4xpiSqmFsKpPuSCQVyQL"
+);
