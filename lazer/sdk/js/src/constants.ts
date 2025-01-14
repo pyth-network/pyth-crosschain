06aca636f53cdd0895dab121f6f4667ce7f016f7
Daniel Chew <cctdaniel@outlook.com>
2025-01-14 21:48:54 +0900

    add SOLANA prefix

--- lazer/sdk/js/src/constants.ts
+++ lazer/sdk/js/src/constants.ts
@@ -1,8 +1,8 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const LAZER_PROGRAM_ID = new PublicKey(
+export const SOLANA_LAZER_PROGRAM_ID = new PublicKey(
   "pytd2yyk641x7ak7mkaasSJVXh6YYZnC7wTmtgAyxPt"
 );
-export const STORAGE_ID = new PublicKey(
+export const SOLANA_STORAGE_ID = new PublicKey(
   "3rdJbqfnagQ4yx9HXJViD4zc4xpiSqmFsKpPuSCQVyQL"
 );
