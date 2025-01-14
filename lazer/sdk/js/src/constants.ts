6c9a24d6895c160b926fddc3999a31ff1cf53a48
Daniel Chew <cctdaniel@outlook.com>
2025-01-14 13:50:50 +0900

    export lazer program id

---
+++ lazer/sdk/js/src/constants.ts
@@ -0,0 +1,5 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const LAZER_PROGRAM_ID = new PublicKey(
+  "pytd2yyk641x7ak7mkaasSJVXh6YYZnC7wTmtgAyxPt"
+);
--- lazer/sdk/js/src/index.ts
+++ lazer/sdk/js/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./client.js";
 export * from "./protocol.js";
 export * from "./ed25519.js";
+export * from "./constants.js";
