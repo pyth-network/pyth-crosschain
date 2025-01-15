4452b901e2a7558d1bb4eb652841834054a494ff
Aditya Arora <arora.aditya520@gmail.com>
2025-01-15 15:50:37 -0500

    update

--- apps/entropy-debug/src/app/page.tsx
+++ apps/entropy-debug/src/app/page.tsx
@@ -1,50 +1,58 @@
-"use client"
-
-import * as React from "react"
-import { Switch } from "../components/ui/switch"
-import { Input } from "../components/ui/input"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select"
-import { useState, useMemo, useCallback, useEffect, useRef } from "react"
-import { EntropyDeployments } from "../store/EntropyDeployments"
-import { isValidTxHash } from "../lib/utils"
-import { requestCallback } from "../lib/revelation"
-import hljs from 'highlight.js/lib/core';
-import bash from 'highlight.js/lib/languages/bash';
-import 'highlight.js/styles/github-dark.css'; // You can choose different themes
+"use client";
+import * as React from "react";
+import { Switch } from "../components/ui/switch";
+import { Input } from "../components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../components/ui/select";
+import { useState, useMemo, useCallback, useEffect, useRef } from "react";
+import { EntropyDeployments } from "../store/EntropyDeployments";
+import { isValidTxHash } from "../lib/utils";
+import { requestCallback } from "../lib/revelation";
+import hljs from "highlight.js/lib/core";
+import bash from "highlight.js/lib/languages/bash";
+import "highlight.js/styles/github-dark.css"; // You can choose different themes
 
 // Register the bash language
-hljs.registerLanguage('bash', bash);
+hljs.registerLanguage("bash", bash);
 
 class BaseError extends Error {
   constructor(message: string) {
-    super(message)
-    this.name = "BaseError"
+    super(message);
+    this.name = "BaseError";
   }
 }
 
 class InvalidTxHashError extends BaseError {
   constructor(message: string) {
-    super(message)
-    this.name = "InvalidTxHashError"
+    super(message);
+    this.name = "InvalidTxHashError";
   }
 }
-
 
 enum TxStateType {
   NotLoaded,
   Loading,
   Success,
-  Error
+  Error,
 }
 
 const TxState = {
-  NotLoaded: () => ({status: TxStateType.NotLoaded as const}),
-  Loading: () => ({status: TxStateType.Loading as const}),
-  Success: (data: string) => ({status: TxStateType.Success as const, data}),
-  Error: (error: unknown) => ({status: TxStateType.Error as const, error}),
-}
+  NotLoaded: () => ({ status: TxStateType.NotLoaded as const }),
+  Loading: () => ({ status: TxStateType.Loading as const }),
+  Success: (data: string) => ({ status: TxStateType.Success as const, data }),
+  Error: (error: unknown) => ({ status: TxStateType.Error as const, error }),
+};
 
-type TxStateContext = ReturnType<typeof TxState.NotLoaded> | ReturnType<typeof TxState.Loading> | ReturnType<typeof TxState.Success> | ReturnType<typeof TxState.Error>
+type TxStateContext =
+  | ReturnType<typeof TxState.NotLoaded>
+  | ReturnType<typeof TxState.Loading>
+  | ReturnType<typeof TxState.Success>
+  | ReturnType<typeof TxState.Error>;
 
 export default function PythEntropyDebugApp() {
   const [state, setState] = useState<TxStateContext>(TxState.NotLoaded());
@@ -55,7 +63,11 @@ export default function PythEntropyDebugApp() {
 
   const validateTxHash = (hash: string) => {
     if (!isValidTxHash(hash) && hash !== "") {
-      setError(new InvalidTxHashError("Transaction hash must be 64 hexadecimal characters"));
+      setError(
+        new InvalidTxHashError(
+          "Transaction hash must be 64 hexadecimal characters"
+        )
+      );
     } else {
       setError(null);
     }
@@ -64,7 +76,10 @@ export default function PythEntropyDebugApp() {
 
   const availableChains = useMemo(() => {
     return Object.entries(EntropyDeployments)
-      .filter(([, deployment]) => deployment.network === (isMainnet ? "mainnet" : "testnet"))
+      .filter(
+        ([, deployment]) =>
+          deployment.network === (isMainnet ? "mainnet" : "testnet")
+      )
       .map(([key]) => key);
   }, [isMainnet]);
 
@@ -79,7 +94,7 @@ export default function PythEntropyDebugApp() {
       });
   }, [txHash, selectedChain]);
 
-  const Info = ({state}: {state: TxStateContext}) => {
+  const Info = ({ state }: { state: TxStateContext }) => {
     const preRef = useRef<HTMLPreElement>(null);
 
     useEffect(() => {
@@ -90,13 +105,15 @@ export default function PythEntropyDebugApp() {
 
     switch (state.status) {
       case TxStateType.NotLoaded:
-        return <div>Not loaded</div>
+        return <div>Not loaded</div>;
       case TxStateType.Loading:
-        return <div>Loading...</div>
+        return <div>Loading...</div>;
       case TxStateType.Success:
         return (
           <div className="mt-4 p-4 bg-gray-100 rounded w-full max-w-3xl">
-            <p className="mb-2">Please run the following command in your terminal:</p>
+            <p className="mb-2">
+              Please run the following command in your terminal:
+            </p>
             <div className="relative">
               <pre
                 ref={preRef}
@@ -112,15 +129,15 @@ export default function PythEntropyDebugApp() {
               </button>
             </div>
           </div>
-        )
+        );
       case TxStateType.Error:
         return (
           <div className="mt-4 p-4 bg-red-100 border border-red-400 rounded">
             <div className="text-red-600">{String(state.error)}</div>
           </div>
-        )
+        );
     }
-  }
+  };
 
   return (
     <div className="flex flex-col items-center justify-start h-screen">
@@ -136,25 +153,30 @@ export default function PythEntropyDebugApp() {
         <label htmlFor="network-mode">Mainnet</label>
       </div>
       <div className="mt-4">
-          <Select onValueChange={setSelectedChain} value={selectedChain}>
+        <Select onValueChange={setSelectedChain} value={selectedChain}>
           <SelectTrigger>
             <SelectValue placeholder="Select Chain" />
           </SelectTrigger>
           <SelectContent>
             {availableChains.map((chain) => (
               <SelectItem key={chain} value={chain}>
-                {chain.charAt(0).toUpperCase() + chain.slice(1).replace(/-/g, ' ')}
+                {chain.charAt(0).toUpperCase() +
+                  chain.slice(1).replace(/-/g, " ")}
               </SelectItem>
             ))}
           </SelectContent>
         </Select>
       </div>
       <div className="mt-4">
-        <label htmlFor="tx-hash" className="mr-2">Request Transaction Hash:</label>
+        <label htmlFor="tx-hash" className="mr-2">
+          Request Transaction Hash:
+        </label>
         <Input
           minLength={64}
           id="tx-hash"
-          className={`border rounded p-2 w-full ${error ? 'border-red-500' : ''}`}
+          className={`border rounded p-2 w-full ${
+            error ? "border-red-500" : ""
+          }`}
           placeholder="Enter Request Transaction Hash:"
           value={txHash}
           onChange={(e) => validateTxHash(e.target.value)}
--- apps/entropy-debug/src/lib/revelation.ts
+++ apps/entropy-debug/src/lib/revelation.ts
@@ -1,6 +1,7 @@
-import { createPublicClient, http, parseEventLogs, publicActions } from 'viem'
-import { EntropyAbi } from './EntropyAbi'
-import { EntropyDeployment, EntropyDeployments } from '../store/EntropyDeployments'
+import { createPublicClient, http, parseEventLogs, publicActions } from "viem";
+import { EntropyAbi } from "./EntropyAbi";
+import { EntropyDeployments } from "../store/EntropyDeployments";
+import type { EntropyDeployment } from "../store/EntropyDeployments";
 
 interface Revelation {
   value: {
@@ -8,117 +9,136 @@ interface Revelation {
   };
 }
 
-export async function requestCallback(txHash: string, chain: string): Promise<string> {
-  console.log("requestCallback", txHash, chain)
-  const deployment = EntropyDeployments[chain]
-  console.log("deployment", deployment)
+export async function requestCallback(
+  txHash: string,
+  chain: string
+): Promise<string> {
+  console.log("requestCallback", txHash, chain);
+  const deployment = EntropyDeployments[chain];
+  console.log("deployment", deployment);
   if (!deployment) {
-    console.error("Deployment for chain not found", chain)
-    throw new Error(`Deployment for chain ${chain} not found`)
+    console.error("Deployment for chain not found", chain);
+    throw new Error(`Deployment for chain ${chain} not found`);
   }
 
-  let provider: string
-  let sequenceNumber: bigint
-  let userRandomNumber: string
+  let provider: string;
+  let sequenceNumber: bigint;
+  let userRandomNumber: string;
 
   try {
-    ({ provider, sequenceNumber, userRandomNumber } = await fetchInfoFromTx(txHash, deployment))
+    ({ provider, sequenceNumber, userRandomNumber } = await fetchInfoFromTx(
+      txHash,
+      deployment
+    ));
   } catch (error) {
-    console.error("Error fetching info from tx:", error)
-    throw new Error("We found an error message: " + error)
+    console.error("Error fetching info from tx:", error);
+    throw new Error("We found an error message: " + error);
   }
 
-  let revelation: string | Revelation
+  let revelation: string | Revelation;
   try {
-    revelation = await getRevelation(chain, Number(sequenceNumber))
+    revelation = await getRevelation(chain, Number(sequenceNumber));
   } catch (error) {
-    console.error("Error fetching revelation:", error)
-    throw new Error("We found an error message: " + error)
+    console.error("Error fetching revelation:", error);
+    throw new Error("We found an error message: " + error);
   }
 
-  console.log("revelation", revelation)
+  console.log("revelation", revelation);
 
   // It means the there is an error message
   if (typeof revelation === "string") {
-    console.error("We found an error message: " + revelation)
-    throw new Error("We found an error message: " + revelation)
+    console.error("We found an error message: " + revelation);
+    throw new Error("We found an error message: " + revelation);
   }
 
-  const message = `cast send ${deployment.address} 'revealWithCallback(address, uint64, bytes32, bytes32)' ${provider} ${sequenceNumber} ${userRandomNumber} ${revelation.value.data} -r ${deployment.rpc} --private-key <YOUR_PRIVATE_KEY>`
-  console.log("message", message)
+  const message = `cast send ${deployment.address} 'revealWithCallback(address, uint64, bytes32, bytes32)' ${provider} ${sequenceNumber} ${userRandomNumber} ${revelation.value.data} -r ${deployment.rpc} --private-key <YOUR_PRIVATE_KEY>`;
+  console.log("message", message);
 
-  return message
+  return message;
 }
 
-  export async function fetchInfoFromTx(txHash: string, deployment: EntropyDeployment) {
-    const publicClient = createPublicClient({
-      transport: http(deployment.rpc)
-    }).extend(publicActions)
-    if (!publicClient) {
-      throw new Error(`Error creating public client for ${deployment}`)
-    }
-
-    const receipt = await publicClient.getTransactionReceipt({
-      hash: txHash as `0x${string}`
-    })
-    if (!receipt) {
-      throw new Error(`Transaction receipt not found for ${txHash}`)
-    }
-    console.log("receipt: ", receipt)
-
-    const logs = parseEventLogs({
-      abi: EntropyAbi,
-      logs: receipt.logs,
-      eventName: "RequestedWithCallback"
-    })
-    if (!logs) {
-      throw new Error(`Error parsing logs for ${txHash}. Are you sure you send the requestCallback Transaction?`)
-    }
-    console.log("logs: ", logs)
-
-    if (logs.length === 0) {
-      throw new Error(`No logs found for ${txHash}. Are you sure you send the requestCallback Transaction?`)
-    }
-
-    const provider = logs[0].args.provider
-    const sequenceNumber = logs[0].args.sequenceNumber
-    const userRandomNumber = logs[0].args.userRandomNumber
-
-    return { provider, sequenceNumber, userRandomNumber }
+export async function fetchInfoFromTx(
+  txHash: string,
+  deployment: EntropyDeployment
+) {
+  const publicClient = createPublicClient({
+    transport: http(deployment.rpc),
+  }).extend(publicActions);
+  if (!publicClient) {
+    throw new Error(`Error creating public client for ${deployment}`);
   }
+
+  const receipt = await publicClient.getTransactionReceipt({
+    hash: txHash as `0x${string}`,
+  });
+  if (!receipt) {
+    throw new Error(`Transaction receipt not found for ${txHash}`);
+  }
+  console.log("receipt: ", receipt);
+
+  const logs = parseEventLogs({
+    abi: EntropyAbi,
+    logs: receipt.logs,
+    eventName: "RequestedWithCallback",
+  });
+  if (!logs) {
+    throw new Error(
+      `Error parsing logs for ${txHash}. Are you sure you send the requestCallback Transaction?`
+    );
+  }
+  console.log("logs: ", logs);
+
+  const logs_zero = logs[0];
+  if (!logs_zero) {
+    throw new Error(
+      `No logs found for ${txHash}. Are you sure you send the requestCallback Transaction?`
+    );
+  }
+
+  const provider = logs_zero.args.provider;
+  const sequenceNumber = logs_zero.args.sequenceNumber;
+  const userRandomNumber = logs_zero.args.userRandomNumber;
+
+  return { provider, sequenceNumber, userRandomNumber };
+}
 
 export async function getRevelation(chain: string, sequenceNumber: number) {
-  const deployment = EntropyDeployments[chain]
+  const deployment = EntropyDeployments[chain];
   if (!deployment) {
-    throw new Error(`Deployment for chain ${chain} not found`)
+    throw new Error(`Deployment for chain ${chain} not found`);
   }
 
-  let response: Response
+  let response: Response;
 
   try {
-      const isMainnet = deployment.network === "mainnet"
-      const baseUrl = isMainnet
-        ? "https://fortuna.dourolabs.app"
-        : "https://fortuna-staging.dourolabs.app"
+    const isMainnet = deployment.network === "mainnet";
+    const baseUrl = isMainnet
+      ? "https://fortuna.dourolabs.app"
+      : "https://fortuna-staging.dourolabs.app";
 
-      response = await fetch(
-        `${baseUrl}/v1/chains/${chain}/revelations/${sequenceNumber}`,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          }
-        }
-      )
-    } catch (error) {
-      console.error("We found an error while fetching the revelation: " + error)
-      throw new Error("We found an error while fetching the revelation: " + error)
-    }
-
-  if (response.status.toString().startsWith("4") || response.status.toString().startsWith("5")) {
-    const errorMessage = await response.text()
-    console.error("The provider returned an error:", errorMessage)
-    throw new Error("The provider returned an error: " + errorMessage)
+    response = await fetch(
+      `${baseUrl}/v1/chains/${chain}/revelations/${sequenceNumber}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.error("We found an error while fetching the revelation: " + error);
+    throw new Error(
+      "We found an error while fetching the revelation: " + error
+    );
   }
 
-  return await response.json()
+  if (
+    response.status.toString().startsWith("4") ||
+    response.status.toString().startsWith("5")
+  ) {
+    const errorMessage = await response.text();
+    console.error("The provider returned an error:", errorMessage);
+    throw new Error("The provider returned an error: " + errorMessage);
+  }
+
+  return await response.json();
 }
