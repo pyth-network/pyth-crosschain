0f0b91f641a2048b9439c7b94bfa6ce53651db8e
Aditya Arora <arora.aditya520@gmail.com>
2025-01-15 14:16:04 -0500

    update

---
+++ apps/entropy-debug/src/lib/utils.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function isValidTxHash(hash: string) {
+  const cleanHash = hash.toLowerCase().replace("0x", "");
+  return /^[a-f0-9]{64}$/.test(cleanHash);
+}
