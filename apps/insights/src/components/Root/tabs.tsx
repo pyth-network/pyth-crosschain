97004ddcc2bec9a018102aa22862b051a4de6fef
Connor Prussin <connor@prussin.net>
2025-02-18 21:09:05 -0800

    wip: add nextjs loading state

--- apps/insights/src/components/Root/tabs.tsx
+++ apps/insights/src/components/Root/tabs.tsx
@@ -27,46 +27,47 @@ export const MainNavTabs = (
   return <MainNavTabsComponent pathname={pathname} {...props} />;
 };
 
-export const TabPanel = ({
-  children,
-  ...props
-}: Omit<ComponentProps<typeof UnstyledTabPanel>, "id">) => {
+export const TabPanel = (
+  props: Omit<ComponentProps<typeof UnstyledTabPanel>, "id">,
+) => {
   const tabId = useSelectedLayoutSegment() ?? "";
 
-  return (
-    <UnstyledTabPanel key="tabpanel" id={tabId} {...props}>
-      {(args) => (
-        <LayoutTransition
-          variants={{
-            initial: (custom) => ({
-              opacity: 0,
-              x: isMovingLeft(custom) ? "-2%" : "2%",
-            }),
-            exit: (custom) => ({
-              opacity: 0,
-              x: isMovingLeft(custom) ? "2%" : "-2%",
-              transition: {
-                x: { type: "spring", bounce: 0 },
-              },
-            }),
-          }}
-          initial="initial"
-          animate={{
-            opacity: 1,
-            x: 0,
-            transition: {
-              x: { type: "spring", bounce: 0 },
-            },
-          }}
-          exit="exit"
-        >
-          {typeof children === "function" ? children(args) : children}
-        </LayoutTransition>
-      )}
-    </UnstyledTabPanel>
-  );
+  return <UnstyledTabPanel key="tabpanel" id={tabId} {...props} />;
+
+  // return (
+  //   <UnstyledTabPanel key="tabpanel" id={tabId} {...props}>
+  //     {(args) => (
+  //       <LayoutTransition
+  //         variants={{
+  //           initial: (custom) => ({
+  //             opacity: 0,
+  //             x: isMovingLeft(custom) ? "-2%" : "2%",
+  //           }),
+  //           exit: (custom) => ({
+  //             opacity: 0,
+  //             x: isMovingLeft(custom) ? "2%" : "-2%",
+  //             transition: {
+  //               x: { type: "spring", bounce: 0 },
+  //             },
+  //           }),
+  //         }}
+  //         initial="initial"
+  //         animate={{
+  //           opacity: 1,
+  //           x: 0,
+  //           transition: {
+  //             x: { type: "spring", bounce: 0 },
+  //           },
+  //         }}
+  //         exit="exit"
+  //       >
+  //         {typeof children === "function" ? children(args) : children}
+  //       </LayoutTransition>
+  //     )}
+  //   </UnstyledTabPanel>
+  // );
 };
 
-const isMovingLeft = ({ segment, prevSegment }: VariantArg): boolean =>
-  segment === null ||
-  (segment === "publishers" && prevSegment === "price-feeds");
+// const isMovingLeft = ({ segment, prevSegment }: VariantArg): boolean =>
+//   segment === null ||
+//   (segment === "publishers" && prevSegment === "price-feeds");
