3646e9140aaa3e7458e088a079bd0d5ac8b440f1
Aditya Arora <arora.aditya520@gmail.com>
2025-03-06 09:51:27 -0500

    chore(entropy) Deploy Hyperevm (#2441)
    * chore(entropy) Deploy Hyperevm
    
    * renamed the chain

--- governance/xc_admin/packages/xc_admin_common/src/chains.ts
+++ governance/xc_admin/packages/xc_admin_common/src/chains.ts
@@ -106,7 +106,7 @@ export const RECEIVER_CHAINS = {
   berachain_mainnet: 60076,
   movement_mainnet: 60077,
   story_mainnet: 60078,
-  hyperEVM: 60079,
+  hyperevm: 60079,
   bittensor_mainnet: 60080,
 
   // Testnets as a separate chain ids (to use stable data sources and governance for them)
