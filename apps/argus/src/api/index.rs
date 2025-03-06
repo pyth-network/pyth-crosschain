f3f80244bdb5bde9fa4812feea5104ade07d81fb
Daniel Chew <cctdaniel@outlook.com>
2025-03-06 13:35:40 +0800

    feat(api): expand index route responses with additional endpoints

--- apps/argus/src/api/index.rs
+++ apps/argus/src/api/index.rs
@@ -4,5 +4,12 @@ use axum::{response::IntoResponse, Json};
 ///
 /// TODO: Dynamically generate this list if possible.
 pub async fn index() -> impl IntoResponse {
-    Json(["/v1/chains", "/v1/chains/:chain_id/revelations/:sequence"])
+    Json([
+        "/",
+        "/live",
+        "/metrics",
+        "/ready",
+        "/v1/chains",
+        "/v1/chains/:chain_id/price-updates/:sequence"
+    ])
 }
