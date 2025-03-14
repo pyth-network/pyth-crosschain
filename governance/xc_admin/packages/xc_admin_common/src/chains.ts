a7aa4e828c82dd12b4d79d2db551cb7da45cd22e
Aditya Arora <arora.aditya520@gmail.com>
2025-03-14 09:31:04 -0400

    chore(contracts) Deploy Berachain (#2484)

--- governance/xc_admin/packages/xc_admin_common/src/chains.ts
+++ governance/xc_admin/packages/xc_admin_common/src/chains.ts
@@ -233,6 +233,7 @@ export const RECEIVER_CHAINS = {
   monad_devnet: 50116,
   monad_testnet: 50117,
   iota_sui_testnet: 50118,
+  berachain_bepolia: 50119,
 };
 
 // If there is any overlapping value the receiver chain will replace the wormhole
