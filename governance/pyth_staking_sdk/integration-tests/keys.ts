c4b937d0950b8046361544ac5bb1867c75b355d6
keyvan <keyvankhademi@gmail.com>
2025-04-03 15:36:34 -0700

    remove integration test

--- governance/pyth_staking_sdk/integration-tests/keys.ts
+++
@@ -1,9 +0,0 @@
-import fs from "node:fs";
-
-import { Keypair } from "@solana/web3.js";
-
-export function loadKeypair(path: string) {
-  return Keypair.fromSecretKey(
-    new Uint8Array(JSON.parse(fs.readFileSync(path, "utf8")) as number[]),
-  );
-}
