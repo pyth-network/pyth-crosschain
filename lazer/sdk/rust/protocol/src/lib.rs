ea14862fdf49fc5a46e40c59178139995ca76ce8
Darun Seethammagari <darunrs@gmail.com>
2025-04-03 16:40:34 -0700

    Update proto file and build generated files

--- lazer/sdk/rust/protocol/src/lib.rs
+++ lazer/sdk/rust/protocol/src/lib.rs
@@ -5,6 +5,9 @@ pub mod binary_update;
 pub mod message;
 pub mod payload;
 pub mod publisher;
+pub mod pyth_lazer {
+    include!(concat!(env!("OUT_DIR"), "/pyth_lazer.rs"));
+}
 pub mod router;
 mod serde_price_as_i64;
 mod serde_str;
