7714abbabd0958786aacf99441ea1a24893ba3d8
Daniel Chew <cctdaniel@outlook.com>
2025-04-11 09:45:45 +0900

    chore: bump version to 1.4.4-alpha.3 in Pyth contract and package.json

--- target_chains/ethereum/contracts/contracts/pyth/Pyth.sol
+++ target_chains/ethereum/contracts/contracts/pyth/Pyth.sol
@@ -556,7 +556,7 @@ abstract contract Pyth is
     }
 
     function version() public pure returns (string memory) {
-        return "1.4.4-alpha.2";
+        return "1.4.4-alpha.3";
     }
 
     function calculateTwap(
