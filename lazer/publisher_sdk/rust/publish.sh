e3243019e69210cdfd890d2aa0e1e64226574ba2
Darun Seethammagari <darunrs@gmail.com>
2025-04-16 10:48:12 -0700

    update publish script

--- lazer/publisher_sdk/rust/publish.sh
+++ lazer/publisher_sdk/rust/publish.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 set -e
 
+PACKAGE_DIR="."
+
 echo "building package to generate type files"
 OUT_DIR=$(cargo build --message-format=json | jq -r 'select(.reason == "build-script-executed") | .out_dir' | grep "pyth-lazer-publisher-sdk")
 
 echo "using output directory: ${OUT_DIR}"
 
 echo "copying files from protobuf output to package directory"
-cp -r "${OUT_DIR}/protobuf" "./src/"
+cp -r "${OUT_DIR}/protobuf" "${PACKAGE_DIR}/src/"
 
 echo "deleting build.rs file"
-rm -f "./build.rs"
+mv "${PACKAGE_DIR}/build.rs"
 
-# echo "updating lib.rs to point to correct artifacts"
-cat > "./src/lib.rs" << EOF
+echo "updating lib.rs to export local protobuf files"
+cat > "${PACKAGE_DIR}/src/lib.rs" << EOF
 pub mod protobuf;
 EOF
+
+echo "removing build spec from Cargo.toml"
+# MacOS sed command is slightly different
+# This script shouldn't be run locally anyway, but I may as well make sure it works
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' '/build.*build\.rs/d' Cargo.toml
+else
+  sed -i '/build.*build\.rs/d' Cargo.toml
+fi
