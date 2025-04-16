27cc261af91f6968bc53cdfa51c580a49c778cde
Darun Seethammagari <darunrs@gmail.com>
2025-04-16 11:05:51 -0700

    Return files to normal

--- lazer/publisher_sdk/rust/src/lib.rs
+++ lazer/publisher_sdk/rust/src/lib.rs
@@ -1,7 +1,11 @@
 pub mod transaction {
     pub use crate::protobuf::pyth_lazer_transaction::*;
 }
+
 pub mod publisher_update {
     pub use crate::protobuf::publisher_update::*;
 }
-mod protobuf;
+
+mod protobuf {
+    include!(concat!(env!("OUT_DIR"), "/protobuf/mod.rs"));
+}
