cbdde626ef228f86f467a0dd0d64bb213b23cecc
Darun Seethammagari <darunrs@gmail.com>
2025-04-16 09:57:34 -0700

    output publisher update types

--- lazer/publisher_sdk/rust/src/lib.rs
+++ lazer/publisher_sdk/rust/src/lib.rs
@@ -2,6 +2,10 @@ pub mod transaction {
     pub use crate::protobuf::pyth_lazer_transaction::*;
 }
 
+pub mod publisher_update {
+    pub use crate::protobuf::publisher_update::*;
+}
+
 mod protobuf {
     include!(concat!(env!("OUT_DIR"), "/protobuf/mod.rs"));
 }
