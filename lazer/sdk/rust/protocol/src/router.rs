961af80f646050468bf47ef7abe3e093a51f2d05
Darun Seethammagari <darunrs@gmail.com>
2025-04-18 13:02:05 -0700

    add impl for timestampus

--- lazer/sdk/rust/protocol/src/router.rs
+++ lazer/sdk/rust/protocol/src/router.rs
@@ -4,6 +4,7 @@ use {
     crate::payload::AggregatedPriceFeedData,
     anyhow::{bail, Context},
     itertools::Itertools,
+    protobuf::{well_known_types::timestamp::Timestamp, MessageField},
     rust_decimal::{prelude::FromPrimitive, Decimal},
     serde::{de::Error, Deserialize, Serialize},
     std::{
@@ -25,6 +26,20 @@ pub struct ChannelId(pub u8);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct TimestampUs(pub u64);
+
+impl TryFrom<&MessageField<Timestamp>> for TimestampUs {
+    type Error = anyhow::Error;
+
+    fn try_from(timestamp_field: &MessageField<Timestamp>) -> anyhow::Result<Self> {
+        if let Some(timestamp) = timestamp_field.clone().into_option() {
+            let seconds_in_micros: u64 = (timestamp.seconds * 1_000_000).try_into()?;
+            let nanos_in_micros: u64 = (timestamp.nanos / 1_000).try_into()?;
+            Ok(TimestampUs(seconds_in_micros + nanos_in_micros))
+        } else {
+            bail!("timestamp field was empty")
+        }
+    }
+}
 
 impl TimestampUs {
     pub fn now() -> Self {
