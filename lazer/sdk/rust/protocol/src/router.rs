a1f2547bb77a1f38249836c9135d54c69f805a7d
Darun Seethammagari <darunrs@gmail.com>
2025-04-22 08:46:37 -0700

    address pr comment

--- lazer/sdk/rust/protocol/src/router.rs
+++ lazer/sdk/rust/protocol/src/router.rs
@@ -4,7 +4,7 @@ use {
     crate::payload::AggregatedPriceFeedData,
     anyhow::{bail, Context},
     itertools::Itertools,
-    protobuf::{well_known_types::timestamp::Timestamp, MessageField},
+    protobuf::well_known_types::timestamp::Timestamp,
     rust_decimal::{prelude::FromPrimitive, Decimal},
     serde::{de::Error, Deserialize, Serialize},
     std::{
@@ -27,17 +27,13 @@ pub struct ChannelId(pub u8);
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct TimestampUs(pub u64);
 
-impl TryFrom<&MessageField<Timestamp>> for TimestampUs {
+impl TryFrom<&Timestamp> for TimestampUs {
     type Error = anyhow::Error;
 
-    fn try_from(timestamp_field: &MessageField<Timestamp>) -> anyhow::Result<Self> {
-        if let Some(timestamp) = timestamp_field.clone().into_option() {
-            let seconds_in_micros: u64 = (timestamp.seconds * 1_000_000).try_into()?;
-            let nanos_in_micros: u64 = (timestamp.nanos / 1_000).try_into()?;
-            Ok(TimestampUs(seconds_in_micros + nanos_in_micros))
-        } else {
-            bail!("timestamp field was empty")
-        }
+    fn try_from(timestamp: &Timestamp) -> anyhow::Result<Self> {
+        let seconds_in_micros: u64 = (timestamp.seconds * 1_000_000).try_into()?;
+        let nanos_in_micros: u64 = (timestamp.nanos / 1_000).try_into()?;
+        Ok(TimestampUs(seconds_in_micros + nanos_in_micros))
     }
 }
 
