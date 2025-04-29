7e63db2ec0f0466040208ffbf6eccec1673a7baa
Tejas Badadare <tejasbadadare@gmail.com>
2025-04-28 18:37:33 -0700

    fix: validate min ts using highest ts in the update data

--- target_chains/ethereum/contracts/contracts/pulse/SchedulerState.sol
+++ target_chains/ethereum/contracts/contracts/pulse/SchedulerState.sol
@@ -10,12 +10,19 @@ contract SchedulerState {
     /// Maximum number of addresses in the reader whitelist
     uint8 public constant MAX_READER_WHITELIST_SIZE = 255;
 
-    // TODO: make these updateable via governance
     /// Maximum time in the past (relative to current block timestamp)
     /// for which a price update timestamp is considered valid
+    /// when validating the update conditions.
+    /// @dev Note: We don't use this when parsing update data from the Pyth contract
+    /// because don't want to reject update data if it contains a price from a market
+    /// that closed a few days ago, since it will contain a timestamp from the last
+    /// trading period. We enforce this value ourselves against the maximum
+    /// timestamp in the provided update data.
     uint64 public constant PAST_TIMESTAMP_MAX_VALIDITY_PERIOD = 1 hours;
+
     /// Maximum time in the future (relative to current block timestamp)
     /// for which a price update timestamp is considered valid
+    /// when validating the update conditions
     uint64 public constant FUTURE_TIMESTAMP_MAX_VALIDITY_PERIOD = 10 seconds;
     /// Fixed gas overhead component used in keeper fee calculation.
     /// This is a rough estimate of the tx overhead for a keeper to call updatePriceFeeds.
