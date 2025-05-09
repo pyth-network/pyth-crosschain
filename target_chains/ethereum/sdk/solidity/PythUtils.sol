b90ec8fdcbf1b62066973159cf694456c87ca35b
Aditya Arora <arora.aditya520@gmail.com>
2025-05-09 14:47:58 -0400

    udpate

--- target_chains/ethereum/sdk/solidity/PythUtils.sol
+++ target_chains/ethereum/sdk/solidity/PythUtils.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "./PythStructs.sol";
+
 library PythUtils {
     /// @notice Converts a Pyth price to a uint256 with a target number of decimals
     /// @param price The Pyth price
