a63891fcef1969fa418f5688acdd9d44f1e2c86e
Aaron Bassett <aaronbassett@gmail.com>
2025-05-19 15:10:21 +0100

    chore: fix linting errors

--- apps/developer-hub/next.config.js
+++ apps/developer-hub/next.config.js
@@ -2,56 +2,56 @@ import createBundleAnalyzer from "@next/bundle-analyzer";
 import { createMDX } from "fumadocs-mdx/next";
 
 const withAnalyzer = createBundleAnalyzer({
-	enabled: process.env.ANALYZE === "true",
+  enabled: process.env.ANALYZE === "true",
 });
 
 const config = {
-	reactStrictMode: true,
-	pageExtensions: ["ts", "tsx", "mdx"],
+  reactStrictMode: true,
+  pageExtensions: ["ts", "tsx", "mdx"],
 
-	logging: {
-		fetches: {
-			fullUrl: true,
-		},
-	},
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/i,
-			use: ["@svgr/webpack"],
-		});
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
 
-		return config;
-	},
+    return config;
+  },
 
-	headers: async () => [
-		{
-			source: "/:path*",
-			headers: [
-				{
-					key: "X-XSS-Protection",
-					value: "1; mode=block",
-				},
-				{
-					key: "Referrer-Policy",
-					value: "strict-origin-when-cross-origin",
-				},
-				{
-					key: "Strict-Transport-Security",
-					value: "max-age=2592000",
-				},
-				{
-					key: "X-Content-Type-Options",
-					value: "nosniff",
-				},
-				{
-					key: "Permissions-Policy",
-					value:
-						"vibrate=(), geolocation=(), midi=(), notifications=(), push=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), speaker=(), vibrate=(), fullscreen=self",
-				},
-			],
-		},
-	],
+  headers: async () => [
+    {
+      source: "/:path*",
+      headers: [
+        {
+          key: "X-XSS-Protection",
+          value: "1; mode=block",
+        },
+        {
+          key: "Referrer-Policy",
+          value: "strict-origin-when-cross-origin",
+        },
+        {
+          key: "Strict-Transport-Security",
+          value: "max-age=2592000",
+        },
+        {
+          key: "X-Content-Type-Options",
+          value: "nosniff",
+        },
+        {
+          key: "Permissions-Policy",
+          value:
+            "vibrate=(), geolocation=(), midi=(), notifications=(), push=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), speaker=(), vibrate=(), fullscreen=self",
+        },
+      ],
+    },
+  ],
 };
 
 const withMDX = createMDX();
