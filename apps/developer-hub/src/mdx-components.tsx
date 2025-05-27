3f298542b9f3ef74430ea0db8a4bd3c5476d9c89
Aaron Bassett <aaronbassett@gmail.com>
2025-05-27 14:57:12 +0100

    chore: automatically make content components available in MDX pages

--- apps/developer-hub/src/mdx-components.tsx
+++ apps/developer-hub/src/mdx-components.tsx
@@ -1,9 +1,16 @@
+import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+
+import { Callout } from "./components/Callout";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Tabs,
+    Tab,
     ...components,
+    // Add custom components as lowercase to follow MDX convention
+    Callout: Callout,
   };
 }
