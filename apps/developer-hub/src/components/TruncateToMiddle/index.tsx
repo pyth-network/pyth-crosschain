1e2042447b188ff3e4d59b25d7e26bc138590c61
Aaron Bassett <aaronbassett@gmail.com>
2025-05-27 14:54:18 +0100

    feat: add component to display strings truncated in the middle

---
+++ apps/developer-hub/src/components/TruncateToMiddle/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./index.module.scss";
+
+export type Props = {
+    text: string
+}
+
+const TruncateToMiddle = ({text}: Props) => {
+  return <span className={styles.truncateToMiddle} data-text-start={text.slice(0, Math.floor(text.length/2))} data-text-end={text.slice(Math.floor(text.length/2) * -1)} />
+}
+
+export default TruncateToMiddle
