577c00febbb29c0ae7b9788d59aaef01c86994b1
Aaron Bassett <aaronbassett@gmail.com>
2025-05-28 17:24:33 +0100

    feat: seperate the navigation and copy actions in CopyAddress

--- apps/developer-hub/src/components/CopyAddress/index.tsx
+++ apps/developer-hub/src/components/CopyAddress/index.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { CopyButton } from "@pythnetwork/component-library/CopyButton";
+import { Link } from "@pythnetwork/component-library/Link";
 
 import TruncateToMiddle from "../TruncateToMiddle";
+import styles from "./index.module.scss";
 
 const CopyAddress = ({ address, url }: { address: string; url?: string }) => {
   return url ? (
-    <form>
-      <CopyButton text={address} formAction={url} type="submit">
+    <div className={styles.address}>
+      <Link href={url} target="_blank" rel="noreferrer">
         <TruncateToMiddle text={address} />
-      </CopyButton>
-    </form>
+      </Link>
+      <CopyButton text={address} iconOnly />
+    </div>
   ) : (
     <CopyButton text={address}>
       <TruncateToMiddle text={address} />
