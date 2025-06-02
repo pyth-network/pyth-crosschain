5280cdba6ca72d42c89ad56e33a43b8d7d3af059
Aditya Arora <arora.aditya520@gmail.com>
2025-06-02 14:34:22 +0200

    fix(price-pusher) Ignore Invlide PF

--- apps/price_pusher/src/solana/solana.ts
+++ apps/price_pusher/src/solana/solana.ts
@@ -118,6 +118,7 @@ export class SolanaPricePusher implements IPricePusher {
         shuffledPriceIds,
         {
           encoding: "base64",
+          ignoreInvalidPriceIds: true,
         },
       );
       priceFeedUpdateData = response.binary.data;
@@ -211,6 +212,7 @@ export class SolanaPricePusherJito implements IPricePusher {
     try {
       const response = await this.hermesClient.getLatestPriceUpdates(priceIds, {
         encoding: "base64",
+        ignoreInvalidPriceIds: true,
       });
       priceFeedUpdateData = response.binary.data;
     } catch (err: any) {
