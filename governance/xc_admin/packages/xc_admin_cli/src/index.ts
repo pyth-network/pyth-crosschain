8818bb43530253783b7bbfd1c720c7072b4e0321
Guillermo Bescos <g.bescos@yahoo.com>
2025-06-02 16:11:52 +0100

    feat: do it

--- governance/xc_admin/packages/xc_admin_cli/src/index.ts
+++ governance/xc_admin/packages/xc_admin_cli/src/index.ts
@@ -419,7 +419,13 @@ multisigCommand(
           ),
         ),
       )
-    ).flat();
+    ).map((stakeAccounts, index) => {
+      if (stakeAccounts.length === 0) {
+        console.log(`Skipping vote account ${voteAccounts[index].toBase58()} - no stake accounts found`);
+      }
+      return stakeAccounts;
+    })
+    .flat();
 
     const instructions = stakeAccounts.flatMap(
       (stakeAccount) =>
