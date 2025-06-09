d478132185fb0ef58bff8169468984d0a2c352ce
Amin Moghaddam <amin@pyth.network>
2025-06-09 15:39:33 -0700

    fix

--- apps/entropy-tester/src/index.ts
+++ apps/entropy-tester/src/index.ts
@@ -1,176 +1,195 @@
-import fs from "node:fs";
+import fs from "node:fs/promises";
 
-import type {PrivateKey} from "@pythnetwork/contract-manager/core/base";
-import {
-    toPrivateKey
-} from "@pythnetwork/contract-manager/core/base";
+import type { PrivateKey } from "@pythnetwork/contract-manager/core/base";
+import { toPrivateKey } from "@pythnetwork/contract-manager/core/base";
 import { EvmEntropyContract } from "@pythnetwork/contract-manager/core/contracts/evm";
 import { DefaultStore } from "@pythnetwork/contract-manager/node/store";
-import type {Logger} from "pino";
-import {  pino } from "pino";
-import YAML from "yaml";
+import type { Logger } from "pino";
+import { pino } from "pino";
+import type { ArgumentsCamelCase, InferredOptionTypes } from "yargs";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-
 type DurationSeconds = number;
 type LoadedConfig = {
-    contract: EvmEntropyContract;
-    interval: DurationSeconds;
+  contract: EvmEntropyContract;
+  interval: DurationSeconds;
 };
 
 function timeToSeconds(timeStr: string): number {
-    const match = /^(\d+)([hms])$/i.exec(timeStr);
-    if (!match?.[1] || !match[2])
-        throw new Error("Invalid format. Use formats like '6h', '15m', or '30s'.");
+  const match = /^(\d+)([hms])$/i.exec(timeStr);
+  if (!match?.[1] || !match[2])
+    throw new Error("Invalid format. Use formats like '6h', '15m', or '30s'.");
 
-    const value = Number.parseInt(match[1], 10);
-    const unit = match[2].toLowerCase();
+  const value = Number.parseInt(match[1], 10);
+  const unit = match[2].toLowerCase();
 
-    switch (unit) {
-        case "h": {
-            return value * 3600;
-        }
-        case "m": {
-            return value * 60;
-        }
-        case "s": {
-            return value;
-        }
-        default: {
-            throw new Error("Unsupported time unit.");
-        }
+  switch (unit) {
+    case "h": {
+      return value * 3600;
     }
+    case "m": {
+      return value * 60;
+    }
+    case "s": {
+      return value;
+    }
+    default: {
+      throw new Error("Unsupported time unit.");
+    }
+  }
 }
 
-function loadConfig(configPath: string): LoadedConfig[] {
-    const configs: any = YAML.parse(fs.readFileSync(configPath, "utf8"));
-    const loadedConfigs = [];
-    for (const config of configs) {
-        const interval = timeToSeconds(config.interval);
-        const contracts = Object.values(DefaultStore.entropy_contracts).filter(
-            (contract) => contract.chain.getId() == config["chain-id"],
-        );
-        if (contracts.length === 0) {
-            throw new Error(
-                `Can not find the contract for chain ${config["chain-id"]}, check contract manager store.`,
-            );
-        }
-        if (contracts.length > 1) {
-            throw new Error(
-                `Multiple contracts found for chain ${config["chain-id"]}, check contract manager store.`,
-            );
-        }
-        loadedConfigs.push({ contract: contracts[0]!, interval });
+type ParsedConfig = {
+  "chain-id": string;
+  interval: string;
+};
+
+async function loadConfig(configPath: string): Promise<LoadedConfig[]> {
+  const configs = (await import(configPath, {
+    with: { type: "json" },
+  })) as { default: ParsedConfig[] };
+  const loadedConfigs = configs.default.map((config) => {
+    const interval = timeToSeconds(config.interval);
+    const contracts = Object.values(DefaultStore.entropy_contracts).filter(
+      (contract) => contract.chain.getId() == config["chain-id"],
+    );
+    if (contracts.length === 0 || !contracts[0]) {
+      throw new Error(
+        `Can not find the contract for chain ${config["chain-id"]}, check contract manager store.`,
+      );
     }
-    return loadedConfigs;
+    if (contracts.length > 1) {
+      throw new Error(
+        `Multiple contracts found for chain ${config["chain-id"]}, check contract manager store.`,
+      );
+    }
+    return { contract: contracts[0], interval };
+  });
+  return loadedConfigs;
 }
 
 async function testLatency(
-    contract: EvmEntropyContract,
-    privateKey: PrivateKey,
-    logger: Logger,
+  contract: EvmEntropyContract,
+  privateKey: PrivateKey,
+  logger: Logger,
 ) {
-    const provider = await contract.getDefaultProvider();
-    const userRandomNumber = contract.generateUserRandomNumber();
-    const requestResponse = await contract.requestRandomness(
-        userRandomNumber,
-        provider,
-        privateKey,
-        true, // with callback
-    );
-    // Read the sequence number for the request from the transaction events.
-    const sequenceNumber = Number.parseInt(
-        requestResponse.events.RequestedWithCallback.returnValues.sequenceNumber,
-    );
-    logger.info(
-        { sequenceNumber, txHash: requestResponse.transactionHash },
-        `Request submitted`,
-    );
+  const provider = await contract.getDefaultProvider();
+  const userRandomNumber = contract.generateUserRandomNumber();
+  const requestResponse = (await contract.requestRandomness(
+    userRandomNumber,
+    provider,
+    privateKey,
+    true, // with callback
+  )) as {
+    transactionHash: string;
+    events: {
+      RequestedWithCallback: {
+        returnValues: {
+          sequenceNumber: string;
+        };
+      };
+    };
+  };
+  // Read the sequence number for the request from the transaction events.
+  const sequenceNumber = Number.parseInt(
+    requestResponse.events.RequestedWithCallback.returnValues.sequenceNumber,
+  );
+  logger.info(
+    { sequenceNumber, txHash: requestResponse.transactionHash },
+    `Request submitted`,
+  );
 
-    const startTime = Date.now();
+  const startTime = Date.now();
 
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  while (true) {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const request = await contract.getRequest(provider, sequenceNumber);
+    logger.debug(request);
 
-    while (true) {
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-        const request = await contract.getRequest(provider, sequenceNumber);
-        logger.debug(request);
-
-        if (Number.parseInt(request.sequenceNumber) === 0) {
-            // 0 means the request is cleared
-            const endTime = Date.now();
-            logger.info(
-                { sequenceNumber, latency: endTime - startTime },
-                `Successful callback`,
-            );
-            break;
-        }
-        if (Date.now() - startTime > 60_000) {
-            logger.error(
-                { sequenceNumber },
-                "Timeout: 60s passed without the callback being called",
-            );
-            break;
-        }
+    if (Number.parseInt(request.sequenceNumber) === 0) {
+      // 0 means the request is cleared
+      const endTime = Date.now();
+      logger.info(
+        { sequenceNumber, latency: endTime - startTime },
+        `Successful callback`,
+      );
+      break;
     }
+    if (Date.now() - startTime > 60_000) {
+      logger.error(
+        { sequenceNumber },
+        "Timeout: 60s passed without the callback being called",
+      );
+      break;
+    }
+  }
 }
+
+const RUN_OPTIONS = {
+  validate: {
+    description: "Only validate the configs and exit",
+    type: "boolean",
+    default: false,
+    required: false,
+  },
+  config: {
+    description: "Yaml config file",
+    type: "string",
+    required: true,
+  },
+  "private-key": {
+    type: "string",
+    required: true,
+    description:
+      "Path to the private key to sign the transactions with. Should be hex encoded",
+  },
+} as const;
 
 export const main = function () {
-    yargs(hideBin(process.argv))
+  yargs(hideBin(process.argv))
     .parserConfiguration({
-        "parse-numbers": false,
+      "parse-numbers": false,
     })
     .command({
-        command: "run",
-        describe: "run the tester until manually stopped",
-        builder: {
-            validate: {
-                description: "Only validate the configs and exit",
-                type: "boolean",
-                default: false,
-                required: false,
-            },
-            config: {
-                description: "Yaml config file",
-                type: "string",
-                required: true,
-            },
-            "private-key": {
-                type: "string",
-                required: true,
-                description:
-                    "Path to the private key to sign the transactions with. Should be hex encoded",
-            },
-        },
-        handler: async (argv: any) => {
-            const logger = pino();
-            const configs = loadConfig(argv.config);
-            if (argv.validate) {
-                logger.info("Config validated");
-                return;
+      command: "run",
+      describe: "run the tester until manually stopped",
+      builder: RUN_OPTIONS,
+      handler: async (
+        argv: ArgumentsCamelCase<InferredOptionTypes<typeof RUN_OPTIONS>>,
+      ) => {
+        const logger = pino();
+        const configs = await loadConfig(argv.config);
+        if (argv.validate) {
+          logger.info("Config validated");
+          return;
+        }
+        const privateKeyFileContent = await fs.readFile(
+          argv["private-key"],
+          "utf8",
+        );
+        const privateKey = toPrivateKey(
+          privateKeyFileContent.replace("0x", "").trimEnd(),
+        );
+        logger.info("Running");
+        const promises = configs.map(async ({ contract, interval }) => {
+          const child = logger.child({ chain: contract.chain.getId() });
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+          while (true) {
+            try {
+              await testLatency(contract, privateKey, child);
+            } catch (error) {
+              child.error(error, "Error testing latency");
             }
-            const privateKey = toPrivateKey(
-                fs
-                    .readFileSync(argv["private-key"], "utf8")
-                    .replace("0x", "")
-                    .trimEnd(),
+            await new Promise((resolve) =>
+              setTimeout(resolve, interval * 1000),
             );
-            logger.info("Running");
-            const promises = configs.map(async ({ contract, interval }) => {
-                const child = logger.child({ chain: contract.chain.getId() });
-
-                while (true) {
-                    try {
-                        await testLatency(contract, privateKey, child);
-                    } catch (error) {
-                        child.error(error, "Error testing latency");
-                    }
-                    await new Promise((resolve) => setTimeout(resolve, interval * 1000));
-                }
-            });
-            await Promise.all(promises);
-        },
+          }
+        });
+        await Promise.all(promises);
+      },
     })
     .demandCommand()
-    .help().argv;
-}
+    .help();
+};
