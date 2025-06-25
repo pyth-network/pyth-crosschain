9def52073a5776dee3cfb83165dbdfe8b40d75a8
Amin Moghaddam <amin@pyth.network>
2025-06-25 09:08:19 -0700

    fix(contract_manager): cleanup

--- contract_manager/src/node/utils/shell.ts
+++ contract_manager/src/node/utils/shell.ts
@@ -5,14 +5,14 @@ const service = tsNode.create({ ...repl.evalAwarePartialHost });
 repl.setService(service);
 repl.start();
 repl.evalCode(
-  "import { loadHotWallet, Vault } from './src/governance';" +
-    "import { SuiChain, CosmWasmChain, AptosChain, EvmChain, StarknetChain } from './src/chains';" +
-    "import { SuiPriceFeedContract } from './src/contracts/sui';" +
-    "import { CosmWasmWormholeContract, CosmWasmPriceFeedContract } from './src/contracts/cosmwasm';" +
-    "import { EvmWormholeContract, EvmPriceFeedContract, EvmEntropyContract, EvmExpressRelayContract } from './src/contracts/evm';" +
-    "import { AptosWormholeContract, AptosPriceFeedContract } from './src/contracts/aptos';" +
-    "import { StarknetPriceFeedContract } from './src/contracts/starknet';" +
-    "import { DefaultStore } from './src/store';" +
-    "import { toPrivateKey } from './src/base';" +
+  "import { loadHotWallet, Vault } from './src/node/utils/governance';" +
+    "import { SuiChain, CosmWasmChain, AptosChain, EvmChain, StarknetChain } from './src/core/chains';" +
+    "import { SuiPriceFeedContract } from './src/core/contracts/sui';" +
+    "import { CosmWasmWormholeContract, CosmWasmPriceFeedContract } from './src/core/contracts/cosmwasm';" +
+    "import { EvmWormholeContract, EvmPriceFeedContract, EvmEntropyContract } from './src/core/contracts/evm';" +
+    "import { AptosWormholeContract, AptosPriceFeedContract } from './src/core/contracts/aptos';" +
+    "import { StarknetPriceFeedContract } from './src/core/contracts/starknet';" +
+    "import { DefaultStore } from './src/node/utils/store';" +
+    "import { toPrivateKey } from './src/core/base';" +
     "DefaultStore",
 );
