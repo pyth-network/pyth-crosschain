f5fc539c38ecf544db581cb721abf54d5ef3c072
Danial Mehrjerdi <dmehrjerdi@gmail.com>
2025-07-02 17:08:27 +0200

    Address comments

--- lazer/sdk/rust/protocol/src/symbol_state.rs
+++ lazer/sdk/rust/protocol/src/symbol_state.rs
@@ -6,16 +6,16 @@ use {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum SymbolState {
-    Stable,
     ComingSoon,
+    Stable,
     Deprecated,
 }
 
 impl Display for SymbolState {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            SymbolState::Stable => write!(f, "stable"),
             SymbolState::ComingSoon => write!(f, "coming_soon"),
+            SymbolState::Stable => write!(f, "stable"),
             SymbolState::Deprecated => write!(f, "deprecated"),
         }
     }
