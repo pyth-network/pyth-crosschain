616fc3e531bd183fc5e26df2095e89d6ea4ab3bd
Ayush Suresh <byteslayer31037@gmail.com>
2025-07-11 16:31:10 -0500

    pushing governancepayload struct

--- target_chains/stylus/contracts/pyth-receiver/src/governance_structs.rs
+++ target_chains/stylus/contracts/pyth-receiver/src/governance_structs.rs
@@ -18,6 +18,18 @@ pub enum GovernanceAction {
     SetFeeInToken,
 }
 
+#[derive(Drop, Clone, Debug, PartialEq, Serde)]
+pub enum GovernancePayload {
+    UpgradeContract: UpgradeContract,
+    AuthorizeGovernanceDataSourceTransfer: AuthorizeGovernanceDataSourceTransfer,
+    SetDataSources: SetDataSources,
+    SetFee: SetFee,
+    // SetValidPeriod is unsupported
+    RequestGovernanceDataSourceTransfer: RequestGovernanceDataSourceTransfer,
+    SetWormholeAddress: SetWormholeAddress,
+    SetFeeInToken: SetFeeInToken,
+}
+
 impl TryFrom<u8> for GovernanceAction {
     type Error = PythReceiverError;
 
