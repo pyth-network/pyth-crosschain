b1e808e28898c483ff231e5a54c4850ed08f7086
Pavel Strakhov <ri@idzaaus.org>
2025-07-16 10:59:47 +0100

    feat(lazer): add DynamicValue parsed type

---
+++ lazer/publisher_sdk/rust/src/dynamic_value.rs
@@ -0,0 +1,225 @@
+use std::collections::BTreeMap;
+
+use crate::protobuf::dynamic_value::{dynamic_value, DynamicValue as ProtobufDynamicValue};
+use ::protobuf::MessageField;
+use anyhow::{ensure, Context};
+use derive_more::From;
+use pyth_lazer_protocol::time::{DurationUs, TimestampUs};
+use serde::{
+    ser::{SerializeMap, SerializeSeq},
+    Serialize,
+};
+
+#[derive(Debug, Clone, PartialEq, From)]
+pub enum DynamicValue {
+    String(String),
+    F64(f64),
+    U64(u64),
+    I64(i64),
+    Bool(bool),
+    Timestamp(TimestampUs),
+    Duration(DurationUs),
+    Bytes(Vec<u8>),
+    List(Vec<DynamicValue>),
+    Map(BTreeMap<String, DynamicValue>),
+}
+
+impl From<DynamicValue> for ProtobufDynamicValue {
+    fn from(value: DynamicValue) -> Self {
+        let converted = match value {
+            DynamicValue::Bool(value) => dynamic_value::Value::BoolValue(value),
+            DynamicValue::U64(value) => dynamic_value::Value::UintValue(value),
+            DynamicValue::I64(value) => dynamic_value::Value::IntValue(value),
+            DynamicValue::F64(value) => dynamic_value::Value::DoubleValue(value),
+            DynamicValue::String(value) => dynamic_value::Value::StringValue(value),
+            DynamicValue::Bytes(value) => dynamic_value::Value::BytesValue(value),
+            DynamicValue::Timestamp(value) => dynamic_value::Value::TimestampValue(value.into()),
+            DynamicValue::Duration(value) => dynamic_value::Value::DurationValue(value.into()),
+            DynamicValue::List(values) => dynamic_value::Value::List(values.into()),
+            DynamicValue::Map(values) => dynamic_value::Value::Map(values.into()),
+        };
+        ProtobufDynamicValue {
+            value: Some(converted),
+            special_fields: Default::default(),
+        }
+    }
+}
+
+impl From<&DynamicValue> for ProtobufDynamicValue {
+    fn from(value: &DynamicValue) -> Self {
+        let converted = match value {
+            DynamicValue::Bool(value) => dynamic_value::Value::BoolValue(*value),
+            DynamicValue::U64(value) => dynamic_value::Value::UintValue(*value),
+            DynamicValue::I64(value) => dynamic_value::Value::IntValue(*value),
+            DynamicValue::F64(value) => dynamic_value::Value::DoubleValue(*value),
+            DynamicValue::String(value) => dynamic_value::Value::StringValue(value.clone()),
+            DynamicValue::Bytes(value) => dynamic_value::Value::BytesValue(value.clone()),
+            DynamicValue::Timestamp(value) => dynamic_value::Value::TimestampValue((*value).into()),
+            DynamicValue::Duration(value) => dynamic_value::Value::DurationValue((*value).into()),
+            DynamicValue::List(values) => dynamic_value::Value::List(values.into()),
+            DynamicValue::Map(values) => dynamic_value::Value::Map(values.into()),
+        };
+        ProtobufDynamicValue {
+            value: Some(converted),
+            special_fields: Default::default(),
+        }
+    }
+}
+
+impl From<BTreeMap<String, DynamicValue>> for dynamic_value::Map {
+    fn from(values: BTreeMap<String, DynamicValue>) -> Self {
+        let mut items = Vec::new();
+        for (key, value) in values {
+            items.push(dynamic_value::MapItem {
+                key: Some(key),
+                value: MessageField::some(value.into()),
+                special_fields: Default::default(),
+            })
+        }
+        dynamic_value::Map {
+            items,
+            special_fields: Default::default(),
+        }
+    }
+}
+
+impl From<&BTreeMap<String, DynamicValue>> for dynamic_value::Map {
+    fn from(values: &BTreeMap<String, DynamicValue>) -> Self {
+        let mut items = Vec::new();
+        for (key, value) in values {
+            items.push(dynamic_value::MapItem {
+                key: Some(key.clone()),
+                value: MessageField::some(value.into()),
+                special_fields: Default::default(),
+            })
+        }
+        dynamic_value::Map {
+            items,
+            special_fields: Default::default(),
+        }
+    }
+}
+
+impl From<Vec<DynamicValue>> for dynamic_value::List {
+    fn from(values: Vec<DynamicValue>) -> Self {
+        let mut items = Vec::new();
+        for value in values {
+            items.push(value.into());
+        }
+        dynamic_value::List {
+            items,
+            special_fields: Default::default(),
+        }
+    }
+}
+
+impl From<&[DynamicValue]> for dynamic_value::List {
+    fn from(values: &[DynamicValue]) -> Self {
+        let mut items = Vec::new();
+        for value in values {
+            items.push(value.into());
+        }
+        dynamic_value::List {
+            items,
+            special_fields: Default::default(),
+        }
+    }
+}
+
+impl From<&Vec<DynamicValue>> for dynamic_value::List {
+    fn from(value: &Vec<DynamicValue>) -> Self {
+        let value: &[DynamicValue] = value;
+        value.into()
+    }
+}
+
+impl TryFrom<ProtobufDynamicValue> for DynamicValue {
+    type Error = anyhow::Error;
+
+    fn try_from(value: ProtobufDynamicValue) -> Result<Self, Self::Error> {
+        let value = value.value.context("missing DynamicValue.value")?;
+        match value {
+            dynamic_value::Value::StringValue(value) => Ok(DynamicValue::String(value)),
+            dynamic_value::Value::DoubleValue(value) => Ok(DynamicValue::F64(value)),
+            dynamic_value::Value::UintValue(value) => Ok(DynamicValue::U64(value)),
+            dynamic_value::Value::IntValue(value) => Ok(DynamicValue::I64(value)),
+            dynamic_value::Value::BoolValue(value) => Ok(DynamicValue::Bool(value)),
+            dynamic_value::Value::BytesValue(value) => Ok(DynamicValue::Bytes(value)),
+            dynamic_value::Value::DurationValue(value) => {
+                let v: DurationUs = value.try_into()?;
+                Ok(DynamicValue::Duration(v))
+            }
+            dynamic_value::Value::TimestampValue(ts) => {
+                let ts = TimestampUs::try_from(&ts)?;
+                Ok(DynamicValue::Timestamp(ts))
+            }
+            dynamic_value::Value::List(list) => Ok(DynamicValue::List(list.try_into()?)),
+            dynamic_value::Value::Map(map) => Ok(DynamicValue::Map(map.try_into()?)),
+        }
+    }
+}
+
+impl TryFrom<dynamic_value::Map> for BTreeMap<String, DynamicValue> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: dynamic_value::Map) -> Result<Self, Self::Error> {
+        let mut output = BTreeMap::new();
+        for item in value.items {
+            let key = item.key.context("missing DynamicValue.MapItem.key")?;
+            let value = item
+                .value
+                .into_option()
+                .context("missing DynamicValue.MapItem.value")?
+                .try_into()?;
+            let old = output.insert(key, value);
+            ensure!(old.is_none(), "duplicate DynamicValue.MapItem.key");
+        }
+        Ok(output)
+    }
+}
+
+impl TryFrom<dynamic_value::List> for Vec<DynamicValue> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: dynamic_value::List) -> Result<Self, Self::Error> {
+        let mut output = Vec::new();
+        for item in value.items {
+            output.push(item.try_into()?);
+        }
+        Ok(output)
+    }
+}
+
+impl Serialize for DynamicValue {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        match self {
+            DynamicValue::String(v) => serializer.serialize_str(v),
+            DynamicValue::F64(v) => serializer.serialize_f64(*v),
+            DynamicValue::U64(v) => serializer.serialize_u64(*v),
+            DynamicValue::I64(v) => serializer.serialize_i64(*v),
+            DynamicValue::Bool(v) => serializer.serialize_bool(*v),
+            DynamicValue::Timestamp(v) => serializer.serialize_u64(v.as_micros()),
+            DynamicValue::Duration(v) => {
+                serializer.serialize_str(&humantime::format_duration((*v).into()).to_string())
+            }
+            DynamicValue::Bytes(v) => serializer.serialize_bytes(v),
+            DynamicValue::List(v) => {
+                let mut seq_serializer = serializer.serialize_seq(Some(v.len()))?;
+                for element in v {
+                    seq_serializer.serialize_element(element)?;
+                }
+                seq_serializer.end()
+            }
+            DynamicValue::Map(map) => {
+                let mut map_serializer = serializer.serialize_map(Some(map.len()))?;
+                for (k, v) in map {
+                    map_serializer.serialize_entry(k, v)?;
+                }
+                map_serializer.end()
+            }
+        }
+    }
+}
--- lazer/publisher_sdk/rust/src/lib.rs
+++ lazer/publisher_sdk/rust/src/lib.rs
@@ -1,13 +1,8 @@
-use std::{collections::BTreeMap, time::Duration};
-
 use crate::publisher_update::feed_update::Update;
 use crate::publisher_update::{FeedUpdate, FundingRateUpdate, PriceUpdate};
-use ::protobuf::MessageField;
-use anyhow::{bail, ensure, Context};
-use humantime::format_duration;
-use protobuf::dynamic_value::{dynamic_value, DynamicValue};
+use crate::state::FeedState;
 use pyth_lazer_protocol::jrpc::{FeedUpdateParams, UpdateParams};
-use pyth_lazer_protocol::time::TimestampUs;
+use pyth_lazer_protocol::symbol_state::SymbolState;
 
 pub mod transaction_envelope {
     pub use crate::protobuf::transaction_envelope::*;
@@ -34,139 +29,9 @@ mod protobuf {
     include!(concat!(env!("OUT_DIR"), "/protobuf/mod.rs"));
 }
 
-impl DynamicValue {
-    pub fn try_option_from_serde(value: serde_value::Value) -> anyhow::Result<Option<Self>> {
-        match value {
-            serde_value::Value::Option(value) => {
-                if let Some(value) = value {
-                    Ok(Some((*value).try_into()?))
-                } else {
-                    Ok(None)
-                }
-            }
-            value => Ok(Some(value.try_into()?)),
-        }
-    }
+mod dynamic_value;
 
-    pub fn to_timestamp(&self) -> anyhow::Result<TimestampUs> {
-        let value = self.value.as_ref().context("missing DynamicValue.value")?;
-        match value {
-            dynamic_value::Value::TimestampValue(ts) => Ok(ts.try_into()?),
-            _ => bail!("expected timestamp, got {:?}", self),
-        }
-    }
-
-    pub fn to_duration(&self) -> anyhow::Result<Duration> {
-        let value = self.value.as_ref().context("missing DynamicValue.value")?;
-        match value {
-            dynamic_value::Value::DurationValue(duration) => Ok(duration.clone().into()),
-            _ => bail!("expected duration, got {:?}", self),
-        }
-    }
-}
-
-impl TryFrom<serde_value::Value> for DynamicValue {
-    type Error = anyhow::Error;
-
-    fn try_from(value: serde_value::Value) -> Result<Self, Self::Error> {
-        let converted = match value {
-            serde_value::Value::Bool(value) => dynamic_value::Value::BoolValue(value),
-            serde_value::Value::U8(value) => dynamic_value::Value::UintValue(value.into()),
-            serde_value::Value::U16(value) => dynamic_value::Value::UintValue(value.into()),
-            serde_value::Value::U32(value) => dynamic_value::Value::UintValue(value.into()),
-            serde_value::Value::U64(value) => dynamic_value::Value::UintValue(value),
-            serde_value::Value::I8(value) => dynamic_value::Value::IntValue(value.into()),
-            serde_value::Value::I16(value) => dynamic_value::Value::IntValue(value.into()),
-            serde_value::Value::I32(value) => dynamic_value::Value::IntValue(value.into()),
-            serde_value::Value::I64(value) => dynamic_value::Value::IntValue(value),
-            serde_value::Value::F32(value) => dynamic_value::Value::DoubleValue(value.into()),
-            serde_value::Value::F64(value) => dynamic_value::Value::DoubleValue(value),
-            serde_value::Value::Char(value) => dynamic_value::Value::StringValue(value.to_string()),
-            serde_value::Value::String(value) => dynamic_value::Value::StringValue(value),
-            serde_value::Value::Bytes(value) => dynamic_value::Value::BytesValue(value),
-            serde_value::Value::Seq(values) => {
-                let mut items = Vec::new();
-                for value in values {
-                    items.push(value.try_into()?);
-                }
-                dynamic_value::Value::List(dynamic_value::List {
-                    items,
-                    special_fields: Default::default(),
-                })
-            }
-            serde_value::Value::Map(values) => {
-                let mut items = Vec::new();
-                for (key, value) in values {
-                    let key = match key {
-                        serde_value::Value::String(key) => key,
-                        _ => bail!("unsupported key type: expected string, got {:?}", key),
-                    };
-                    items.push(dynamic_value::MapItem {
-                        key: Some(key),
-                        value: MessageField::some(value.try_into()?),
-                        special_fields: Default::default(),
-                    })
-                }
-                dynamic_value::Value::Map(dynamic_value::Map {
-                    items,
-                    special_fields: Default::default(),
-                })
-            }
-            serde_value::Value::Unit
-            | serde_value::Value::Option(_)
-            | serde_value::Value::Newtype(_) => bail!("unsupported type: {:?}", value),
-        };
-        Ok(DynamicValue {
-            value: Some(converted),
-            special_fields: Default::default(),
-        })
-    }
-}
-
-impl TryFrom<DynamicValue> for serde_value::Value {
-    type Error = anyhow::Error;
-
-    fn try_from(value: DynamicValue) -> Result<Self, Self::Error> {
-        let value = value.value.context("missing DynamicValue.value")?;
-        match value {
-            dynamic_value::Value::StringValue(value) => Ok(serde_value::Value::String(value)),
-            dynamic_value::Value::DoubleValue(value) => Ok(serde_value::Value::F64(value)),
-            dynamic_value::Value::UintValue(value) => Ok(serde_value::Value::U64(value)),
-            dynamic_value::Value::IntValue(value) => Ok(serde_value::Value::I64(value)),
-            dynamic_value::Value::BoolValue(value) => Ok(serde_value::Value::Bool(value)),
-            dynamic_value::Value::BytesValue(value) => Ok(serde_value::Value::Bytes(value)),
-            dynamic_value::Value::DurationValue(duration) => {
-                let s: Duration = duration.into();
-                Ok(serde_value::Value::String(format_duration(s).to_string()))
-            }
-            dynamic_value::Value::TimestampValue(ts) => {
-                let ts = TimestampUs::try_from(&ts)?;
-                Ok(serde_value::Value::U64(ts.as_micros()))
-            }
-            dynamic_value::Value::List(list) => {
-                let mut output = Vec::new();
-                for item in list.items {
-                    output.push(item.try_into()?);
-                }
-                Ok(serde_value::Value::Seq(output))
-            }
-            dynamic_value::Value::Map(map) => {
-                let mut output = BTreeMap::new();
-                for item in map.items {
-                    let key = item.key.context("missing DynamicValue.MapItem.key")?;
-                    let value = item
-                        .value
-                        .into_option()
-                        .context("missing DynamicValue.MapItem.value")?
-                        .try_into()?;
-                    let old = output.insert(serde_value::Value::String(key), value);
-                    ensure!(old.is_none(), "duplicate DynamicValue.MapItem.key");
-                }
-                Ok(serde_value::Value::Map(output))
-            }
-        }
-    }
-}
+pub use crate::dynamic_value::DynamicValue;
 
 impl From<FeedUpdateParams> for FeedUpdate {
     fn from(value: FeedUpdateParams) -> Self {
@@ -199,6 +64,26 @@ impl From<UpdateParams> for Update {
                     special_fields: Default::default(),
                 })
             }
+        }
+    }
+}
+
+impl From<FeedState> for SymbolState {
+    fn from(value: FeedState) -> Self {
+        match value {
+            FeedState::COMING_SOON => SymbolState::ComingSoon,
+            FeedState::STABLE => SymbolState::Stable,
+            FeedState::INACTIVE => SymbolState::Inactive,
+        }
+    }
+}
+
+impl From<SymbolState> for FeedState {
+    fn from(value: SymbolState) -> Self {
+        match value {
+            SymbolState::ComingSoon => FeedState::COMING_SOON,
+            SymbolState::Stable => FeedState::STABLE,
+            SymbolState::Inactive => FeedState::INACTIVE,
         }
     }
 }
