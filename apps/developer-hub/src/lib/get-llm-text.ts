4a14b16d753ad8444c407a14023958ad318ed11e
Aditya Arora <arora.aditya520@gmail.com>
2025-09-12 16:26:10 -0400

    chore(dev-hub) Move source to lib

---
+++ apps/developer-hub/src/lib/get-llm-text.ts
@@ -0,0 +1,27 @@
+// import fs from 'node:fs/promises';
+
+// import type { InferPageType } from 'fumadocs-core/source';
+// import { remarkInclude } from 'fumadocs-mdx/config';
+// import { remark } from 'remark';
+// import remarkGfm from 'remark-gfm';
+// import remarkMdx from 'remark-mdx';
+
+// import { source } from './source';
+
+// const processor = remark()
+//   .use(remarkMdx)
+//   .use(remarkInclude)
+//   .use(remarkGfm)
+
+// export async function getLLMText(page: InferPageType<typeof source>) {
+//   const processed = await processor.process({
+//     path: page.absolutePath,
+//     value: await fs.readFile(page.absolutePath, 'utf8'),
+//   });
+
+//   // note: it doesn't escape frontmatter, it's up to you.
+//   return `# ${page.data.title}
+// URL: ${page.url}
+
+// ${processed.value}`;
+// }
