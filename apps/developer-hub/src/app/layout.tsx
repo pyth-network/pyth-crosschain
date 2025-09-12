9e95516aba813319219cb225a58f9db93d096700
nidhi-singh02 <nidhi2894@gmail.com>
2025-09-12 19:41:07 +0530

    build issues fix

--- apps/developer-hub/src/app/layout.tsx
+++ apps/developer-hub/src/app/layout.tsx
@@ -5,8 +5,6 @@ import { Root } from "../components/Root";
 
 export { metadata, viewport } from "../metadata";
 
-import "katex/dist/katex.css";
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   return <Root>{children}</Root>;
 }
