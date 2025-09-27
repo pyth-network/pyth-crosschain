f9fe1d2b8e288b13704699b075194d9f646b88c3
Aditya Arora <arora.aditya520@gmail.com>
2025-09-27 14:36:10 +0530

    feat(components-library) Social Links Drawer

--- packages/component-library/src/social-links.tsx
+++ packages/component-library/src/social-links.tsx
@@ -1,14 +1,21 @@
+import { Code } from "@phosphor-icons/react/dist/ssr/Code";
 import { DiscordLogo } from "@phosphor-icons/react/dist/ssr/DiscordLogo";
 import { GithubLogo } from "@phosphor-icons/react/dist/ssr/GithubLogo";
 import { TelegramLogo } from "@phosphor-icons/react/dist/ssr/TelegramLogo";
+import { UsersThree } from "@phosphor-icons/react/dist/ssr/UsersThree";
 import { XLogo } from "@phosphor-icons/react/dist/ssr/XLogo";
 import { YoutubeLogo } from "@phosphor-icons/react/dist/ssr/YoutubeLogo";
 
 export const socialLinks = [
   {
-    name: "Discord",
-    icon: <DiscordLogo />,
-    href: "https://discord.gg/invite/PythNetwork",
+    name: "GitHub",
+    icon: <GithubLogo />,
+    href: "https://github.com/pyth-network",
+  },
+  {
+    name: "Pyth Dev Forum",
+    icon: <Code />,
+    href: "https://dev-forum.pyth.network/",
   },
   {
     name: "X",
@@ -16,18 +23,23 @@ export const socialLinks = [
     href: "https://x.com/PythNetwork",
   },
   {
+    name: "Discord",
+    icon: <DiscordLogo />,
+    href: "https://discord.gg/invite/PythNetwork",
+  },
+  {
     name: "Telegram",
     icon: <TelegramLogo />,
     href: "https://t.me/Pyth_Network",
   },
   {
-    name: "GitHub",
-    icon: <GithubLogo />,
-    href: "https://github.com/pyth-network",
-  },
-  {
     name: "Youtube",
     icon: <YoutubeLogo />,
     href: "https://www.youtube.com/channel/UCjCkvPN9ohl0UDvldfn1neg",
+  },
+  {
+    name: "Pyth DAO Forum",
+    icon: <UsersThree />,
+    href: "https://forum.pyth.network/",
   },
 ];
