09c01dfff0244ad4fc4747b5b4cb1e4b99037222
Alexandru Cambose <alexcambose@yahoo.com>
2025-10-07 17:41:26 +0100

    feat: remove app shell for now

--- apps/developer-hub/src/config/layout.config.tsx
+++ apps/developer-hub/src/config/layout.config.tsx
@@ -6,20 +6,36 @@ import { source } from "../lib/source";
 export const baseOptions: BaseLayoutProps = {
   nav: {
     enabled: true,
+    title: (
+      // todo change the logo here
+      <>
+        <svg
+          width="24"
+          height="24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-label="Logo"
+        >
+          <circle cx={12} cy={12} r={12} fill="currentColor" />
+        </svg>
+        Developer Hub
+      </>
+    ),
   },
-  themeSwitch: {
-    enabled: false, // Keep this false as the theme switch is handled by the component library
-  },
-  searchToggle: {
-    enabled: false,
-  },
+  // these are commented out because we don't have the app shell anymore
+  // themeSwitch: {
+  //   enabled: false, // Keep this false as the theme switch is handled by the component library
+  // },
+  // searchToggle: {
+  //   enabled: false,
+  // },
 };
 
 export const docsOptions: DocsLayoutProps = {
   ...baseOptions,
   tree: source.pageTree,
   sidebar: {
-    tabs: false,
-    collapsible: false,
+    // these are commented out because we don't have the app shell anymore
+    // tabs: false,
+    // collapsible: false,
   },
 };
