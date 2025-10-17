f7277e9df320edd9b7d7e044a54ef6d6b67d9b50
benduran <stratodyne@gmail.com>
2025-10-17 11:53:58 +0100

    chore(lazer-sdk-js): linting issue

--- lazer/sdk/js/src/util/env-util.ts
+++ lazer/sdk/js/src/util/env-util.ts
@@ -10,7 +10,10 @@ const g = globalThis as Partial<{
  * @returns true if in a worker of some kind, false if otherwise
  */
 export function envIsServiceOrWebWorker() {
-  return typeof WorkerGlobalScope !== "undefined" && g.self instanceof WorkerGlobalScope;
+  return (
+    typeof WorkerGlobalScope !== "undefined" &&
+    g.self instanceof WorkerGlobalScope
+  );
 }
 
 /**
