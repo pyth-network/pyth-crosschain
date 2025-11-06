54df5b2459b8b8052aaecac747e87bb4ad14b3fe
Mike Rolish <mrolish@dourolabs.xyz>
2025-11-06 11:48:34 -0600

    update unit tests to new state

--- apps/hip-3-pusher/tests/test_price_state.py
+++ apps/hip-3-pusher/tests/test_price_state.py
@@ -36,22 +36,28 @@ def get_config():
 
 
 def test_good_hl_price():
+    """
+    Pass through fresh HL oracle price.
+    """
     config = get_config()
     price_state = PriceState(config)
     now = time.time()
-    price_state.state["hl_oracle"][SYMBOL] = PriceUpdate("110000.0", now - price_state.stale_price_threshold_seconds / 2.0)
+    price_state.hl_oracle_state.put(SYMBOL, PriceUpdate("110000.0", now - price_state.stale_price_threshold_seconds / 2.0))
 
     oracle_px, _, _ = price_state.get_all_prices(DEX)
     assert oracle_px == {f"{DEX}:{SYMBOL}": "110000.0"}
 
 
 def test_fallback_lazer():
+    """
+    HL oracle price is stale, so fall back to fresh Lazer price.
+    """
     config = get_config()
     price_state = PriceState(config)
     now = time.time()
-    price_state.state["hl_oracle"][SYMBOL] = PriceUpdate("110000.0", now - price_state.stale_price_threshold_seconds - 1.0)
-    price_state.state["lazer"][1] = PriceUpdate("11050000000000", now - price_state.stale_price_threshold_seconds / 2.0)
-    price_state.state["lazer"][8] = PriceUpdate("99000000", now - price_state.stale_price_threshold_seconds / 2.0)
+    price_state.hl_oracle_state.put(SYMBOL, PriceUpdate("110000.0", now - price_state.stale_price_threshold_seconds - 1.0))
+    price_state.lazer_state.put(1, PriceUpdate("11050000000000", now - price_state.stale_price_threshold_seconds / 2.0))
+    price_state.lazer_state.put(8, PriceUpdate("99000000", now - price_state.stale_price_threshold_seconds / 2.0))
 
     oracle_px, _, _ = price_state.get_all_prices(DEX)
     assert oracle_px == {f"{DEX}:{SYMBOL}": "111616.16"}
@@ -59,32 +65,38 @@ def test_fallback_lazer():
 
 
 def test_fallback_hermes():
+    """
+    HL oracle price and Lazer prices are stale, so fall back to fresh Hermes price.
+    """
     config = get_config()
     price_state = PriceState(config)
     now = time.time()
-    price_state.state["hl_oracle"][SYMBOL] = PriceUpdate("110000.0", now - price_state.stale_price_threshold_seconds - 1.0)
-    price_state.state["lazer"][1] = PriceUpdate("11050000000000", now - price_state.stale_price_threshold_seconds - 1.0)
-    price_state.state["lazer"][8] = PriceUpdate("99000000", now - price_state.stale_price_threshold_seconds / 2.0)
-    price_state.state["hermes"]["e62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43"] = \
-        PriceUpdate("11100000000000", now - price_state.stale_price_threshold_seconds / 2.0)
-    price_state.state["hermes"]["2b89b9dc8fdf9f34709a5b106b472f0f39bb6ca9ce04b0fd7f2e971688e2e53b"] = \
-        PriceUpdate("98000000", now - price_state.stale_price_threshold_seconds / 2.0)
+    price_state.hl_oracle_state.put(SYMBOL, PriceUpdate("110000.0", now - price_state.stale_price_threshold_seconds - 1.0))
+    price_state.lazer_state.put(1, PriceUpdate("11050000000000", now - price_state.stale_price_threshold_seconds - 1.0))
+    price_state.lazer_state.put(8, PriceUpdate("99000000", now - price_state.stale_price_threshold_seconds / 2.0))
+    price_state.hermes_state.put("e62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43",
+                                 PriceUpdate("11100000000000", now - price_state.stale_price_threshold_seconds / 2.0))
+    price_state.hermes_state.put("2b89b9dc8fdf9f34709a5b106b472f0f39bb6ca9ce04b0fd7f2e971688e2e53b",
+                                 PriceUpdate("98000000", now - price_state.stale_price_threshold_seconds / 2.0))
 
     oracle_px, _, _ = price_state.get_all_prices(DEX)
     assert oracle_px == {f"{DEX}:{SYMBOL}": "113265.31"}
 
 
 def test_all_fail():
+    """
+    All prices are stale, so return nothing.
+    """
     config = get_config()
     price_state = PriceState(config)
     now = time.time()
-    price_state.state["hl_oracle"][SYMBOL] = PriceUpdate("110000.0", now - price_state.stale_price_threshold_seconds - 1.0)
-    price_state.state["lazer"][1] = PriceUpdate("11050000000000", now - price_state.stale_price_threshold_seconds - 1.0)
-    price_state.state["lazer"][8] = PriceUpdate("99000000", now - price_state.stale_price_threshold_seconds - 1.0)
-    price_state.state["hermes"]["e62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43"] = \
-        PriceUpdate("11100000000000", now - price_state.stale_price_threshold_seconds - 1.0)
-    price_state.state["hermes"]["2b89b9dc8fdf9f34709a5b106b472f0f39bb6ca9ce04b0fd7f2e971688e2e53b"] = \
-        PriceUpdate("98000000", now - price_state.stale_price_threshold_seconds - 1.0)
+    price_state.hl_oracle_state.put(SYMBOL, PriceUpdate("110000.0", now - price_state.stale_price_threshold_seconds - 1.0))
+    price_state.lazer_state.put(1, PriceUpdate("11050000000000", now - price_state.stale_price_threshold_seconds - 1.0))
+    price_state.lazer_state.put(8, PriceUpdate("99000000", now - price_state.stale_price_threshold_seconds - 1.0))
+    price_state.hermes_state.put("e62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43",
+                                 PriceUpdate("11100000000000", now - price_state.stale_price_threshold_seconds - 1.0))
+    price_state.hermes_state.put("2b89b9dc8fdf9f34709a5b106b472f0f39bb6ca9ce04b0fd7f2e971688e2e53b",
+                                 PriceUpdate("98000000", now - price_state.stale_price_threshold_seconds - 1.0))
 
     oracle_px, _, _ = price_state.get_all_prices(DEX)
     assert oracle_px == {}
