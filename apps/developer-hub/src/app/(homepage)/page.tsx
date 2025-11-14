290e26bd2bccc63a16b5920a0f361c99123bc3c7
Alexandru Cambose <alexcambose@yahoo.com>
2025-11-14 17:40:50 +0100

    feat: start landing page sections

--- apps/developer-hub/src/app/(homepage)/page.tsx
+++ apps/developer-hub/src/app/(homepage)/page.tsx
@@ -1,7 +1,5 @@
-// export { Homepage as default } from "../../components/Pages/Homepage";
-
-import { DeveloperHubPreview } from "../../components/Pages/TempPage"
+import { DeveloperHubPreview } from "../../components/Pages/TempPage";
 
 export default function Page() {
-  return <DeveloperHubPreview />
+  return <DeveloperHubPreview />;
 }
