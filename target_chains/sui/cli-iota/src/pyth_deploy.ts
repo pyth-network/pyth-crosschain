d32ed5ae8a7451d1cfca5dc1334e5ada4bc98d24
Ali Behjati <bahjatia@gmail.com>
2025-11-25 20:03:40 +0100

    revert back sui/iota fixes. it's everywhere

--- target_chains/sui/cli-iota/src/pyth_deploy.ts
+++ target_chains/sui/cli-iota/src/pyth_deploy.ts
@@ -8,7 +8,7 @@ import {
 
 import { Ed25519Keypair } from "@iota/iota-sdk/keypairs/ed25519";
 import { execSync } from "child_process";
-import type { DataSource } from "@pythnetwork/xc-admin-common/governance_payload/SetDataSources";
+import type { DataSource } from "@pythnetwork/xc-admin-common";
 import { IotaClient } from "@iota/iota-sdk/client";
 import { bcs } from "@iota/iota-sdk/bcs";
 
@@ -67,7 +67,7 @@ export async function publishPackage(
   ) {
     throw new Error(
       "No publish event found in transaction:" +
-        JSON.stringify(result.objectChanges, null, 2),
+      JSON.stringify(result.objectChanges, null, 2),
     );
   }
 
