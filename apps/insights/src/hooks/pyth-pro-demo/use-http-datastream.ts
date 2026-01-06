84614bd47513af5040d4785ca70e2307354e42b7
benduran <stratodyne@gmail.com>
2026-01-06 13:38:14 +0000

    feat(insights): added prefetching next data and processing until data runs out

--- apps/insights/src/hooks/pyth-pro-demo/use-http-datastream.ts
+++ apps/insights/src/hooks/pyth-pro-demo/use-http-datastream.ts
@@ -1,7 +1,7 @@
 import { useAlert } from "@pythnetwork/component-library/useAlert";
 import type { Nullish } from "@pythnetwork/shared-lib/types";
 import { wait } from "@pythnetwork/shared-lib/util";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import type {
   UseDataStreamReturnType,
@@ -27,6 +27,11 @@ const BASE_FETCH_HISTORICAL_DATA_RETRY_DELAY = 100; // 100 milliseconds
 // 🚨 DEMO HACK ALERT: we pick some point into trading
 // to ensure all APIs have saturated query results
 const INITIAL_START_AT = new Date("2025-12-05T19:00:00.000Z");
+
+// wait a maximum of 1 minute for data points to flush out.
+// we should only be getting 1 minutes-worth of data, anyways,
+// so this is even a fairly large waiting period
+const MAX_WAIT_TO_PROCESS_TIME = 1000 * 60 * 2;
 
 function getFetchHistoricalUrl(
   datasources: AllDataSourcesType[],
@@ -109,12 +114,88 @@ export function useHttpDataStream({
   const playbackSpeedRef = useRef(playbackSpeed);
   const symbolRef = useRef(symbol);
   const startAtToFetchRef = useRef(INITIAL_START_AT);
+  const canProcessRef = useRef(true);
 
   /** state */
   const [status, setStatus] =
     useState<UseDataStreamReturnType["status"]>("closed");
   const [error, setError] = useState<Nullish<Error>>(undefined);
   const [startAtToFetch, setStartAtToFetch] = useState(INITIAL_START_AT);
+
+  /** callbacks */
+  const handleFetchError = useCallback((error_: unknown) => {
+    if (!(error_ instanceof Error)) throw error_;
+    setError(error_);
+  }, []);
+  const processData = useCallback(
+    async (data: GetPythHistoricalPricesReturnType) => {
+      // if we don't have the greelight to process results,
+      // we'll just spin for a while.
+      // if we hit a max time, then we fully abort, doing nothing
+      const waitStartTime = Date.now();
+      while (!canProcessRef.current) {
+        const now = Date.now();
+
+        // if we've waited long enough and the prior
+        // processor didn't free up the processing lock,
+        // just abort and show an error to the user
+        if (now - waitStartTime >= MAX_WAIT_TO_PROCESS_TIME) {
+          showAlert({
+            contents:
+              "A timeout occurred while waiting to process the next historical data chunk. This is a bug 🐛 and should be reported.",
+            title: "Historical playback error",
+          });
+          return;
+        }
+
+        await wait(10);
+      }
+
+      // all the results should be in order of timestamp, regardless of the datasource
+      // so we'll just write them all out to the chart as they flow in.
+
+      const dataLen = data.length;
+      const quarterPoint = Math.floor(dataLen / 4);
+      const lastPoint = data.at(-1);
+
+      for (let i = 0; i < dataLen; i++) {
+        canProcessRef.current = false;
+        const currPoint = data[i];
+        const nextPoint = data[i + 1];
+
+        if (currPoint) {
+          const dataPointSymbol = appendReplaySymbolSuffix(
+            currPoint.symbol as AllAllowedSymbols,
+          );
+
+          if (dataPointSymbol !== symbolRef.current) break;
+
+          addDataPoint(currPoint.source, dataPointSymbol, currPoint);
+        }
+
+        if (i === quarterPoint) {
+          if (lastPoint) {
+            setStartAtToFetch(new Date(lastPoint.timestamp));
+          } else {
+            // there wasn't any data for this chunk, so just keep adding 1 minute
+            // to the request until we get data back
+            const d = new Date(startAtToFetchRef.current);
+            d.setTime(d.getTime() + 1000 * 60);
+            setStartAtToFetch(d);
+          }
+        }
+
+        if (currPoint && nextPoint) {
+          const delta =
+            new Date(nextPoint.timestamp).valueOf() -
+            new Date(currPoint.timestamp).valueOf();
+          await wait(delta / playbackSpeedRef.current);
+        }
+      }
+      canProcessRef.current = true;
+    },
+    [addDataPoint, showAlert],
+  );
 
   /** effects */
   useEffect(() => {
@@ -137,52 +218,15 @@ export function useHttpDataStream({
 
     setStatus("connected");
 
-    fetchHistoricalData(url)
-      .then(async (data) => {
-        // all the results should be in order of timestamp, regardless of the datasource
-        // so we'll just write them all out to the chart as they flow in.
-        // this may mean that one data source runs further ahead than another for a bit,
-        // if there is no data for a certain time interval
-
-        for (let i = 0; i < data.length; i++) {
-          const currPoint = data[i];
-          const nextPoint = data[i + 1];
-
-          if (currPoint) {
-            const dataPointSymbol = appendReplaySymbolSuffix(
-              currPoint.symbol as AllAllowedSymbols,
-            );
-
-            if (dataPointSymbol !== symbolRef.current) break;
-
-            addDataPoint(currPoint.source, dataPointSymbol, currPoint);
-          }
-
-          if (currPoint && nextPoint) {
-            const delta =
-              new Date(nextPoint.timestamp).valueOf() -
-              new Date(currPoint.timestamp).valueOf();
-            await wait(delta / playbackSpeedRef.current);
-          }
-        }
-
-        const lastPoint = data.at(-1);
-
-        if (lastPoint) {
-          setStartAtToFetch(new Date(lastPoint.timestamp));
-        } else {
-          // there wasn't any data for this chunk, so just keep adding 1 minute
-          // to the request until we get data back
-          const d = new Date(startAtToFetchRef.current);
-          d.setTime(d.getTime() + 1000 * 60);
-          setStartAtToFetch(d);
-        }
-      })
-      .catch((error_: unknown) => {
-        if (!(error_ instanceof Error)) throw error_;
-        setError(error_);
-      });
-  }, [addDataPoint, dataSources, enabled, startAtToFetch, symbol]);
+    fetchHistoricalData(url).then(processData).catch(handleFetchError);
+  }, [
+    dataSources,
+    enabled,
+    handleFetchError,
+    processData,
+    startAtToFetch,
+    symbol,
+  ]);
 
   useEffect(() => {
     if (!error) return;
