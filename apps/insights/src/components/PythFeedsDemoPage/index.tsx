21937de7367e1009c871d963fcd57ae222d3ebd4
benduran <stratodyne@gmail.com>
2026-01-08 12:23:06 +0000

    fix(insights): fixed app state provider not being wrapped in a suspense

--- apps/insights/src/components/PythFeedsDemoPage/index.tsx
+++ apps/insights/src/components/PythFeedsDemoPage/index.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Spinner } from "@pythnetwork/component-library/Spinner";
+import { Suspense } from "react";
+
 import classes from "./index.module.scss";
 import {
   PythProApiTokensProvider,
@@ -12,32 +15,43 @@ import { PythProDemoPriceChart } from "../PythProDemoPriceChart";
 import { PythProDemoSourceSelector } from "../PythProDemoSourceSelector";
 
 export function PythFeedsDemoPage() {
+  const suspenseLoaderLabel = "Initializing Pyth Pro demo...";
+
   return (
     <PythProApiTokensProvider>
-      <PythProAppStateProvider>
-        <WebSocketsProvider>
-          <article className={classes.pythFeedsDemoPageRoot}>
-            <section>
-              <div className={classes.subheader}>
-                <div>
-                  <h3>Pyth Pro</h3>
-                  <h4>Real-time feed comparison tool</h4>
+      <Suspense
+        fallback={
+          <div className={classes.suspenseLoader}>
+            <div>{suspenseLoaderLabel}</div>
+            <Spinner isIndeterminate label={suspenseLoaderLabel} />
+          </div>
+        }
+      >
+        <PythProAppStateProvider>
+          <WebSocketsProvider>
+            <article className={classes.pythFeedsDemoPageRoot}>
+              <section>
+                <div className={classes.subheader}>
+                  <div>
+                    <h3>Pyth Pro</h3>
+                    <h4>Real-time feed comparison tool</h4>
+                  </div>
+                  <div>
+                    <PythProApiTokensMenu />
+                    <PythProDemoSourceSelector />
+                  </div>
                 </div>
-                <div>
-                  <PythProApiTokensMenu />
-                  <PythProDemoSourceSelector />
+                <div className={classes.body}>
+                  <PythProDemoCards />
                 </div>
-              </div>
-              <div className={classes.body}>
-                <PythProDemoCards />
-              </div>
-            </section>
-            <aside>
-              <PythProDemoPriceChart />
-            </aside>
-          </article>
-        </WebSocketsProvider>
-      </PythProAppStateProvider>
+              </section>
+              <aside>
+                <PythProDemoPriceChart />
+              </aside>
+            </article>
+          </WebSocketsProvider>
+        </PythProAppStateProvider>
+      </Suspense>
     </PythProApiTokensProvider>
   );
 }
