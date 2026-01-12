66a51cb60c08f8c898fefec13ecc7dedbf95e1d8
benduran <stratodyne@gmail.com>
2026-01-12 16:07:20 +0000

    chore disable linting of examples fiels

--- lazer/contracts/sui/sdk/js/eslint.config.js
+++ lazer/contracts/sui/sdk/js/eslint.config.js
@@ -1,1 +1,12 @@
-export { base as default } from "@cprussin/eslint-config";
+import { base } from "@cprussin/eslint-config";
+import { globalIgnores } from "eslint/config";
+
+export default [
+  globalIgnores(["src/__tests__/**/*", "src/examples/**/*"]),
+  ...base,
+  {
+    rules: {
+      "unicorn/filename-case": "off",
+    },
+  },
+];
