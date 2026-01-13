b7c877e1c5f5ee2e65302d2329a2139df3fafe17
benduran <stratodyne@gmail.com>
2026-01-13 19:56:18 +0000

    fix(xc_admin): fixed build

--- governance/xc_admin/packages/xc_admin_frontend/next.config.js
+++ governance/xc_admin/packages/xc_admin_frontend/next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
   // eslint-disable-next-line n/no-process-env
   output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
   turbopack: {
+    resolveAlias: {
+      fs: {
+        /**
+         * HACK ALERT: There are some react hooks
+         * that are importing things from @coral-xyz/anchor,
+         * which is a huge offender of including node-only deps
+         * in its library, even if it's supposed to have isomorphic exports.
+         * Since the "correct" fix would require substantial rearchitecting
+         * of this project, shimming accidental node imports with an
+         * empty module is what we'll use
+         */
+        browser: "./turbopack-hacks/empty.ts",
+      },
+    },
     resolveExtensions: [
       ".ts",
       ".tsx",
