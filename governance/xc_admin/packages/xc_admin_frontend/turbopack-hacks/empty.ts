977b8d859f52fb43ad3497cd7739582d4cd241ea
benduran <stratodyne@gmail.com>
2026-01-15 09:15:05 +0000

    chore(xc_admin_frontend): added comment in the empty.ts file for the turbopack hacks

--- governance/xc_admin/packages/xc_admin_frontend/turbopack-hacks/empty.ts
+++ governance/xc_admin/packages/xc_admin_frontend/turbopack-hacks/empty.ts
@@ -1,1 +1,11 @@
-export {}
+/**
+ * HACK ALERT: There are some react hooks
+ * that are importing things from coral-xyz/anchor,
+ * which is a huge offender of including node-only deps
+ * in its library, even if it's supposed to have isomorphic exports.
+ * Since the "correct" fix would require substantial rearchitecting
+ * of this project, shimming accidental node imports with an
+ * empty module is what we'll use.
+ * this file is used in the nearest next.config.js for this project
+ */
+export {};
