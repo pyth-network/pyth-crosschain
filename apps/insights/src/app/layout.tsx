b31fa8fd5ca74d93e60681a4d689a679cd1a93b3
benduran <stratodyne@gmail.com>
2026-01-20 09:29:30 +0000

    chore: removed another accidental file

--- apps/insights/src/app/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-import { Root } from "../components/Root";
-export { metadata, viewport } from "../metadata";
-
-export default function DefaultLayout({ children }: PropsWithChildren) {
-  return <Root>{children}</Root>;
-}
