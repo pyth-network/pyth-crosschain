3ed1d785be89f0d10e17dfff576f4ae725235547
benduran <stratodyne@gmail.com>
2026-01-20 09:26:56 +0000

    chore: removed file that accidentally snuck in from a previous branch

--- apps/insights/src/app/layout.tsx
+++ apps/insights/src/app/layout.tsx
@@ -1,13 +1,8 @@
-import { SimpleStyleProvider } from "@pythnetwork/component-library/styles";
 import type { PropsWithChildren } from "react";
 
 import { Root } from "../components/Root";
 export { metadata, viewport } from "../metadata";
 
 export default function DefaultLayout({ children }: PropsWithChildren) {
-  return (
-    <SimpleStyleProvider>
-      <Root>{children}</Root>
-    </SimpleStyleProvider>
-  );
+  return <Root>{children}</Root>;
 }
