9a7addae403816daf3272527a3ce25dd2cba7a85
benduran <stratodyne@gmail.com>
2026-02-03 06:30:25 +0000

    fix(pyth-app): fixed build

--- apps/pyth-app/src/components/ComingSoonNavLink/ComingSoonNavLink.tsx
+++ apps/pyth-app/src/components/ComingSoonNavLink/ComingSoonNavLink.tsx
@@ -1,20 +1,20 @@
-import type { NavigationButtonLinkProps } from "@pythnetwork/component-library/v2";
-import { Badge, NavigationButtonLink } from "@pythnetwork/component-library/v2";
+import type { ButtonLinkProps } from "@pythnetwork/component-library/v2";
+import { Badge, ButtonLink } from "@pythnetwork/component-library/v2";
 
 import { classes } from "./ComingSoonNavLink.styles";
 
 export function ComingSoonNavLink({
   children,
   ...rest
-}: Omit<NavigationButtonLinkProps, "disabled">) {
+}: Omit<ButtonLinkProps, "disabled">) {
   return (
-    <NavigationButtonLink {...rest} disabled>
+    <ButtonLink {...rest} disabled>
       <span className={classes.root}>
         {children}
         <Badge size="xs" variant="neutral">
           COMING SOON
         </Badge>
       </span>
-    </NavigationButtonLink>
+    </ButtonLink>
   );
 }
