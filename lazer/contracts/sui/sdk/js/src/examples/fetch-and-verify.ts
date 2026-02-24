82057008376312f2b14521730d0ea7de74306cdf
Matej <matej@dourolabs.xyz>
2026-02-24 14:50:36 +0100

    @pythnetwork/pyth-lazer-sui-js: update to latest Sui SDK

--- lazer/contracts/sui/sdk/js/src/examples/fetch-and-verify.ts
+++ lazer/contracts/sui/sdk/js/src/examples/fetch-and-verify.ts
@@ -1,10 +1,8 @@
-/* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/** biome-ignore-all lint/correctness/noUnusedVariables: code example */
+/** biome-ignore-all lint/suspicious/noConsole: code example */
 
 import process from "node:process";
-
-import { SuiClient } from "@mysten/sui/client";
-import { decodeSuiPrivateKey } from "@mysten/sui/cryptography";
+import { SuiGrpcClient } from "@mysten/sui/grpc";
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import { Transaction } from "@mysten/sui/transactions";
 import { PythLazerClient } from "@pythnetwork/pyth-lazer-sdk";
@@ -13,60 +11,63 @@ import { hideBin } from "yargs/helpers";
 
 import { addParseAndVerifyLeEcdsaUpdateCall } from "../index.js";
 
-const { fullnodeUrl, stateId, lazerToken } = await yargs(hideBin(process.argv))
-  .option("fullnode-url", {
-    type: "string",
+const {
+  network,
+  baseUrl,
+  stateId: STATE_ID,
+  lazerToken: LAZER_TOKEN,
+} = await yargs(hideBin(process.argv))
+  .option("network", {
+    choices: ["mainnet", "testnet", "devnet", "localnet"] as const,
+    default: "testnet" as const,
+    description: "Sui network name, e.g. 'mainnet'",
+  })
+  .option("base-url", {
+    demandOption: true,
     description:
       "URL of the full Sui node RPC endpoint. e.g: https://fullnode.testnet.sui.io:443",
-    demandOption: true,
+    type: "string",
   })
   .option("state-id", {
-    type: "string",
-    description: "Lazer contract shared State object ID",
     demandOption: true,
+    description: "Lazer contract shared State object ID",
+    type: "string",
   })
   .option("lazer-token", {
-    type: "string",
-    description: "Lazer authentication token",
     demandOption: true,
+    description: "Lazer authentication token",
+    type: "string",
   })
   .help()
   .parseAsync();
 
-// eslint-disable-next-line n/no-process-env
-if (!process.env.SUI_KEY) {
-  throw new Error(
-    "'SUI_KEY' environment variable should be set to your Sui private key in Bech32 format.",
-  );
-}
-// eslint-disable-next-line n/no-process-env
-const keypair = decodeSuiPrivateKey(process.env.SUI_KEY);
-const signer = Ed25519Keypair.fromSecretKey(keypair.secretKey);
+const { SUI_KEY = "" } = process.env;
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
 // Steps for fetching and verifying the price update:
 
 // 1. Fetch the price update from Pyth Lazer in "leEcdsa" format:
-const lazer = await PythLazerClient.create({ token: lazerToken });
+const lazer = await PythLazerClient.create({ token: LAZER_TOKEN });
 const latestPrice = await lazer.getLatestPrice({
+  channel: "fixed_rate@200ms",
+  formats: ["leEcdsa"],
+  jsonBinaryEncoding: "hex",
   priceFeedIds: [1],
   properties: ["price", "bestBidPrice", "bestAskPrice", "exponent"],
-  formats: ["leEcdsa"],
-  channel: "fixed_rate@200ms",
-  jsonBinaryEncoding: "hex",
 });
 const update = Buffer.from(latestPrice.leEcdsa?.data ?? "", "hex");
 
 // 2. Create a new Sui transaction:
-const client = new SuiClient({ url: fullnodeUrl });
+const signer = Ed25519Keypair.fromSecretKey(SUI_KEY);
+const client = new SuiGrpcClient({ baseUrl, network });
 const tx = new Transaction();
 
 // 3. Add the parse and verify call:
 const verifiedUpdate = await addParseAndVerifyLeEcdsaUpdateCall({
-  client,
+  client: client.core,
+  stateObjectId: STATE_ID,
   tx,
-  stateObjectId: stateId,
   update,
 });
 
@@ -74,8 +75,8 @@ const verifiedUpdate = await addParseAndVerifyLeEcdsaUpdateCall({
 
 // 5. Sign and execute the transaction:
 const result = await client.signAndExecuteTransaction({
-  transaction: tx,
   signer,
+  transaction: tx,
 });
 
 console.log("Result:", JSON.stringify(result, undefined, 2));
