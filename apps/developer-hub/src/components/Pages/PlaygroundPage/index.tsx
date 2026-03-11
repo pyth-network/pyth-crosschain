ae1501f56278c0251b181c66260131e327db467c
benduran <stratodyne@gmail.com>
2026-03-11 15:29:12 +0000

    chore: removed bogus form that was used for testing

--- apps/developer-hub/src/components/Pages/PlaygroundPage/index.tsx
+++ apps/developer-hub/src/components/Pages/PlaygroundPage/index.tsx
@@ -12,14 +12,6 @@ export async function PlaygroundPage() {
         accessToken: tokenData?.initialAccessToken ?? "",
       }}
     >
-      <form action="/api/playground/deeplink" method="POST">
-        <input
-          name="initialAccessToken"
-          placeholder="put your token here"
-          type="password"
-        />
-        <button type="submit">Submit</button>
-      </form>
       <PlaygroundContent />
     </PlaygroundProvider>
   );
