309ffc9b26c98b5d4a6752d95edb1259e963048a
Guillermo Bescos <g.bescos@yahoo.com>
2026-04-24 18:44:03 +0100

    clean

--- target_chains/solana/programs/core-bridge/src/sdk/mod.rs
+++ target_chains/solana/programs/core-bridge/src/sdk/mod.rs
@@ -29,10 +29,10 @@ pub use crate::{
     constants::{PROGRAM_EMITTER_SEED_PREFIX, SOLANA_CHAIN},
     id,
     legacy::instruction::PostMessageArgs,
-    processor::{InitMessageV1Args, WriteMessageV1Args, WriteEncodedVaaArgs},
+    processor::{InitMessageV1Args, WriteEncodedVaaArgs, WriteMessageV1Args},
     state,
     types::*,
-    utils::vaa::{VAA_START, EmitterInfo, VaaAccount},
+    utils::vaa::{EmitterInfo, VaaAccount, VAA_START},
 };
 #[doc(inline)]
 #[cfg(feature = "cpi")]
