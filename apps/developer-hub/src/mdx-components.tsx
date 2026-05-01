df6206275ebf5c59a9cdd8cccceeb9b2dd52e0c0
Aditya Arora <arora.aditya520@gmail.com>
2026-05-01 11:11:59 -0400

    Revert "fix(developer-hub): adapt fumadocs image component"
    This reverts commit 4b6a934bd2866f4d36ffd7f74815d6bfd476f8d0.

--- apps/developer-hub/src/mdx-components.tsx
+++ apps/developer-hub/src/mdx-components.tsx
@@ -3,7 +3,6 @@ import { createAPIPage } from "fumadocs-openapi/ui";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
-import type { ImgHTMLAttributes } from "react";
 
 import { APICard, APICards } from "./components/APICard";
 import { BinaryFormatCards } from "./components/BinaryFormatCards";
@@ -14,42 +13,21 @@ import { YouTubeEmbed } from "./components/YouTubeEmbed";
 import { openapi } from "./lib/openapi";
 
 const OpenAPIPage = createAPIPage(openapi);
-const { img: FumadocsImage, ...defaultMdxComponentsWithoutImage } =
-  defaultMdxComponents;
-
-function Image(props: ImgHTMLAttributes<HTMLImageElement>) {
-  const { sizes, ...rest } = props;
-
-  return sizes === undefined ? (
-    <FumadocsImage {...rest} />
-  ) : (
-    <FumadocsImage {...rest} sizes={sizes} />
-  );
-}
-
-const overridableMdxComponents = {
-  APICard,
-  APICards,
-  APIPage: OpenAPIPage,
-  Tab,
-  Tabs,
-} satisfies MDXComponents;
-
-const requiredMdxComponents = {
-  BinaryFormatCards,
-  FieldCodePanel,
-  InfoBox: InfoBox,
-  PropertyCard,
-  PropertyFieldLinker,
-  YouTubeEmbed,
-} satisfies MDXComponents;
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
-    ...defaultMdxComponentsWithoutImage,
-    img: Image,
-    ...overridableMdxComponents,
+    ...defaultMdxComponents,
+    APIPage: OpenAPIPage,
+    APICard,
+    APICards,
+    Tabs,
+    Tab,
     ...components,
-    ...requiredMdxComponents,
-  };
+    InfoBox: InfoBox,
+    YouTubeEmbed,
+    BinaryFormatCards,
+    FieldCodePanel,
+    PropertyCard,
+    PropertyFieldLinker,
+  } satisfies MDXComponents;
 }
